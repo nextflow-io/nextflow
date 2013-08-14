bb69e0d0a9c9200071b53f7a243fb4916a0ecb1a
bmsanjuan <bmsanjuan@gmail.com>
2013-08-14 16:38:33 +0200

    DxHelper & DxHelperTest updated to JsonNodeToMap and tests

--- src/main/groovy/nextflow/util/DxHelper.groovy
+++ src/main/groovy/nextflow/util/DxHelper.groovy
@@ -1,26 +1,36 @@
 package nextflow.util
-import java.nio.ByteBuffer
-import java.util.concurrent.atomic.AtomicInteger
 
 import com.dnanexus.DXAPI
 import com.dnanexus.DXJSON
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.BigIntegerNode
+import com.fasterxml.jackson.databind.node.BinaryNode
+import com.fasterxml.jackson.databind.node.BooleanNode
+import com.fasterxml.jackson.databind.node.DecimalNode
+import com.fasterxml.jackson.databind.node.DoubleNode
+import com.fasterxml.jackson.databind.node.IntNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.LongNode
+import com.fasterxml.jackson.databind.node.NullNode
+import com.fasterxml.jackson.databind.node.NumericNode
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.POJONode
+import com.fasterxml.jackson.databind.node.TextNode
 import groovy.util.logging.Slf4j
-import groovyx.gpars.dataflow.Dataflow
-import groovyx.gpars.dataflow.DataflowQueue
-import groovyx.gpars.dataflow.DataflowVariable
-import groovyx.gpars.dataflow.operator.DataflowEventAdapter
-import groovyx.gpars.dataflow.operator.DataflowProcessor
 import org.apache.http.HttpVersion
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.InputStreamEntity
+import org.apache.http.entity.FileEntity
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.params.CoreProtocolPNames
 import org.apache.http.util.EntityUtils
-import sun.nio.ch.DirectBuffer
 
 /**
  *
@@ -61,284 +71,251 @@ class DxHelper {
     /**
      * Uploads the defined file to the Dnanexus' space
      *
-     * @param fileToUpload The file to be uploaded. The size cannot exceed 5TB
-     * @param targetName Name used to store the file in the remote storage. If not specified the source file name is used.
-     * @param workspaceId The DNAnexus workspace-id where the file has to be stored. If not specified the environment variable {@code DX_WORKSPACE_ID} is used
+     * @param fileToUpload
+     * @param fileName
      * @return fileId
      */
-    static String uploadFile( File fileToUpload, String targetName = null, String workspaceId = null ) {
-        assert fileToUpload
+    static String uploadFile( File fileToUpload, String fileName) {
 
-        if( !fileToUpload.exists() ) {
-            throw new IllegalArgumentException("Missing file: $fileToUpload -- upload failed" )
-        }
+        def projectId = System.getenv('DX_PROJECT_CONTEXT_ID')
+        log.debug  "Current project >> ${projectId}"
 
-        if( fileToUpload.size() > new MemoryUnit('5TB').toBytes() ) {
-            throw new IllegalArgumentException("File $fileToUpload exceed the maximum allowed upload size (5TB) -- upload failed" )
-        }
+        def newFileRequest = mapToJsonNode([name: fileName, project: projectId ?: 'project-B7fQ9vj0FqXB2z80y5FQ0JGG'] )
+        println "Request >> ${newFileRequest.toString()}"
 
-        // fallback to the source file name
-        if( !targetName ) {
-            targetName = fileToUpload.name
-        }
-        def targetFile = new File(targetName)
+        def result = DXAPI.fileNew(  newFileRequest )
+        log.debug "FileNew >> ${result.toString()}"
 
-        // try to access to the current 'DX_WORKSPACE_ID'
-        if( !workspaceId ) {
-            workspaceId = System.getenv('DX_WORKSPACE_ID')
-        }
+        def fileId = result.get('id').textValue()
+        def upload = DXAPI.fileUpload(fileId)
+        log.debug "FileUpload >> ${upload.toString()}"
 
-        def uploadInfo = [project: workspaceId, name: targetFile.name]
-        if( targetFile.parent ) {
-            // set the parent folder where it must be stored
-            uploadInfo.folder = targetFile.parent
-            uploadInfo.parents = true
-        }
-        log.debug "Dx upload info: $uploadInfo"
+        def url = upload.get('url').textValue()
+        def headers = upload.get('headers')
+        log.debug "Headers >>>\n" + headers.toString()
 
-        /*
-         * Create a new remote file
-         */
-        def newFileRequest = mapToJsonNode(uploadInfo)
-        log.trace "Dx fileNew request > ${newFileRequest.toString()}"
+        def auth = headers.get('Authorization').textValue()
 
-        def result = DXAPI.fileNew(newFileRequest)
-        log.trace "Dx fileNew reply > ${result.toString()}"
+        HttpClient client = new DefaultHttpClient();
+        client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
 
-        /*
-         * The file is splitted in multiple chunks which are uploaded in parallel
-         */
-        final fileId = result.get('id').textValue()
+        HttpPost post = new HttpPost( url );
 
-        // defines the max size of each chunk
-        // By the API spec if must be a value between 5MB and 5GB
-        // See https://wiki.dnanexus.com/API-Specification-v1.0.0/Files#API-method:-/file-xxxx/upload
-        final chunkSize = 1024 * 1024 * 100 // 100 MB
-        final maxParallel = 5
-        final chunks = new DataflowQueue()
-        def totChunks = 0
-        while( (totChunks*chunkSize) <= fileToUpload.size() ) {
-            chunks << totChunks++
-        }
+        post.setHeader('Authorization', auth)
 
-        // synchronization vars
-        final counter = new AtomicInteger()
-        final done = new DataflowVariable()
-        final Map<Thread,ByteBuffer> buffers = [:]
+        FileEntity entity = new FileEntity(fileToUpload)
+        post.setEntity(entity)
 
-        // error handler to redirect exception on the application log
-        def errorHandler = new DataflowEventAdapter() {
-            public boolean onException(final DataflowProcessor processor, final Throwable e) {
-                DxHelper.log.error "Failure uploading: $fileToUpload", e
-                done << false
-                return true;
-            }
-        }
+        String response = EntityUtils.toString( client.execute( post ).getEntity(), "UTF-8" );
+        log.debug "Post >> ${response}"
 
-        /*
-         * parallel uploader
-         */
-        Dataflow.operator( inputs:[chunks], outputs:[], maxForks: maxParallel, listeners: [errorHandler] ) { int current ->
+        def close = DXAPI.fileClose(fileId)
+        log.debug "FileClose >> ${close.toString()}"
 
-            final fileName = fileToUpload.name
-            log.trace "Uploading chunk [$current] for file: ${fileName}"
-
-            // open the file in 'read' mode
-            final file = new RandomAccessFile(fileToUpload, 'r')
-            final offset = current * chunkSize
-            final channel = file.getChannel()
-
-            // get am available buffer or allocate it
-            def tt = Thread.currentThread()
-            def buffer = buffers[tt]
-            if( !buffer ) {
-                log.trace "File: $fileName; chunk [$current] > allocating buffer for thread: ${tt}"
-                buffer = buffers[tt] = ByteBuffer.allocateDirect(chunkSize)
-            }
-            else {
-                log.trace "File: $fileName; chunk [$current] > clearing buffer: $tt"
-                buffer.clear()
-            }
-
-            // read the chunk to be uploaded using a direct buffer
-            def len = channel.read(buffer, offset)
-            log.trace "File: $fileName; chunk [$current] > read buffer len: $len "
-
-            // request to upload a new chunk
-            // note: dnanexus upload chunk index is 1-based
-            def params = mapToJsonNode( index: current+1 )
-            def upload = DXAPI.fileUpload(fileId, params)
-            log.trace "File: $fileName; chunk [$current] > FileUpload: ${upload.toString()}"
-
-            // the response provide the url when 'post' the chunk and the
-            // 'authorization' code
-            def url = upload.get('url').textValue()
-            def auth = upload.get('headers')?.get('Authorization')?.textValue()
-
-            // create a 'post' request to upload the stuff
-            HttpPost post = new HttpPost(url);
-            post.setHeader('Authorization', auth)
-
-            buffer.flip()
-            log.trace "File: $fileName; chunk [$current] > buffer remaining: ${buffer.remaining()} "
-
-            def payload = new InputStreamEntity(new ByteBufferBackedInputStream(buffer), len)
-            post.setEntity(payload)
-
-            HttpClient client = new DefaultHttpClient();
-            client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-            log.trace "File: $fileName; chunk [$current] > Post starting: $post "
-
-            def entity = client.execute( post ).getEntity()
-            String response = EntityUtils.toString( entity, "UTF-8" );
-            log.trace "File: $fileName; chunk [$current] > post response: ${response}"
-
-            client.getConnectionManager().shutdown()
-
-            // when ALL the chunks have been uploaded, signal the termination
-            if( counter.incrementAndGet() == totChunks )  {
-                done << true
-            }
-
-        }
-
-        /*
-         * await the termination
-         */
-        try {
-            if( done.get() )  {
-                /*
-                 * when finished, close the remote file
-                 */
-                def close = DXAPI.fileClose(fileId)
-                log.debug "Dx fileClose > ${close.toString()}"
-                return fileId
-            }
-
-            // return null in case of error (!done)
-            return null
-        }
-        finally {
-            log.trace "Uploader > release buffers"
-            // release the direct buffer
-            buffers.values().each { DirectBuffer it ->
-                it?.cleaner() ?.clean()
-            }
-        }
-
+        return fileId
     }
 
 
-    /**
-     * Converts a {@code Map} to a {@code ObjectNode} instance
-     *
-     * @param params
-     * @return
-     */
-    static ObjectNode mapToJsonNode( Map params ) {
-        assert params != null
-
-        def node = DXJSON.getObjectBuilder()
-        params.each { String name, value ->
-
-            switch( value ) {
-                case boolean:
-                    node = node.put(name, value.asBoolean() );
-                    break
-
-                case Integer:
-                    node = node.put(name, value as Integer)
-                    break
-
-                case Long:
-                    node = node.put(name, value as Long)
-                    break
-
-                case double:
-                    node = node.put(name, value as Double)
-                    break
-
-                case Map:
-                    node = node.put(name, mapToJsonNode(value as Map))
-                    break
-
-                default:
-                    node = node.put(name, value?.toString() )
-
-            }
-
-        }
-
-        node.build()
-    }
-
-
-//    static jsonNodeToMap( JsonNode node ) {
+//    /**
+//     * Converts a {@code Map} to a {@code ObjectNode} instance
+//     *
+//     * @param params
+//     * @return
+//     */
+//    static ObjectNode mapToJsonNode( Map params ) {
+//        assert params != null
 //
-//        def result = [:]
-//        jsonNodeToMap(node, result)
-//        return result
-//    }
-
-//    static jsonNodeToMap( JsonNode node, Map result  ) {
+//        def node = DXJSON.getObjectBuilder()
+//        params.each { String name, value ->
+//
+//            switch( value ) {
+//
+//                case String:
+//                    node = node.put(name, value as String)
+//                    break
+//
+//                case BigDecimal:
+//                    node = node.put(name, new DecimalNode(value as BigDecimal))
+//                    break
+//
+//                case BigInteger:
+//                    node = node.put(name, new BigIntegerNode(value as BigInteger))
+//                    break
+//
+//                case byte[]:
+//                    node = node.put( name, new BinaryNode(value as byte[]) )
+//                    break
+//
+//                case Boolean:
+//                    node = node.put(name, value as Boolean)
+//                    break
+//
+//                case Float:
+//                case Double:
+//                    node = node.put(name, value as Double)
+//                    break
+//
+//                case Integer:
+//                    node = node.put(name, value as Integer)
+//                    break
+//
+//                case Long:
+//                    node = node.put(name, value as Long)
+//                    break
+//
+//                case Map:
+//                    node = node.put(name, mapToJsonNode(value as Map))
+//                    break
+//
+//                case Collection:
+//                case Object[]:
+//
+//                    def array = new ArrayNode(JsonNodeFactory.instance)
+//                    (value as List) .each {
+//                        array.add( it )
+//                    }
+//                    node = node.put(name, array)
+//                    break
 //
 //
-//        if( node.isArray() ) {
-//            def list = []
-//            for( def item : node.elements() ) {
-//                list << jsonNodeToMap(item)
+//                default:
+//                    log.debug "Type not supported: ${value.class.name}"
+//                    node = node.put(name, value?.toString() )
+//
 //            }
 //
 //        }
-//        else if( node.isBigDecimal() ) {
 //
-//        }
-//        else if( node.isBigInteger() ) {
-//
-//        }
-//        else if( node.isBinary() ) {
-//
-//        }
-//        else if( node.isBoolean() ) {
-//
-//        }
-//        else if( node.isDouble() ) {
-//
-//        }
-//        else if( node.isFloatingPointNumber()) {
-//
-//        }
-//        else if( node.isInt() ) {
-//
-//        }
-//        else if( node.isLong()) {
-//
-//        }
-//        else if( node.isNumber()) {
-//
-//        }
-//        else if( node.isObject()) {
-//
-//        }
-//        else if( node.isPojo() ) {
-//
-//        }
-//        else if( node.isTextual()) {
-//
-//        }
-//        else if( node.isValueNode()) {
-//
-//        }
-//
-//        else if( node.isNull()) {
-//
-//        }
-//        else {
-//
-//        }
-//
-//
-//
+//        node.build()
 //    }
+
+    static JsonNode mapToJsonNode( def value ) {
+
+        switch( value ) {
+
+            case String:
+                return TextNode.valueOf(value as String)
+
+            case BigDecimal:
+                return DecimalNode.valueOf(value as BigDecimal)
+
+
+            case Integer:
+                return IntNode.valueOf(value as int)
+
+            case BigInteger:
+                return BigIntegerNode.valueOf(value as BigInteger)
+
+            case byte[]:
+                return BinaryNode.valueOf(value as byte[])
+
+            case Boolean:
+                return BooleanNode.valueOf(value as Boolean)
+
+            case Float:
+            case Double:
+                return DoubleNode.valueOf(value as Double)
+
+            case Long:
+                return LongNode.valueOf(value as Long)
+
+            case Map:
+                def result = new ObjectNode(JsonNodeFactory.instance)
+                (value as Map).each { String name, Object item ->
+                    result.put( name, mapToJsonNode(item))
+                }
+                return result
+
+            case Collection:
+            case Object[]:
+
+                def result = new ArrayNode(JsonNodeFactory.instance)
+                (value as List) .each {
+                    result.add( mapToJsonNode(it) )
+                }
+                return result
+        }
+    }
+
+
+    static Map jsonNodeToMap( JsonNode node ) {
+        def result = [:]
+
+        if( node.isObject() ) {
+            for( Map.Entry<String,JsonNode> item : node.fields() ) {
+                jsonNodeToMap(item.key, item.value, result)
+            }
+        }
+        else if( node.isArray() ) {
+
+        }
+        else {
+            throw new IllegalStateException("Not a valid json node: ${node.toString()}")
+        }
+
+        return result
+    }
+
+
+    static Map jsonNodeToMap( String field, JsonNode node, Map result ) {
+
+        if( node.isArray()){
+            for( def item : node.elements() ) {
+                result[field] = item
+            }
+        }
+        else if( node.isBigDecimal() ) {
+            result[field] = node.decimalValue()
+        }
+        else if( node.isBigInteger() ) {
+            result[field] = node.bigIntegerValue()
+        }
+        else if( node.isBinary() ) {
+            result[field] = node.binaryValue()
+        }
+        else if( node.isBoolean() ) {
+            result[field] = node.booleanValue()
+        }
+        else if( node.isDouble() ) {
+            result[field] = node.doubleValue()
+        }
+        else if( node.isFloatingPointNumber()) {
+            result[field] = node
+        }
+        else if( node.isInt() ) {
+            result[field] = node.intValue()
+        }
+        else if( node.isLong()) {
+            result[field] = node.longValue()
+        }
+        else if( node.isNumber()) {
+            result[field] = node.numberValue()
+        }
+        else if( node.isObject()) {
+            result[field] = jsonNodeToMap(node)
+        }
+        else if( node.isPojo() ) {
+            result[field] = node
+        }
+        else if( node.isTextual()) {
+            result[field] = node.textValue()
+        }
+        else if( node.isValueNode()) {
+            if(node.isContainerNode() || node.isMissingNode()){
+                result[field] = false
+            }else{
+                result[field] = true
+            }
+        }
+        else if( node.isNull()) {
+            result[field] = null
+        }
+        else {
+
+        }
+
+        return result
+    }
+
 
 }
--- src/test/groovy/nextflow/util/DxHelperTest.groovy
+++ src/test/groovy/nextflow/util/DxHelperTest.groovy
@@ -1,6 +1,15 @@
 package nextflow.util
 
-import groovy.json.JsonSlurper
+import com.dnanexus.DXJSON
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.codehaus.groovy.runtime.typehandling.FloatingPointMath
 import spock.lang.Specification
 
 /**
@@ -27,21 +36,155 @@ class DxHelperTest extends Specification {
     def testUpload() {
 
         setup:
-        def home = System.getProperty('user.home')
-        def config = new File(home, '.dnanexus_config/environment.json')
-        def json = new JsonSlurper().parseText(config.text)
-        def projectId = json.DX_PROJECT_CONTEXT_ID as String
+        String projectId = System.getenv('DX_PROJECT_CONTEXT_ID') ?: 'project-B7fQ9vj0FqXB2z80y5FQ0JGG'
 
         when:
-        File target = File.createTempFile('dxupload', 'test')
-        target.text = "Hello there!\n"
-        String id = DxHelper.uploadFile(target, 'test-upload', projectId)
+        File target = new File("fileToUpload")
+        target.text = "Esto es una prueba"
+        String id = DxHelper.uploadFile(target,target.name)
 
         then:
         id.startsWith('file-')
 
         cleanup:
         target?.delete()
+    }
+
+
+    def testMapToJsonNode(){
+
+        when:
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("one",1); // autoboxed to an object
+        map.put("two", [1, 2]); // array of ints is an object
+        map.put("three","hello"); // string is an object
+        ObjectNode result = DxHelper.mapToJsonNode(map);
+
+        then:
+        println("RESULTADO >> ${result}")
+    }
+
+    def testJsonNodeToMap2() {
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1: "string", field2:[1,2,3], field3:[x: 1, y: 2], field4:[ [p:10, q:20], [w:99], ['a', 'b', 'c'] ]] )
+        def map = DxHelper.jsonNodeToMap(node)
+
+        println map.field2.class.name
+
+        then:
+        map.field1 == 'string'
+        map.field2 == [1,2,3]
+        map.field3 == [x: 1, y: 2]
+        map.field4 == [ [p:10, q:20], [w:99], ['a', 'b', 'c'] ]
+
+    }
+
+    def testJsonNodeToMapNative(){
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1: false as Boolean , field2:234.1 as Double, field3: 3 as Integer, field4:2344444444443 as Long, field5:34 as Number, field6:'Hello'])
+        def map = DxHelper.jsonNodeToMap(node)
+
+        then:
+        map.field1 instanceof Boolean
+        map.field2 instanceof Double
+        map.field3 instanceof Integer
+        map.field4 instanceof Long
+        map.field5 instanceof Number
+        map.field6 instanceof String
+    }
+
+    def testJsonNodeToMapBigDecimal(){
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1: 0.23546769425757967900001 as BigDecimal])
+        def map = DxHelper.jsonNodeToMap(node)
+
+        then:
+        map.field1 == 0.23546769425757967900001 as BigDecimal
+    }
+
+    def testJsonNodeToMapBigInteger(){
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1: 239423904802384 as BigInteger])
+        def map = DxHelper.jsonNodeToMap(node)
+
+        then:
+        map.field1 == 239423904802384 as BigInteger
+    }
+
+    def testJsonNodeToMapBinary() {
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1: [0xa,0x2,0xf] as byte[] ] )
+        def map = DxHelper.jsonNodeToMap(node)
+
+        then:
+        map.field1 == [0xa,0x2,0xf] as byte[]
+
+    }
+
+
+    def testJSonNodeToMapArray(){
+
+        when:
+        def node = DxHelper.mapToJsonNode( [field1:[1,2,3] as Object[] ])
+        def map = DxHelper.jsonNodeToMap(node)
+
+        then:
+        map.field1 == [1,2,3]
+    }
+
+
+    def testMapToJsonNodeGeneric() {
+
+        when:
+        def node = DxHelper.mapToJsonNode( [a:1] )
+        then:
+        node.get('a').intValue() == 1
+
+
+        when:
+        node = DxHelper.mapToJsonNode( [a:1, b: 'string'] )
+        then:
+        node.get('a').intValue() == 1
+        node.get('b').textValue() == 'string'
+
+
+        when:
+        node = DxHelper.mapToJsonNode( [a:1, b: [1,2,3] ] )
+        then:
+        node.get('a').intValue() == 1
+        node.get('b') instanceof ArrayNode
+        (node.get('b') as ArrayNode).size() == 3
+        (node.get('b') as ArrayNode).get(0).intValue() == 1
+        (node.get('b') as ArrayNode).get(1).intValue() == 2
+        (node.get('b') as ArrayNode).get(2).intValue() == 3
+
+
+        when:
+        node = DxHelper.mapToJsonNode( [a:1, b: ['x', 'y'] ] )
+        then:
+        node.get('a').intValue() == 1
+        node.get('b') instanceof ArrayNode
+        (node.get('b') as ArrayNode).size() == 2
+        (node.get('b') as ArrayNode).get(0).textValue() == 'x'
+        (node.get('b') as ArrayNode).get(1).textValue() == 'y'
+
+
+        when:
+        node = DxHelper.mapToJsonNode( [a:1, b: [ [x:1], [y:2, z:3] ] ] )
+        then:
+        node.get('a').intValue() == 1
+        node.get('b') instanceof ArrayNode
+        (node.get('b') as ArrayNode).size() == 2
+        (node.get('b') as ArrayNode).get(0).get('x').intValue() == 1
+
+
+
+
     }
 
 
