3975004b27c8de4ce3fe79393b8ce6e993832507
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2013-10-25 06:49:24 +0200

    Moved java sources to separate folder and enable joint compile

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -67,7 +67,7 @@ class Const {
     /**
      * The app build number
      */
-    static final int APP_BUILDNUM = 1109
+    static final int APP_BUILDNUM = 1138
 
     /**
      * The date time formatter string
