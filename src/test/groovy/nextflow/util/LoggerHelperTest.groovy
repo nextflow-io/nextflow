43d427ebb595110b5778846063803cfb67f14087
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-02-20 15:45:24 +0100

    Fixed tests

---
+++ src/test/groovy/nextflow/util/LoggerHelperTest.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012, the authors.
+ *
+ *   This file is part of 'Nextflow'.
+ *
+ *   Nextflow is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Nextflow is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nextflow.util
+
+import spock.lang.Specification
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class LoggerHelperTest extends Specification {
+
+    def testErrorLine() {
+
+        expect:
+        LoggerHelper.getErrorLine('at pfam3d.run(pfam3d.nf:189) ~[na:na]') == 'pfam3d.nf:189'
+        LoggerHelper.getErrorLine('at pfam3d.run(pfam3d.nf:189) ~[na:na]','pfam3d') == 'pfam3d.nf:189'
+        LoggerHelper.getErrorLine('at pfam3d.run(pfam3d.nf:189) ~[na:na]','hola') == null
+    }
+}
