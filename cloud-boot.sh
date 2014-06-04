aaa3e75fc4c7be08401a782b9f5d40442073f80a
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-06-04 15:54:59 +0200

    Create cloud-boot.sh

---
+++ cloud-boot.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+set -x 
+
+#
+# verify Amazon auth codes are availables
+#
+[[ -z 'AWS_ACCESS_KEY' ]] && echo 'Missing $AWS_ACCESS_KEY variable'; exit 1
+[[ -z 'AWS_SECRET_KEY' ]] && echo 'Missing AWS_SECRET_KEY variable'; exit 1
+
+#
+# Lauch docker and pull the container when DOCKER variable is defined
+#
+if [ -z "DOCKER" ]; then
+    docker pull $DOCKER
+fi
+
+# the bucket name
+AWS_S3BUCKET=${AWS_S3BUCKET:-'nxf-cluster'}
+
+#
+# Install NEXTFLOW and launch it
+#
+wget -q -O - http://get.nextflow.io > nextflow && chmod +x nextflow
+./nextflow -bg \
+  -daemon.name gridgain \
+  -daemon.interface eth0 \
+  -daemon.join s3:$AWS_S3BUCKET
+
+# save the environment for debugging 
+env | sort > .boot.env
+
+
