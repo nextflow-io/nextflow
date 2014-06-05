575a904fb927600f0211329527f63432ada27acc
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-06-05 16:36:44 +0200

    Update cloud-boot.sh

--- cloud-boot.sh
+++ cloud-boot.sh
@@ -6,7 +6,13 @@ set -x
 # verify Amazon auth codes are availables
 #
 [[ -z 'AWS_ACCESS_KEY' ]] && { echo 'Missing $AWS_ACCESS_KEY variable'; exit 1; }
-[[ -z 'AWS_SECRET_KEY' ]] && { echo 'Missing AWS_SECRET_KEY variable'; exit 2; }
+[[ -z 'AWS_SECRET_KEY' ]] && { echo 'Missing $AWS_SECRET_KEY variable'; exit 2; }
+
+mkdir -p $HOME/bin
+cat <<EOF >> $HOME/.bash_profile
+export $AWS_ACCESS_KEY=$AWS_ACCESS_KEY
+export $AWS_SECRET_KEY=$AWS_SECRET_KEY
+EOF
 
 #
 # Lauch docker and pull the container when DOCKER variable is defined
@@ -21,8 +27,9 @@ AWS_S3BUCKET=${AWS_S3BUCKET:-'nxf-cluster'}
 #
 # Install NEXTFLOW and launch it
 #
-curl -fsSL http://get.nextflow.io > nextflow && chmod +x nextflow
-./nextflow -bg \
+curl -fsSL http://get.nextflow.io > $HOME/bin/nextflow 
+chmod +x $HOME/bin/nextflow
+$HOME/bin/nextflow -bg \
   -daemon.name gridgain \
   -daemon.interface eth0 \
   -daemon.join s3:$AWS_S3BUCKET
