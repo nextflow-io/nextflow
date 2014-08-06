c76ea0f36d06894a1cca706ceedfcc8a56999cfd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-08-06 12:21:14 +0200

    Added test

--- src/test/groovy/nextflow/cli/LauncherTest.groovy
+++ src/test/groovy/nextflow/cli/LauncherTest.groovy
@@ -19,7 +19,7 @@
  */
 
 package nextflow.cli
-import com.beust.jcommander.ParameterException
+
 import spock.lang.Specification
 /**
  *
@@ -97,17 +97,47 @@ class LauncherTest extends Specification {
     def testPull() {
 
         when:
-        def launcher = new Launcher('pull').parseMainArgs()
-        then:
-        thrown(ParameterException)
-
-        when:
-        launcher = new Launcher('pull','xxx').parseMainArgs()
+        def launcher = new Launcher('pull','xxx').parseMainArgs()
         then:
         launcher.command instanceof CmdPull
         launcher.command.args == ['xxx']
 
     }
+
+
+    def testNormalizeCmdline () {
+
+        expect:
+        Launcher.normalizeArgs('a','-bb','-ccc','dddd') == ['a','-bb','-ccc','dddd']
+        Launcher.normalizeArgs('a','-bb','-ccc','-resume', 'last') == ['a','-bb','-ccc','-resume','last']
+        Launcher.normalizeArgs('a','-bb','-ccc','-resume') == ['a','-bb','-ccc','-resume','last']
+        Launcher.normalizeArgs('a','-bb','-ccc','-resume','1d2c942a-345d-420b-b7c7-18d90afc6c33', 'zzz') == ['a','-bb','-ccc','-resume','1d2c942a-345d-420b-b7c7-18d90afc6c33', 'zzz']
+
+        Launcher.normalizeArgs('x','-test') == ['x','-test','%all']
+        Launcher.normalizeArgs('x','-test','alpha') == ['x','-test','alpha']
+        Launcher.normalizeArgs('x','-test','-other') == ['x','-test','%all','-other']
+
+        Launcher.normalizeArgs('--alpha=1') == ['--alpha=1']
+        Launcher.normalizeArgs('--alpha','1') == ['--alpha=1']
+        Launcher.normalizeArgs('-x', '1', 'script.nf', '--long', 'v1', '--more', 'v2', '--flag') == ['-x','1','script.nf','--long=v1','--more=v2','--flag=true']
+
+        Launcher.normalizeArgs('-x', '1', '-process.alpha','2', '3') == ['-x', '1', '-process.alpha=2', '3']
+        Launcher.normalizeArgs('-x', '1', '-process.echo') == ['-x', '1', '-process.echo=true']
+
+        Launcher.normalizeArgs('-x', '1', '-daemon.alpha','2', '3') == ['-x', '1', '-daemon.alpha=2', '3']
+        Launcher.normalizeArgs('-x', '1', '-daemon.echo') == ['-x', '1', '-daemon.echo=true']
+
+        Launcher.normalizeArgs('-x', '1', '-executor.alpha','2', '3') == ['-x', '1', '-executor.alpha=2', '3']
+        Launcher.normalizeArgs('-x', '1', '-executor.echo') == ['-x', '1', '-executor.echo=true']
+
+        Launcher.normalizeArgs('-x', '1', '-that.alpha','2', '3') == ['-x', '1', '-that.alpha','2', '3']
+
+        Launcher.normalizeArgs('run', 'file-name', '-a', '-b') == ['run','file-name', '-a', '-b']
+        Launcher.normalizeArgs('run', '-', '-a', '-b') == ['run','-stdin', '-a', '-b']
+        Launcher.normalizeArgs('run') == ['run']
+
+    }
+
 
 
 }
