92c42bf4fbed70c530091d5905413addee8383dc
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-09-28 14:02:43 +0200

    Fixed sync doc script

--- docs/sync-doc.sh
+++ docs/sync-doc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TARGET=../../nextflow-web/assets
+TARGET=../../nextflow-website/assets
 LATEST=$TARGET/docs/latest/
 VERSION=$TARGET/docs/v0.10.2/
 
@@ -9,4 +9,4 @@ mkdir -p $TARGET/docs/v0.10.2/
 rsync -r _build/html/* $LATEST
 rsync -r _build/html/* $VERSION
 
-( cd ../../nextflow-web; jbake)
+( cd ../../nextflow-website; jbake)
