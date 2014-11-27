fbdca3723e7357ad081ff286bc19847b6f37895c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-11-27 11:01:36 +0100

    Upgraded version number and build timestamp

--- docs/sync-doc.sh
+++ docs/sync-doc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TARGET=../../nextflow-website/assets
 LATEST=$TARGET/docs/latest/
-VERSION=$TARGET/docs/v0.11.2/
+VERSION=$TARGET/docs/v0.11.3/
 
 mkdir -p $TARGET/docs/latest/
-mkdir -p $TARGET/docs/v0.11.2/
+mkdir -p $TARGET/docs/v0.11.3/
 
 rsync -r _build/html/* $LATEST
 rsync -r _build/html/* $VERSION
