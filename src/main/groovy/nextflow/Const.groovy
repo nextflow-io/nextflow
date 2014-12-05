7d5f0b68ee91b3fe4c66e695bad8308645ad032f
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-12-05 15:59:27 +0100

    Updated changelog and build numbers

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -53,12 +53,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static final long APP_TIMESTAMP = 1417527442355
+    static final long APP_TIMESTAMP = 1417710165359
 
     /**
      * The app build number
      */
-    static final int APP_BUILDNUM = 2491
+    static final int APP_BUILDNUM = 2495
 
     /**
      * The date time formatter string
