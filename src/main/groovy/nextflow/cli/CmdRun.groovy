58eab0da8a040fa51f613534096e6fca622faa74
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-12-23 22:51:13 +0100

    Print pid/hostname in the status log

--- src/main/groovy/nextflow/cli/CmdRun.groovy
+++ src/main/groovy/nextflow/cli/CmdRun.groovy
@@ -169,7 +169,8 @@ class CmdRun extends CmdBase implements HubOptions {
             runner.test(this.test, scriptArgs )
         }
         else {
-            log.debug( '\n'+CmdInfo.getInfo( log.isTraceEnabled() ) )
+            def info = CmdInfo.status( log.isTraceEnabled() )
+            log.debug( '\n'+info )
 
             // -- add this run to the local history
             trackHistory(runner.session.uniqueId)
