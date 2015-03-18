477c58b5cb905a3be99024235daa26150abfc931
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-03-18 10:27:35 +0100

    [release v0.12.5] updating build and version numbers

--- docs/sync-doc.sh
+++ docs/sync-doc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TARGET=../../nextflow-website/assets
 LATEST=$TARGET/docs/latest/
-VERSION=$TARGET/docs/v0.12.4/
+VERSION=$TARGET/docs/v0.12.5/
 
 mkdir -p $TARGET/docs/latest/
-mkdir -p $TARGET/docs/v0.12.4/
+mkdir -p $TARGET/docs/v0.12.5/
 
 rsync -r _build/html/* $LATEST
 rsync -r _build/html/* $VERSION
