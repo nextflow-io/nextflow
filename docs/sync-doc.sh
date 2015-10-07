7dfe9bf14864b6842bb3748eed35c6cf1875d199
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-10-07 15:07:21 +0200

    [release 0.15.6] Updating changelog, build and version numbers

--- docs/sync-doc.sh
+++ docs/sync-doc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TARGET=../../nextflow-website/assets
 LATEST=$TARGET/docs/latest/
-VERSION=$TARGET/docs/v0.15.5/
+VERSION=$TARGET/docs/v0.15.6/
 
 mkdir -p $TARGET/docs/latest/
-mkdir -p $TARGET/docs/v0.15.5/
+mkdir -p $TARGET/docs/v0.15.6/
 
 rsync -r _build/html/* $LATEST
 rsync -r _build/html/* $VERSION
--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -48,17 +48,17 @@ class Const {
     /**
      * The application version
      */
-    static final String APP_VER = "0.15.5"
+    static final String APP_VER = "0.15.6"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static final long APP_TIMESTAMP = 1443430527543
+    static final long APP_TIMESTAMP = 1444223130020
 
     /**
      * The app build number
      */
-    static final int APP_BUILDNUM = 3111
+    static final int APP_BUILDNUM = 3112
 
     /**
      * The date time formatter string
