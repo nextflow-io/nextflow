2d4ab36e4bf76c0a2a5d89af0c80f29dd30ae8ab
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-10-21 15:20:31 +0200

    Publish documentation only to `latest` folder

--- docs/sync-doc.sh
+++ docs/sync-doc.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 TARGET=../../nextflow-website/assets
 LATEST=$TARGET/docs/latest/
-VERSION=$TARGET/docs/v0.15.4/
 
 mkdir -p $TARGET/docs/latest/
-mkdir -p $TARGET/docs/v0.15.4/
 
 rsync -r _build/html/* $LATEST
-rsync -r _build/html/* $VERSION
 
 ( cd ../../nextflow-website; jbake)
