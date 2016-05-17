c8b7dec0327731137e1eea8373920e90f694b81c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-05-17 12:27:18 +0200

    Fixed typos

--- src/main/groovy/nextflow/scm/RepositoryProvider.groovy
+++ src/main/groovy/nextflow/scm/RepositoryProvider.groovy
@@ -207,7 +207,7 @@ abstract class RepositoryProvider {
                 invokeAndParseResponse( getEndpointUrl() )
             }
             catch( IOException e2 ) {
-                throw new AbortOperationException("Cannot find '$project' -- Make sure exists a ${name.capitalize()} repository at this address ${getRepositoryUrl()}")
+                throw new AbortOperationException("Cannot find `$project` -- Make sure exists a ${name.capitalize()} repository at this address `${getRepositoryUrl()}`")
             }
 
             throw new AbortOperationException("Not a valid Nextflow project -- The repository `${getRepositoryUrl()}` must contain a the script `${AssetManager.DEFAULT_MAIN_FILE_NAME}` or the file `${AssetManager.MANIFEST_FILE_NAME}`")
