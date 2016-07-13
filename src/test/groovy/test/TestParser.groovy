b9f6c1840d1f225cff58830c1ab0ca088e474bd0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-07-13 21:32:11 +0200

    Refactored ParallelTaskProcessor and TaskProcessor classes to a single class

--- src/test/groovy/test/TestParser.groovy
+++ src/test/groovy/test/TestParser.groovy
@@ -25,10 +25,8 @@ import groovy.transform.InheritConstructors
 import nextflow.Channel
 import nextflow.Nextflow
 import nextflow.Session
-
 import nextflow.ast.NextflowDSL
 import nextflow.executor.Executor
-import nextflow.processor.ParallelTaskProcessor
 import nextflow.processor.ProcessConfig
 import nextflow.processor.ProcessFactory
 import nextflow.processor.TaskProcessor
@@ -109,7 +107,7 @@ class TestParser {
     }
 
     @InheritConstructors
-    static class MockTaskProcessor extends ParallelTaskProcessor {
+    static class MockTaskProcessor extends TaskProcessor {
         @Override
         def run () { }
     }
