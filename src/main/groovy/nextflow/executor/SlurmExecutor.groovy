7f458b795dc668236d6a024bc3a1912ae18b27dd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-11-03 15:27:36 +0100

    Added `—no-requeue` flag to SLURM jobs

--- src/main/groovy/nextflow/executor/SlurmExecutor.groovy
+++ src/main/groovy/nextflow/executor/SlurmExecutor.groovy
@@ -46,6 +46,7 @@ class SlurmExecutor extends AbstractGridExecutor {
         result << '-D' << quote(task.workDir)
         result << '-J' << getJobNameFor(task)
         result << '-o' << quote(task.workDir.resolve(TaskRun.CMD_LOG))     // -o OUTFILE and no -e option => stdout and stderr merged to stdout/OUTFILE
+        result << '--no-requeue' << '' // note: directive need to be returned as pairs
 
         if( task.config.cpus > 1 ) {
             result << '-c' << task.config.cpus.toString()
