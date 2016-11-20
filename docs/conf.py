961edd4212e43d61fe0146528ef751b6c2ddf2d0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-11-20 11:45:01 +0100

    Updated docs sphinx_rtd_theme to version 0.1.9 [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,9 @@ html_theme_path = ["_themes"]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+  'display_version': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
