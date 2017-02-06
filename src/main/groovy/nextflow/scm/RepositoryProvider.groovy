1283686c616017491931db0b688eb826d68eef89
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-02-06 18:18:38 +0100

    Updated copyright info

--- src/main/groovy/nextflow/scm/RepositoryProvider.groovy
+++ src/main/groovy/nextflow/scm/RepositoryProvider.groovy
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2013-2016, Centre for Genomic Regulation (CRG).
- * Copyright (c) 2013-2016, Paolo Di Tommaso and the respective authors.
+ * Copyright (c) 2013-2017, Centre for Genomic Regulation (CRG).
+ * Copyright (c) 2013-2017, Paolo Di Tommaso and the respective authors.
  *
  *   This file is part of 'Nextflow'.
  *
