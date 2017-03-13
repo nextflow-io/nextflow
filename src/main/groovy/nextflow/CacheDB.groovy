05ec4fd218e17819f9d72867c3dd609f85b757b4
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-03-13 14:59:28 +0100

    Added exception cause when DB file cannot be opened

--- src/main/groovy/nextflow/CacheDB.groovy
+++ src/main/groovy/nextflow/CacheDB.groovy
@@ -100,7 +100,7 @@ class CacheDB implements Closeable {
             db = Iq80DBFactory.factory.open(file, new Options().createIfMissing(true))
         }
         catch( Exception e ) {
-            throw new IOException("Can't create cache DB: $file")
+            throw new IOException("Can't create cache DB: $file", e)
         }
     }
 
