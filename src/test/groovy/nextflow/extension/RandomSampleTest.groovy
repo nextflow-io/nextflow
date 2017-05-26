ae1a8f4bd9fb66ff44221db06a2a7dcd459476e3
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-05-26 21:20:29 +0200

    Fixed tests #344

--- src/test/groovy/nextflow/extension/RandomSampleTest.groovy
+++ src/test/groovy/nextflow/extension/RandomSampleTest.groovy
@@ -17,59 +17,44 @@ class RandomSampleTest extends Specification {
     def 'should produce random sample' () {
 
         given:
-        def ch = Channel.from(0,1,2,3,4,5,6,7,8,9)
-        def sampler = new RandomSampleOp(ch, 5)
+        def ch = Channel.from('A'..'Z')
+        def sampler = new RandomSampleOp(ch, 10)
 
         when:
         def result = (List)sampler.apply().toList().val
         then:
-        result.size() == 5
-        result.unique().size() == 5 
-        result != [0,1,2,3,4]
-        result[0] in 0..9
-        result[1] in 0..9
-        result[2] in 0..9
-        result[3] in 0..9
-        result[4] in 0..9
+        result.size() == 10
+        result.unique().size() == 10
+        result != 'A'..'J'
     }
 
 
     def 'should produce random sample given a short channel' () {
 
         given:
-        def ch = Channel.from(0,1,2,3,4)
-        def sampler = new RandomSampleOp(ch, 10)
+        def ch = Channel.from('A'..'J')
+        def sampler = new RandomSampleOp(ch, 20)
 
         when:
         def result = (List)sampler.apply().toList().val
         then:
-        result.size() == 5
-        result.unique().size() == 5
-        result != [0,1,2,3,4]
-        result[0] in 0..9
-        result[1] in 0..9
-        result[2] in 0..9
-        result[3] in 0..9
-        result[4] in 0..9
+        result.size() == 10
+        result.unique().size() == 10
+        result != 'A'..'J'
     }
 
     def 'should produce random sample given a channel emitting the same number of items as the buffer' () {
 
         given:
-        def ch = Channel.from(0,1,2,3,4)
-        def sampler = new RandomSampleOp(ch, 5)
+        def ch = Channel.from('A'..'J')
+        def sampler = new RandomSampleOp(ch, 10)
 
         when:
         def result = (List)sampler.apply().toList().val
         then:
-        result.size() == 5
-        result.unique().size() == 5
-        result != [0,1,2,3,4]
-        result[0] in 0..9
-        result[1] in 0..9
-        result[2] in 0..9
-        result[3] in 0..9
-        result[4] in 0..9
+        result.size() == 10
+        result.unique().size() == 10
+        result != 'A'..'J'
     }
 
 }
