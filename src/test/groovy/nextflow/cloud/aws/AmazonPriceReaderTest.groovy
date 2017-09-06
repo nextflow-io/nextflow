bdf3a1a308fd7128ba49ab688531d37abde7dc98
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-09-06 19:08:32 +0200

    Fixed Unable to parse the AWS price list API #444

--- src/test/groovy/nextflow/cloud/aws/AmazonPriceReaderTest.groovy
+++ src/test/groovy/nextflow/cloud/aws/AmazonPriceReaderTest.groovy
@@ -79,15 +79,23 @@ class AmazonPriceReaderTest extends Specification {
         def parser = new AmazonPriceReader('eu-west-1')
 
         when:
-        def fields = parser.parseCsvLine('"alpha","beta",,,"delt\"a",')
+        def fields = parser.parseCsvLine('"alpha","beta",,,"delta",')
         then:
         fields[0] == 'alpha'
         fields[1] == 'beta'
         fields[2] == null
         fields[3] == null
-        fields[4] == 'delt\"a'
+        fields[4] == 'delta'
         fields[5] == null
         fields.size() == 6
+
+        when:
+        fields = parser.parseCsvLine('"alpha",beta,"delta"')
+        then:
+        fields[0] == 'alpha'
+        fields[1] == 'beta'
+        fields[2] == 'delta'
+        fields.size() == 3
 
         when:
         def line = '"6QB2UH376KYJD6MB","6QCMYABX3D","6QB2UH376KYJD6MB.6QCMYABX3D.6YS6EN2CT7","Reserved","USD 0.0 per Red Hat Enterprise Linux (Amazon VPC), t2.medium instance-hour (or partial hour)","2015-04-30","0","Inf","Hrs","0.0000000000","USD","1yr","All Upfront","standard","Compute Instance","AmazonEC2","Asia Pacific (Sydney)","AWS Region","t2.medium",,"General purpose","2","Intel Xeon Family","Up to 3.3 GHz","4 GiB","EBS only","Low to Moderate","32-bit or 64-bit",,,,,,,,"Shared",,"RHEL","No License required",,,,,,,,"APS2-BoxUsage:t2.medium","RunInstances:0010",,,,,,,,,,,,,,,"NA","Intel AVX; Intel Turbo",'
@@ -95,14 +103,40 @@ class AmazonPriceReaderTest extends Specification {
         then:
         result.size()==65
         result[0] == '6QB2UH376KYJD6MB'
-        result[AmazonPriceReader.colPricePerUnit] == '0.0000000000'
-        result[AmazonPriceReader.colCurrency] == 'USD'
-        result[AmazonPriceReader.colProdFamily] == 'Compute Instance'
-        result[AmazonPriceReader.colServiceCode] == 'AmazonEC2'
-        result[AmazonPriceReader.colInstanceType] == 't2.medium'
-        result[AmazonPriceReader.colCpu] == '2'
-        result[AmazonPriceReader.colMem] == '4 GiB'
-        result[AmazonPriceReader.colLocation]  == "Asia Pacific (Sydney)"
+        result[AmazonPriceReader.COL_CURRENCY] == 'USD'
+        result[AmazonPriceReader.COL_PRODFAMILY] == 'Compute Instance'
+        result[AmazonPriceReader.COL_SERVICECODE] == 'AmazonEC2'
+        result[AmazonPriceReader.COL_INSTANCETYPE] == 't2.medium'
+        result[AmazonPriceReader.COL_CPU] == '2'
+        result[AmazonPriceReader.COL_MEM] == '4 GiB'
+        result[AmazonPriceReader.COL_LOCATION]  == "Asia Pacific (Sydney)"
+
+        when:
+        line = '6S5B59JFU3B2M3XM,4NA7Y494T4,6S5B59JFU3B2M3XM.4NA7Y494T4.6YS6EN2CT7,Reserved,"Red Hat Enterprise Linux (Amazon VPC), x1.16xlarge reserved instance applied",2017-04-30,0,Inf,Hrs,5.0620000000,USD,1yr,No Upfront,standard,Compute Instance,AmazonEC2,EU (Ireland),AWS Region,x1.16xlarge,Yes,Memory optimized,64,High Frequency Intel Xeon E7-8880 v3 (Haswell),2.3 GHz,976 Gib,"1 x 1,920",High,64-bit,,,,,,,,Shared,RHEL,No License required,,,,,,,,EU-BoxUsage:x1.16xlarge,RunInstances:0010,,,124.5,Yes,,,,,,,,,,,Yes,Yes,Yes,128,,NA,,Amazon Elastic Compute Cloud'
+        result = parser.parseCsvLine(line)
+        then:
+        result[0] == '6S5B59JFU3B2M3XM'
+        result[AmazonPriceReader.COL_CURRENCY] == 'USD'
+        result[AmazonPriceReader.COL_PRODFAMILY] == 'Compute Instance'
+        result[AmazonPriceReader.COL_SERVICECODE] == 'AmazonEC2'
+        result[AmazonPriceReader.COL_INSTANCETYPE] == 'x1.16xlarge'
+        result[AmazonPriceReader.COL_CPU] == '64'
+        result[AmazonPriceReader.COL_MEM] == '976 Gib'
+
+        when:
+        line = '"QDG74QMT7ZXABFC9","JRTCKXETXF","QDG74QMT7ZXABFC9.JRTCKXETXF.6YS6EN2CT7","OnDemand","$0.000 per Linux m4.large Dedicated Host Instance hour","2017-08-01","0","Inf","Hrs","0.0000000000","USD",,,,"Compute Instance","AmazonEC2","EU (Ireland)","AWS Region","m4.large","Yes","General purpose","2","Intel Xeon E5-2676 v3 (Haswell)","2.4 GHz","8 GiB","EBS only","Moderate","64-bit",,,,,,,,"Host","Linux","No License required",,,,,,,,"EU-HostBoxUsage:m4.large","RunInstances",,"450 Mbps","6.5","Yes",,,,,,,,,,,,,,"4",,"NA","Intel AVX; Intel AVX2; Intel Turbo","Amazon Elastic Compute Cloud"'
+        result = parser.parseCsvLine(line)
+        then:
+        result[0] == 'QDG74QMT7ZXABFC9'
+        result[AmazonPriceReader.COL_CURRENCY] == 'USD'
+        result[AmazonPriceReader.COL_PRODFAMILY] == 'Compute Instance'
+        result[AmazonPriceReader.COL_SERVICECODE] == 'AmazonEC2'
+        result[AmazonPriceReader.COL_INSTANCETYPE] == 'm4.large'
+        result[AmazonPriceReader.COL_CPU] == '2'
+        result[AmazonPriceReader.COL_MEM] == '8 GiB'
+        result[AmazonPriceReader.COL_OS] == 'Linux'
+        result[AmazonPriceReader.COL_LOCATION]  == "EU (Ireland)"
+
 
     }
 
