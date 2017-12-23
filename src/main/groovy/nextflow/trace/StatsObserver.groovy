f22dcbf974863ec1ea928f79926e575f1b93a253
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-12-23 18:12:03 +0100

    Added Stats/ReportObserver tests

--- src/main/groovy/nextflow/trace/StatsObserver.groovy
+++ src/main/groovy/nextflow/trace/StatsObserver.groovy
@@ -1,5 +1,6 @@
 package nextflow.trace
-
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import nextflow.Session
 import nextflow.processor.ErrorStrategy
 import nextflow.processor.TaskHandler
@@ -9,22 +10,13 @@ import nextflow.processor.TaskProcessor
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@Slf4j
+@CompileStatic
 class StatsObserver implements TraceObserver {
 
     private WorkflowStats stats = new WorkflowStats()
 
     WorkflowStats getStats() { stats }
-
-
-    protected long getCpuTime( TraceRecord record ) {
-        // note: `realtime` field is not available when task metrics are not enabled
-        if( !record.containsKey('realtime') )
-            return 0
-        final time = (long)record.get('realtime')
-        final cpus = (long)record.get('cpus')
-        return time * cpus / 1000 as long
-    }
-
 
     @Override
     void onFlowStart(Session session) {
@@ -54,36 +46,76 @@ class StatsObserver implements TraceObserver {
     @Override
     void onProcessComplete(TaskHandler handler) {
         final record = handler.getTraceRecord()
-        synchronized( stats ) {
-            if( record.get('status') == 'COMPLETED' ) {
-                stats.succeed++
-                stats.timeSucceed += getCpuTime(record)
-            }
-            else {
-                stats.timeFailed += getCpuTime(record)
-
-                final action = record.get('error_action')
-                if( action == ErrorStrategy.IGNORE.toString() ) {
-                    stats.ignored++
-                }
-                else {
-                    stats.failed++
-                }
-            }
+        if( !record ) {
+            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
+            return
         }
+
+        updateTasksCompleted(record)
     }
 
     @Override
     void onProcessCached(TaskHandler handler) {
         final record = handler.getTraceRecord()
-        synchronized (stats) {
-            stats.timeCached += getCpuTime(record)
-            stats.cached++
+        if( !record ) {
+            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
+            return
         }
+
+        updateTasksCached(record)
     }
 
     @Override
     boolean enableMetrics() {
         return false
     }
+
+    /**
+     * Update succeed and failed task stats
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     */
+    protected synchronized void updateTasksCompleted(TraceRecord record) {
+        if( record.get('status') == 'COMPLETED' ) {
+            stats.succeed++
+            stats.timeSucceed += getCpuTime(record)
+        }
+        else {
+            stats.timeFailed += getCpuTime(record)
+
+            final action = record.get('error_action')
+            if( action == ErrorStrategy.IGNORE.toString() ) {
+                stats.ignored++
+            }
+            else {
+                stats.failed++
+            }
+        }
+    }
+
+    /**
+     * Update cached task stats
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     */
+    protected synchronized void updateTasksCached(TraceRecord record) {
+        stats.timeCached += getCpuTime(record)
+        stats.cached++
+    }
+
+    /**
+     * Computes the CPU-seconds used by the task
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     * @return The CPU-seconds used by the specified task
+     */
+    protected long getCpuTime( TraceRecord record ) {
+        // note: `realtime` field is not available when task metrics are not enabled
+        if( !record.containsKey('realtime') )
+            return 0
+        final time = (long)record.get('realtime')
+        final cpus = (long)record.get('cpus')
+        return time * cpus / 1000 as long
+    }
+
 }
---
+++ src/test/groovy/nextflow/trace/StatsObserverTest.groovy
@@ -0,0 +1,8 @@
+package nextflow.trace
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class StatsObserverTest {
+}
