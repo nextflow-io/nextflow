b8a439e308ccd364896fc5f3881143f2ebe2e5ba
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-12-23 20:59:58 +0100

    Refactored workflow stats #534

--- src/main/groovy/nextflow/trace/StatsObserver.groovy
+++ src/main/groovy/nextflow/trace/StatsObserver.groovy
@@ -2,7 +2,6 @@ package nextflow.trace
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import nextflow.Session
-import nextflow.processor.ErrorStrategy
 import nextflow.processor.TaskHandler
 import nextflow.processor.TaskProcessor
 /**
@@ -25,7 +24,7 @@ class StatsObserver implements TraceObserver {
 
     @Override
     void onFlowComplete() {
-
+        log.debug "Workflow completed > $stats"
     }
 
     @Override
@@ -46,76 +45,28 @@ class StatsObserver implements TraceObserver {
     @Override
     void onProcessComplete(TaskHandler handler) {
         final record = handler.getTraceRecord()
-        if( !record ) {
-            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
-            return
+        if( record ) {
+            stats.updateTasksCompleted(record)
         }
-
-        updateTasksCompleted(record)
+        else {
+            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
+        }
     }
 
     @Override
     void onProcessCached(TaskHandler handler) {
         final record = handler.getTraceRecord()
-        if( !record ) {
-            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
-            return
+        if( record ) {
+            stats.updateTasksCached(record)
         }
-
-        updateTasksCached(record)
+        else {
+            log.debug "WARN: Unable to find trace record for task id=${handler.task?.id}"
+        }
     }
 
     @Override
     boolean enableMetrics() {
         return false
-    }
-
-    /**
-     * Update succeed and failed task stats
-     *
-     * @param record The {@link TraceRecord} object representing the task metrics
-     */
-    protected synchronized void updateTasksCompleted(TraceRecord record) {
-        if( record.get('status') == 'COMPLETED' ) {
-            stats.succeed++
-            stats.timeSucceed += getCpuTime(record)
-        }
-        else {
-            stats.timeFailed += getCpuTime(record)
-
-            final action = record.get('error_action')
-            if( action == ErrorStrategy.IGNORE.toString() ) {
-                stats.ignored++
-            }
-            else {
-                stats.failed++
-            }
-        }
-    }
-
-    /**
-     * Update cached task stats
-     *
-     * @param record The {@link TraceRecord} object representing the task metrics
-     */
-    protected synchronized void updateTasksCached(TraceRecord record) {
-        stats.timeCached += getCpuTime(record)
-        stats.cached++
-    }
-
-    /**
-     * Computes the CPU-seconds used by the task
-     *
-     * @param record The {@link TraceRecord} object representing the task metrics
-     * @return The CPU-seconds used by the specified task
-     */
-    protected long getCpuTime( TraceRecord record ) {
-        // note: `realtime` field is not available when task metrics are not enabled
-        if( !record.containsKey('realtime') )
-            return 0
-        final time = (long)record.get('realtime')
-        final cpus = (long)record.get('cpus')
-        return time * cpus / 1000 as long
     }
 
 }
--- src/main/groovy/nextflow/trace/WorkflowStats.groovy
+++ src/main/groovy/nextflow/trace/WorkflowStats.groovy
@@ -1,71 +1,159 @@
 package nextflow.trace
-
 import java.text.DecimalFormat
+
+import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.PackageScope
+import groovy.util.logging.Slf4j
+import nextflow.processor.ErrorStrategy
+import nextflow.util.Duration
 
 /**
  * Value object representing the workflow execution statistics
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@Slf4j
+@EqualsAndHashCode
+@CompileStatic
 class WorkflowStats {
 
-    /**
-     * Overall workflow compute time (CPUs-seconds) for task executed successfully
-     */
-    long timeSucceed
+    private long succeedMillis
+
+    private long cachedMillis
+
+    private long failedMillis
+
+    private int succeedCount
+
+    private int cachedCount
+
+    private int failedCount
+
+    private int ignoredCount
 
     /**
-     * Overall compute time (CPUs-seconds) for cached tasks
+     * @return Overall workflow compute time (CPUs-seconds) for task executed successfully
      */
-    long timeCached
+    Duration getSucceedDuration() { new Duration(succeedMillis) }
 
     /**
-     * Overall compute time (CPUs-seconds) for failed tasks
+     * @return Overall workflow compute time (CPUs-seconds) for failed tasks
      */
-    long timeFailed
+    Duration getFailedDuration() { new Duration(failedMillis) }
 
     /**
-     * Task successfully completed
+     * @return Overall workflow compute time (CPUs-seconds) for cached tasks
      */
-    long succeed
+    Duration getCachedDuration() { new Duration(cachedMillis) }
 
     /**
-     * Task cached
+     * @return Succeed tasks count
      */
-    long cached
+    int getSucceedCount() { succeedCount }
 
     /**
-     * Task terminated with an error
+     * @return Failed tasks count
      */
-    long failed
+    int getFailedCount() { failedCount }
 
     /**
-     * Task executions terminated with an error which was ignored
+     * @return Ignored tasks count
      */
-    long ignored
+    int getIgnoredCount() { ignoredCount }
+
+    /**
+     * @return Cached tasks count
+     */
+    int getCachedCount() { cachedCount }
 
     /**
      * @return A formatted string representing the overall execution time as CPU-Hours
      */
-    protected String getComputeTime() {
+    String getComputeTimeString() {
 
         def fmt = new DecimalFormat("0.#")
-        def total = (timeSucceed + timeCached + timeFailed)
+        def total = (succeedMillis + cachedMillis + failedMillis) / 1000
         if( total < 180 )
             return '(a few seconds)'
 
         def result = String.format('%.1f', total/3600)
-        if( timeCached || timeFailed ) {
+        if( cachedMillis || failedMillis ) {
             result += ' ('
             def items = []
-            if( timeCached )
-                items << fmt.format(timeCached/total*100) + '% cached'
-            if( timeFailed )
-                items << fmt.format(timeFailed/total*100) + '% failed'
+            if( cachedMillis ) {
+                items << fmt.format(cachedMillis/10/total) + '% cached'
+            }
+            if( failedMillis ) {
+                items << fmt.format(failedMillis/10/total) + '% failed'
+            }
             result += items.join(', ')
             result += ')'
         }
 
         return result
+    }
+
+
+    /**
+     * Computes the CPU-millis used by the task
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     * @return The CPU-millis used by the specified task
+     */
+    protected long getCpuTime( TraceRecord record ) {
+        // note: `realtime` field is not available when task metrics are not enabled
+        if( !record.containsKey('realtime') )
+            return 0
+        final time = (long)record.get('realtime')
+        final cpus = (long)record.get('cpus')
+        return time * cpus
+    }
+
+    /**
+     * Update succeed and failed task stats
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     */
+    @PackageScope
+    synchronized void updateTasksCompleted(TraceRecord record) {
+        if( record.get('status') == 'COMPLETED' ) {
+            succeedCount++
+            succeedMillis += getCpuTime(record)
+        }
+        else {
+            failedMillis += getCpuTime(record)
+
+            final action = record.get('error_action')
+            if( action == ErrorStrategy.IGNORE.toString() ) {
+                ignoredCount++
+            }
+            else {
+                failedCount++
+            }
+        }
+    }
+
+    /**
+     * Update cached task stats
+     *
+     * @param record The {@link TraceRecord} object representing the task metrics
+     */
+    @PackageScope
+    synchronized void updateTasksCached(TraceRecord record) {
+        cachedMillis += getCpuTime(record)
+        cachedCount++
+    }
+
+    String toString() {
+        "WorkflowStats[" +
+                "succeedCount=${getSucceedCount()}; " +
+                "failedCount=${getFailedCount()}; " +
+                "ignoredCount=${getIgnoredCount()}; " +
+                "cachedCount=${getCachedCount()}; " +
+                "succeedDuration=${getSucceedDuration()}; " +
+                "failedDuration=${getFailedDuration()}; " +
+                "cachedDuration=${getCachedDuration()}" +
+                "]"
     }
 }
