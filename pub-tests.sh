4050d968932570f29b92943de544efb0f6f643b8
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-03-18 23:27:18 +0100

    Fixed tests report publish script

--- pub-tests.sh
+++ pub-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 #
 # Make test results available through
 #    https://www.nextflow.io/tests/travis/index.html
@@ -8,9 +8,13 @@ export AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
 export AWS_DEFAULT_REGION=eu-west-1
 
-aws --region eu-west-1 s3 sync $1 s3://www.nextflow.io/tests/$2/ \
+aws --region $AWS_DEFAULT_REGION s3 rm --quiet --recursive s3://www.nextflow.io/tests/$1
+
+for x in $(find . -path \*build/reports/tests/test); do
+aws --region $AWS_DEFAULT_REGION s3 sync $x s3://www.nextflow.io/tests/$1/${x#./} \
  --cache-control max-age=0 \
  --metadata-directive REPLACE \
  --storage-class REDUCED_REDUNDANCY \
  --acl public-read \
  --delete
+done
