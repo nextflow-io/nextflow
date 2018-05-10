46cb0c074f2a4ae5c19f77793727606f5e6dd0ca
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-05-10 09:53:43 +0200

    [release 0.29.1] update timestamp and build numbers

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -54,17 +54,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "0.29.0"
+    static public final String APP_VER = "0.29.1"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1524557352936
+    static public final long APP_TIMESTAMP = 1525938455402
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 4803
+    static public final int APP_BUILDNUM = 4804
 
 
     /**
