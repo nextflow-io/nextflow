f642fbe92c7d5a9d2ac9f0b6c899d9de8c1e0556
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-05-29 16:48:20 +0200

    Fixed Expose Kubernetes Secrets to worker pods (added missing classes) #651

---
+++ src/main/groovy/nextflow/k8s/model/PodHostMount.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013-2018, Centre for Genomic Regulation (CRG).
+ * Copyright (c) 2013-2018, Paolo Di Tommaso and the respective authors.
+ *
+ *   This file is part of 'Nextflow'.
+ *
+ *   Nextflow is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Nextflow is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nextflow.k8s.model
+
+import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+/**
+ * Model a K8s pod host mount defintion
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+@EqualsAndHashCode
+@ToString(includeNames = true)
+@CompileStatic
+class PodHostMount {
+
+    String hostPath
+
+    String mountPath
+
+    PodHostMount(String host, String container) {
+        this.hostPath = host
+        this.mountPath = container
+    }
+}
