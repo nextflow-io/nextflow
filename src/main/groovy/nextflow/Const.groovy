ba5c1bacdca910aba86bb478a47d107b3643cc29
Radoslaw Suchecki <r.suchecki@gmail.com>
2018-07-10 22:27:17 +0930

    revert commit of changes to auto generated file

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -59,12 +59,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1531207368095
+    static public final long APP_TIMESTAMP = 1529171346935
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 4873
+    static public final int APP_BUILDNUM = 4867
 
 
     /**
