640936ae3cd78b8e6353a20edab0133387be21a2
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-07-12 17:28:42 +0200

    Added prov subproject skeleton

---
+++ prov.sh
@@ -0,0 +1,2 @@
+MAIN_CLASS='nextflow.prov.Main'
+source launch.sh
---
+++ subprojects/nxf-prov/src/main/nextflow/prov/Main.groovy
@@ -0,0 +1,13 @@
+package nextflow.prov
+
+/**
+ * RO-Prov command entry point
+ * 
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class Main {
+
+    static void main(String... args) {
+        println "Hello prov!"
+    }
+}
