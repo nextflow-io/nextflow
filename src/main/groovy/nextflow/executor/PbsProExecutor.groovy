ed89c7e0e7b96580b8baf1998aadc41ea9887f77
Lorenz Gerber <gerberlo@gis.a-star.edu.sg>
2018-10-04 11:35:47 +0800

    adapt pbs pro syntax

--- src/main/groovy/nextflow/executor/PbsProExecutor.groovy
+++ src/main/groovy/nextflow/executor/PbsProExecutor.groovy
@@ -1,29 +1,9 @@
-/*
- * Copyright (c) 2013-2018, Centre for Genomic Regulation (CRG).
- * Copyright (c) 2013-2018, Paolo Di Tommaso and the respective authors.
- *
- *   This file is part of 'Nextflow'.
- *
- *   Nextflow is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   Nextflow is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package nextflow.executor
 
 import groovy.util.logging.Slf4j
 import nextflow.processor.TaskRun
 
-import java.nio.file.Path
+
 
 /**
  * Implements a executor for PBSPro cluster
@@ -31,7 +11,7 @@ import java.nio.file.Path
  * See http://www.pbspro.org
  */
 @Slf4j
-class PbsProExecutor extends AbstractGridExecutor {
+class PbsProExecutor extends PbsExecutor {
 
     /**
      * Gets the directives to submit the specified task to the cluster for execution
@@ -40,7 +20,8 @@ class PbsProExecutor extends AbstractGridExecutor {
      * @param result The {@link List} instance to which add the job directives
      * @return A {@link List} containing all directive tokens and values.
      */
-    protected List<String> getDirectives( TaskRun task, List<String> result ) {
+    @Override
+    protected List<String> getDirectives(TaskRun task, List<String> result ) {
         assert result !=null
 
         result << '-N' << getJobNameFor(task)
@@ -71,95 +52,9 @@ class PbsProExecutor extends AbstractGridExecutor {
     }
 
     @Override
-    String getHeaders( TaskRun task ) {
-        String result = super.getHeaders(task)
-        result += "cd ${quote(task.workDir)}\n"
-        return result
-    }
-
-    @Override
-    String getJobNameFor( TaskRun task ) {
-        def result = super.getJobNameFor(task)
-        // some implementations do not allow parenthesis in the job name -- see #271
-        result = result.replace('(','').replace(')','')
-        // PBS does not allow more than 15 characters for the job name string
-        result && result.size()>15 ? result.substring(0,15) : result
-    }
-    /**
-     * The command line to submit this job
-     *
-     * @param task The {@link TaskRun} instance to submit for execution to the cluster
-     * @param scriptFile The file containing the job launcher script
-     * @return A list representing the submit command line
-     */
-    List<String> getSubmitCommandLine(TaskRun task, Path scriptFile ) {
-        // in some PBS implementation the submit command will fail if the script name starts with a dot eg `.command.run`
-        // add the `-N <job name>` to fix this -- see issue #228
-        [ 'qsub', '-N', getJobNameFor(task), scriptFile.getName() ]
-    }
-
-    protected String getHeaderToken() { '#PBS' }
-
-    /**
-     * Parse the string returned by the {@code qsub} command and extract the job ID string
-     *
-     * @param text The string returned when submitting the job
-     * @return The actual job ID string
-     */
-    @Override
-    def parseJobId( String text ) {
-        // return always the last line
-        def result = text?.trim()
-        if( result && !result.contains('\n')) {
-            return result
-        }
-
-        throw new IllegalArgumentException("Invalid PBS/Torque submit response:\n$text\n\n")
-    }
-
-    @Override
-    protected List<String> getKillCommand() { ['qdel'] }
-
-    @Override
     protected List<String> queueStatusCommand(Object queue) {
         String cmd = 'qstat -f'
         if( queue ) cmd += ' ' + queue
         return ['sh','-c', "$cmd | egrep '(Job Id:|job_state =)'".toString()]
     }
-
-    static private Map DECODE_STATUS = [
-            'C': QueueStatus.DONE,
-            'R': QueueStatus.RUNNING,
-            'Q': QueueStatus.PENDING,
-            'H': QueueStatus.HOLD,
-            'S': QueueStatus.HOLD
-    ]
-
-    @Override
-    protected Map<String, QueueStatus> parseQueueStatus(String text) {
-
-        final JOB_ID = 'Job Id:'
-        final JOB_STATUS = 'job_state ='
-        final result = [:]
-
-        String id = null
-        String status = null
-        text.eachLine { line ->
-            if( line.startsWith(JOB_ID) ) {
-                id = fetchValue(JOB_ID, line)
-            }
-            else if( id ) {
-                status = fetchValue(JOB_STATUS, line)
-            }
-            result.put( id, DECODE_STATUS[status] ?: AbstractGridExecutor.QueueStatus.UNKNOWN )
-        }
-
-        return result
-    }
-
-    static String fetchValue( String prefix, String line ) {
-        final p = line.indexOf(prefix)
-        return p!=-1 ? line.substring(p+prefix.size()).trim() : null
-    }
-
 }
