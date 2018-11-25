d3bd12e65bec0741362394dbfa34749120b75e4c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-11-25 16:58:34 +0100

    Update nextflow website bucket name

--- pub-tests.sh
+++ pub-tests.sh
@@ -10,7 +10,7 @@ export AWS_DEFAULT_REGION=eu-west-1
 
 # use the java version as target context
 ver=$(java -version 2>&1 | awk '/version/ {gsub(/"/, "", $3); print $3}')
-base=s3://www.nextflow.io/tests/$1/$ver
+base=s3://www2.nextflow.io/tests/$1/$ver
 
 # delete previous run
 aws --region $AWS_DEFAULT_REGION s3 rm --quiet --recursive $base
