7aa5c2b9cd8ebe408b19dbfb4839affe3e1e2046
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-01-08 16:14:43 +0100

    Speed-up CI tests
    This commit limits integration tests only for changes on branches
    'master' and 'testing' when JDK version > 8

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+X_JDK=${TEST_JDK:=8}
+X_BRANCH=${TRAVIS_BRANCH:-${CIRCLE_BRANCH:-'master'}}
+X_PULL_REQUEST=${TRAVIS_PULL_REQUEST:-false}
 
-if [[ $TRAVIS_PULL_REQUEST == true ]] && [ ${TEST_JDK:=8} -gt 8 ]; then
+if [[ $X_PULL_REQUEST == true ]] && [ ${X_JDK} -gt 8 ]; then
   echo "Skipping integration tests on PR and JDK>8"
   exit 0
 fi
 
-if [[ $TEST_JDK ]]; then
+if [[ $X_BRANCH != master && $X_BRANCH != testing ]] && [ ${X_JDK:=8} -gt 8 ]; then
+  echo "Integration tests are only executed on branch 'master' or 'testing' for JDK>8"
+  exit 0
+fi
+
+if [ ${X_JDK} -gt 8 ]; then
     curl -o install-jdk.sh https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
     chmod +x install-jdk.sh
-    source ./install-jdk.sh --cacerts --feature $TEST_JDK
+    source ./install-jdk.sh --cacerts --feature $X_JDK
 fi
 
 export WITH_DOCKER='-with-docker'
