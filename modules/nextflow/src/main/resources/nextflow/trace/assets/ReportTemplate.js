78d6ee3cb60ff64ab2b9f2180fecf655d5dbb052
Philippe Hupé <philippe.hupe@curie.fr>
2019-02-05 22:44:54 +0100

    Use base 2 to report memory values in the table-humanreadable similarly to the memplot
    Signed-off-by: Philippe Hupé <philippe.hupe@curie.fr>

--- modules/nextflow/src/main/resources/nextflow/trace/assets/ReportTemplate.js
+++ modules/nextflow/src/main/resources/nextflow/trace/assets/ReportTemplate.js
@@ -166,6 +166,7 @@ $(function() {
     if (bytes == '-' || bytes == 0){
       return bytes;
     }
+    bytes = norm_mem([bytes]);
     // https://stackoverflow.com/a/14919494
     var thresh = 1000;
     if(Math.abs(bytes) < thresh) {
