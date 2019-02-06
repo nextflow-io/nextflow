583af58d1cfd238a7fb89cb97d358a347c5898e4
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-02-06 13:42:29 +0100

    [release 19.02.0-edge] Update timestamp and build numbers [ci skip]

--- modules/nextflow/src/main/groovy/nextflow/Const.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Const.groovy
@@ -50,17 +50,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "19.02.0"
+    static public final String APP_VER = "19.02.0-edge"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1549452287290
+    static public final long APP_TIMESTAMP = 1549456818122
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5056
+    static public final int APP_BUILDNUM = 5055
 
 
     /**
