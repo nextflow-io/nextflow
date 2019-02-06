00a412b15c957383b46c55278fdc0a8d6bfe1722
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-02-06 13:35:57 +0100

    Updated changelog

--- modules/nextflow/src/main/groovy/nextflow/Const.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Const.groovy
@@ -50,17 +50,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "19.01.0-edge"
+    static public final String APP_VER = "19.02.0"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1547117614234
+    static public final long APP_TIMESTAMP = 1549452287290
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5050
+    static public final int APP_BUILDNUM = 5056
 
 
     /**
