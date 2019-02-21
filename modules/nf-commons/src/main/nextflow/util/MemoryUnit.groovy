18a8de4630ff4cafcce4d17e7140ae579daf9a6d
evanbiederstedt <evan.biederstedt@gmail.com>
2019-02-21 10:26:33 -0500

    removed trailing bracket
    Signed-off-by: evanbiederstedt <evan.biederstedt@gmail.com>

--- modules/nf-commons/src/main/nextflow/util/MemoryUnit.groovy
+++ modules/nf-commons/src/main/nextflow/util/MemoryUnit.groovy
@@ -105,7 +105,6 @@ class MemoryUnit implements Comparable<MemoryUnit>, Serializable, Cloneable {
         int p = UNITS.indexOf(unit) 
         if (p==-1)
             throw new IllegalArgumentException("Not a valid memory unit: $unit")
-        }
         return Math.round( size/Math.pow(1024, p) )
     }
 
