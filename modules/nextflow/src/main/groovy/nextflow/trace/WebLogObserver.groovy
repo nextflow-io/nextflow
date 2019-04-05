08a3d1b26d58a70df29243d1edb0e8f52c87592e
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2019-04-05 15:50:48 +0200

    Adds parameter and workflow metadata to weblog payload (#1077)
    Signed-off-by: Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>

--- modules/nextflow/src/main/groovy/nextflow/trace/WebLogObserver.groovy
+++ modules/nextflow/src/main/groovy/nextflow/trace/WebLogObserver.groovy
@@ -17,14 +17,19 @@
 
 package nextflow.trace
 
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
+import groovy.json.JsonGenerator
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import groovyx.gpars.agent.Agent
+import nextflow.Const
 import nextflow.Session
 import nextflow.processor.TaskHandler
+import nextflow.script.ScriptBinding.ParamsMap
+import nextflow.script.WorkflowMetadata
+import nextflow.util.Duration
 import nextflow.util.SimpleHttpClient
+
+import java.nio.file.Path
 
 /**
  * Send out messages via HTTP to a configured URL on different workflow
@@ -37,6 +42,8 @@ import nextflow.util.SimpleHttpClient
 @CompileStatic
 class WebLogObserver implements TraceObserver{
 
+    private Session session
+
     private static final TimeZone UTC = TimeZone.getTimeZone("UTC")
 
     /**
@@ -48,11 +55,6 @@ class WebLogObserver implements TraceObserver{
      * Store the sessions unique ID for downstream reference purposes
      */
     private String runId
-
-    /**
-     * Not a HTTP header, but a message header with some general workflow info
-     */
-    private static JsonSlurper SLURPER = new JsonSlurper()
 
     /**
      * The default url is localhost
@@ -70,6 +72,11 @@ class WebLogObserver implements TraceObserver{
     private Agent<WebLogObserver> webLogAgent
 
     /**
+     * Json generator for weblog payloads
+     */
+    private JsonGenerator generator
+
+    /**
      * Constructor that consumes a URL and creates
      * a basic HTTP client.
      * @param url The target address for sending messages to
@@ -77,6 +84,7 @@ class WebLogObserver implements TraceObserver{
     WebLogObserver(String url) {
         this.httpClient.setUrl(checkUrl(url))
         this.webLogAgent = new Agent<>(this)
+        this.generator = createJsonGeneratorForPayloads()
     }
 
     /**
@@ -110,10 +118,11 @@ class WebLogObserver implements TraceObserver{
      */
     @Override
     void onFlowStart(Session session) {
-        // This is either set by the user or via Nextflow name generator
+        this.session = session
         runName = session.getRunName()
         runId = session.getUniqueId()
-        asyncHttpMessage("started")
+
+        asyncHttpMessage("started", createFlowPayloadFromSession(session))
     }
 
     /**
@@ -121,7 +130,7 @@ class WebLogObserver implements TraceObserver{
      */
     @Override
     void onFlowComplete() {
-        asyncHttpMessage("completed")
+        asyncHttpMessage("completed", createFlowPayloadFromSession(this.session))
     }
 
     /**
@@ -174,37 +183,45 @@ class WebLogObserver implements TraceObserver{
      * object, if present.
      * @param event The current run status. One of {'started', 'process_submit', 'process_start',
      * 'process_complete', 'error', 'completed'}
-     * @param trace A TraceRecord object that contains current process information
+     * @param payload An additional object to send. Must be of type TraceRecord or Manifest
      */
-    protected void sendHttpMessage(String event, TraceRecord trace = null){
+    protected void sendHttpMessage(String event, Object payload = null){
 
         // Set the message info
-        final time = new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'", UTC)
+        final time = new Date().format(Const.ISO_8601_DATETIME_FORMAT, UTC)
 
-        final message = new HashMap(5)
+        final message = new HashMap(4)
         message.runName = runName
         message.runId = runId
         message.event = event
-        message.runStatus = event // deprecated to be removed
         message.utcTime = time
 
-        // Append the trace object if present
-        if (trace)
-            message.trace = trace.store
+        if (payload instanceof TraceRecord)
+                message.trace = (payload as TraceRecord).store
+        else if (payload instanceof FlowPayload)
+                message.metadata = payload
+        else if (payload != null)
+            throw new IllegalArgumentException("Only TraceRecord and Manifest class types are supported: [${payload.getClass().getName()}] $payload")
 
         // The actual HTTP request
-        httpClient.sendHttpMessage(JsonOutput.toJson(message))
+        httpClient.sendHttpMessage(generator.toJson(message))
         logHttpResponse()
+    }
+
+    protected static FlowPayload createFlowPayloadFromSession(Session session) {
+        def params = session.binding.getProperty('params') as ParamsMap
+        def workflow = (WorkflowMetadata)session.binding.getVariable('workflow')
+        workflow.nextflow.timestamp = new Date(Const.APP_TIMESTAMP)
+        new FlowPayload(params, workflow)
     }
 
     /**
      * Asynchronous HTTP POST request wrapper.
      * @param event The workflow run status
-     * @param trace A TraceRecord object with workflow information
-     * @return A Java string, that contains the HTTP request response
+     * @param payload An additional object to send. Must be of type TraceRecord or Manifest
      */
-    protected void asyncHttpMessage(String event, TraceRecord trace = null){
-        webLogAgent.send{sendHttpMessage(event, trace)}
+    protected void asyncHttpMessage(String event, Object payload = null){
+        webLogAgent.send{sendHttpMessage(event, payload)}
     }
 
     /**
@@ -212,17 +229,39 @@ class WebLogObserver implements TraceObserver{
      */
     protected void logHttpResponse(){
         def statusCode = httpClient.getResponseCode()
-        if (statusCode == 200)
-            log.debug "Successfully send message to ${httpClient.getUrl()} -- received status code 200"
-        else {
+        if (statusCode >= 200 && statusCode < 300) {
+            log.debug "Successfully send message to ${httpClient.getUrl()} -- received status code ${statusCode}."
+        } else {
             def msg = """\
+                Unexpected HTTP response.
                 Failed to send message to ${httpClient.getUrl()} -- received 
                 - status code : $statusCode    
                 - response msg: ${httpClient.getResponse()}  
-                """
-                .stripIndent()
+                """.stripIndent()
             log.debug msg
         }
     }
 
+    private static JsonGenerator createJsonGeneratorForPayloads() {
+        new JsonGenerator.Options()
+                .addConverter(Path) { Path p, String key -> p.toUriString() }
+                .addConverter(Duration) { Duration d, String key -> d.durationInMillis }
+                .dateFormat(Const.ISO_8601_DATETIME_FORMAT).timezone("UTC")
+                .build()
+    }
+
+    private static class FlowPayload {
+
+        final ParamsMap parameters
+
+        final WorkflowMetadata workflow
+
+        FlowPayload(ParamsMap params, WorkflowMetadata workflow ) {
+            this.parameters = params
+            this.workflow = workflow
+            this.workflow.nextflow = ["version": Const.APP_VER,
+                                      "build": Const.APP_BUILDNUM,
+                                      "timestamp": new Date(Const.APP_TIMESTAMP)]
+        }
+    }
 }
