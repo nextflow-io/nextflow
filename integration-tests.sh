2c7e00b316430cb81090538ab20360f9407ac9aa
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-04-28 19:03:15 +0200

    Improve CI tests scripts

--- integration-tests.sh
+++ integration-tests.sh
@@ -13,10 +13,14 @@ if [[ $X_BRANCH != master && $X_BRANCH != testing ]] && [ ${X_JDK:=8} -gt 8 ]; t
   exit 0
 fi
 
+# install custom Java version
 if [ ${X_JDK} -gt 8 ]; then
-    curl -o install-jdk.sh https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
-    chmod +x install-jdk.sh
-    source ./install-jdk.sh --cacerts --feature $X_JDK
+  wget -q https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
+  # temporary path -- see https://github.com/sormuras/bach/issues/51#issuecomment-487380743
+  sed -i 's@https://download.java.net/java@https://download.oracle.com/java@g' install-jdk.sh
+  export JAVA_HOME=$HOME/jdk-$TEST_JDK
+  chmod +x install-jdk.sh
+  source ./install-jdk.sh -f $TEST_JDK -c --target $JAVA_HOME || ( echo Failed to configure Java $TEST_JDK && exit 1 )
 fi
 
 export WITH_DOCKER='-with-docker'
--- pub-tests.sh
+++ pub-tests.sh
@@ -4,20 +4,21 @@ set -x
 # Make test results available through
 #    https://www.nextflow.io/tests/travis/index.html
 #
-export AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
-export AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
+export AWS_ACCESS_KEY_ID=${NXF_AWS_ACCESS}
+export AWS_SECRET_ACCESS_KEY=${NXF_AWS_SECRET}
 export AWS_DEFAULT_REGION=eu-west-1
 
 # use the java version as target context
-ver=$(java -version 2>&1 | awk '/version/ {gsub(/"/, "", $3); print $3}')
-base=s3://www2.nextflow.io/tests/$1/$ver
+ver=${TEST_JDK:-8}
+base=s3://www2.nextflow.io/tests/$1/jdk$ver
 
 # delete previous run
-aws --region $AWS_DEFAULT_REGION s3 rm --quiet --recursive $base
+aws --region $AWS_DEFAULT_REGION s3 rm --only-show-errors --recursive $base
 
 # upload unit test results
 for x in $(find . -path \*build/reports/tests/test); do
 aws --region $AWS_DEFAULT_REGION s3 sync $x $base/${x#./} \
+ --only-show-errors \
  --cache-control max-age=0 \
  --metadata-directive REPLACE \
  --storage-class STANDARD \
@@ -26,4 +27,4 @@ aws --region $AWS_DEFAULT_REGION s3 sync $x $base/${x#./} \
 done
 
 # upload integration test results
-aws --region $AWS_DEFAULT_REGION s3 sync tests/checks $base/integration
+aws --region $AWS_DEFAULT_REGION s3 sync --only-show-errors tests/checks $base/integration
