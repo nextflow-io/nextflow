4530cf435023617d979f2318469643b4b922d950
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-08-19 10:02:07 +0200

    Fix tests on GH action

--- integration-tests.sh
+++ integration-tests.sh
@@ -2,6 +2,9 @@
 X_JDK=${TEST_JDK:=8}
 X_BRANCH=${TRAVIS_BRANCH:-${CIRCLE_BRANCH:-'master'}}
 X_PULL_REQUEST=${TRAVIS_PULL_REQUEST:-false}
+## handle github variables
+[[ $GITHUB_REF ]] && X_BRANCH=$(echo $GITHUB_REF | awk '{n=split($1,A,"/"); print A[n]}')
+[[ $GITHUB_EVENT_NAME == pull_request ]] && X_PULL_REQUEST=true
 
 if [[ $X_PULL_REQUEST != false ]] && [ ${X_JDK} -gt 8 ]; then
   echo "Skipping integration tests on PR and JDK>8"
