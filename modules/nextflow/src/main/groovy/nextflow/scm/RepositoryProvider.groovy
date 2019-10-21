81fd3ee370a4095afb5c7af7cce98aa36fc56584
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-10-21 16:24:29 +0200

    Improve resource not found error message
    Merge #1341

--- modules/nextflow/src/main/groovy/nextflow/scm/RepositoryProvider.groovy
+++ modules/nextflow/src/main/groovy/nextflow/scm/RepositoryProvider.groovy
@@ -153,6 +153,9 @@ abstract class RepositoryProvider {
                     def message = config.auth ? "Check that the ${name.capitalize()} user name and password provided are correct" : "Provide your ${name.capitalize()} user name and password to access this repository"
                     throw new AbortOperationException("Forbidden -- $message")
                 }
+            case 404:
+                log.debug "Response status: $code -- ${connection.getErrorStream()?.text}"
+                throw new AbortOperationException("Remote resource not found: ${connection.getURL()}")
         }
 
     }
