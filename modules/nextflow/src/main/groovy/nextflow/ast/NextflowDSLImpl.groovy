4b4d230f39cd66f1e9d55558711570b44c9e1697
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-11-15 16:11:58 +0100

    Introduce workflow take in place of get [DSL2]

--- modules/nextflow/src/main/groovy/nextflow/ast/NextflowDSLImpl.groovy
+++ modules/nextflow/src/main/groovy/nextflow/ast/NextflowDSLImpl.groovy
@@ -115,7 +115,8 @@ class NextflowDSLImpl implements ASTTransformation {
     @CompileStatic
     static class DslCodeVisitor extends ClassCodeVisitorSupport {
 
-        final static String WORKFLOW_GET = 'get'
+        @Deprecated final static String WORKFLOW_GET = 'get'
+        final static String WORKFLOW_TAKE = 'take'
         final static String WORKFLOW_EMIT = 'emit'
         final static String WORKFLOW_MAIN = 'main'
         final static String WORKFLOW_PUBLISH = 'publish'
@@ -333,7 +334,7 @@ class NextflowDSLImpl implements ASTTransformation {
                 return createAssignX(stat, body, type, uniqueNames)
             }
 
-            if( type == WORKFLOW_PUBLISH     ) {
+            if( type == WORKFLOW_PUBLISH ) {
                 return createAssignX(stat, body, type, uniqueNames)
             }
 
@@ -429,6 +430,7 @@ class NextflowDSLImpl implements ASTTransformation {
                         }
 
                     case WORKFLOW_GET:
+                    case WORKFLOW_TAKE:
                     case WORKFLOW_EMIT:
                         if( !(stm instanceof ExpressionStatement) ) {
                             syntaxError(stm, "Workflow malformed parameter definition")
