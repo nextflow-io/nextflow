4dacebbf718018110e25f75c73963f18f7adbbb0
Benjamin Schuster-Boeckler <ben@evershelfapp.com>
2020-04-07 07:39:15 +0100

    Recognises more UGE states (#1536)
    Solves #1461
    
    Signed-off-by: Benjamin Schuster-Böckler <ben.sb@methodicate.com>
    Co-authored-by: Benjamin Schuster-Böckler <ben.sb@methodicate.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/SgeExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/SgeExecutor.groovy
@@ -132,11 +132,21 @@ class SgeExecutor extends AbstractGridExecutor {
     }
 
     static protected Map DECODE_STATUS = [
-            'r': QueueStatus.RUNNING,
-            'qw': QueueStatus.PENDING,
-            'Tr': QueueStatus.HOLD,
-            'hqw': QueueStatus.HOLD,
-            'Eqw': QueueStatus.ERROR
+        't': QueueStatus.RUNNING,
+        'r': QueueStatus.RUNNING,
+        'R': QueueStatus.RUNNING,
+        'qw': QueueStatus.PENDING,
+        'h': QueueStatus.PENDING,
+        'w': QueueStatus.PENDING,
+        'P': QueueStatus.PENDING,
+        'N': QueueStatus.PENDING,
+        'S': QueueStatus.HOLD,
+        's': QueueStatus.HOLD,
+        'T': QueueStatus.HOLD,
+        'Tr': QueueStatus.HOLD,
+        'hqw': QueueStatus.HOLD,
+        'Eqw': QueueStatus.ERROR,
+        'E': QueueStatus.ERROR
     ]
 
     @Override
