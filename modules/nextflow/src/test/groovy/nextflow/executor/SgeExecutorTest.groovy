903ebe90873f958bba0c8c2287442321e52e34eb
Benjamin Schuster-Boeckler <ben@evershelfapp.com>
2020-04-07 07:39:15 +0100

    Recognises more UGE states (#1536)
    Solves #1461
    
    Signed-off-by: Benjamin Schuster-Böckler <ben.sb@methodicate.com>
    Co-authored-by: Benjamin Schuster-Böckler <ben.sb@methodicate.com>

--- modules/nextflow/src/test/groovy/nextflow/executor/SgeExecutorTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/executor/SgeExecutorTest.groovy
@@ -298,23 +298,43 @@ class SgeExecutorTest extends Specification {
         """
         job-ID     prior   name       user         state submit/start at     queue                          jclass                         slots ja-task-ID
         ------------------------------------------------------------------------------------------------------------------------------------------------
+              1219 1.00000 oliver-tes abria        t     08/29/2014 10:17:21 long@node-hp0115.linux.crg.es                                     4
               1220 1.00000 oliver-tes abria        r     08/29/2014 10:17:21 long@node-hp0115.linux.crg.es                                     8
-              1258 0.17254 mouse.4689 epalumbo     r     08/29/2014 11:13:55 long@node-hp0515.linux.crg.es                                    16
+              1258 0.17254 mouse.4689 epalumbo     R     08/29/2014 11:13:55 long@node-hp0515.linux.crg.es                                    16
               1261 0.17254 run_mappin epalumbo     qw    08/29/2014 11:28:11 short@node-ib0208bi.linux.crg.                                    4
               1262 0.17254 run_mappin epalumbo     Eqw   08/29/2014 11:28:31 short@node-ib0209bi.linux.crg.                                    4
               1263 0.17254 run_mappin epalumbo     Tr    08/29/2014 11:28:31 short@node-ib0209bi.linux.crg.                                    4
+              24261953 0.00005 nf-collect benjamin     T     03/18/2020 11:13:25 short.qc@compc038.hpc.in.bmrc.                                    1 
+              1265 0.17254 run_mappin epalumbo     S     08/29/2014 11:28:31 short@node-ib0209bi.linux.crg.                                    4
+              1266 0.17254 run_mappin epalumbo     N     08/29/2014 11:28:31 short@node-ib0209bi.linux.crg.                                    4
+              1267 0.17254 run_mappin epalumbo     h     08/29/2014 11:28:11 short@node-ib0208bi.linux.crg.                                    4
+              1268 0.15000 run_testin bschuster    P     08/29/2018 12:15:00 short@node-ib0208bi.linux.crg.                                    1
+              1269 0.15000 run_testin bschuster    s     08/29/2018 12:15:00 short@node-ib0208bi.linux.crg.                                    1
+              1270 0.15000 run_testin bschuster    hqw   08/29/2018 12:15:01 short@node-ib0208bi.linux.crg.                                    1
+              1271 0.15000 run_testin bschuster    E     08/29/2018 12:15:02 short@node-ib0208bi.linux.crg.                                    1
+              1272 0.15000 run_testin bschuster    w     08/29/2018 12:15:02 short@node-ib0208bi.linux.crg.                                    1
         """.stripIndent().trim()
 
 
         when:
         def result = executor.parseQueueStatus(text)
         then:
-        result.size() == 5
+        result.size() == 15
+        result['1219'] == AbstractGridExecutor.QueueStatus.RUNNING
         result['1220'] == AbstractGridExecutor.QueueStatus.RUNNING
         result['1258'] == AbstractGridExecutor.QueueStatus.RUNNING
         result['1261'] == AbstractGridExecutor.QueueStatus.PENDING
         result['1262'] == AbstractGridExecutor.QueueStatus.ERROR
         result['1263'] == AbstractGridExecutor.QueueStatus.HOLD
+        result['24261953'] == AbstractGridExecutor.QueueStatus.HOLD
+        result['1265'] == AbstractGridExecutor.QueueStatus.HOLD
+        result['1266'] == AbstractGridExecutor.QueueStatus.PENDING
+        result['1267'] == AbstractGridExecutor.QueueStatus.PENDING
+        result['1268'] == AbstractGridExecutor.QueueStatus.PENDING
+        result['1269'] == AbstractGridExecutor.QueueStatus.HOLD
+        result['1270'] == AbstractGridExecutor.QueueStatus.HOLD
+        result['1271'] == AbstractGridExecutor.QueueStatus.ERROR
+        result['1272'] == AbstractGridExecutor.QueueStatus.PENDING
     }
 
     def testParseQueueDump() {
