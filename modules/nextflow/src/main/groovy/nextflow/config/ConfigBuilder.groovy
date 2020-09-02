83365aa339bc34e23c685579df1dcb49907c7811
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-09-02 15:01:23 +0200

    Add variable NXF_CONFIG_FILE

--- modules/nextflow/src/main/groovy/nextflow/config/ConfigBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/config/ConfigBuilder.groovy
@@ -77,6 +77,8 @@ class ConfigBuilder {
 
     Map<ConfigObject, String> emptyVariables = new LinkedHashMap<>(10)
 
+    Map<String,String> env = new HashMap<>(System.getenv())
+
     List<String> warnings = new ArrayList<>(10);
 
     {
@@ -205,6 +207,7 @@ class ConfigBuilder {
 
         /**
          * Config file in the pipeline base dir
+         * This config file name should be predictable, therefore cannot be overridden
          */
         def base = null
         if( baseDir && baseDir != currentDir ) {
@@ -217,8 +220,10 @@ class ConfigBuilder {
 
         /**
          * Local or user provided file
+         * Default config file name can be overridden with `NXF_CONFIG_FILE` env variable
          */
-        def local = currentDir.resolve('nextflow.config')
+        def configFileName = env.get('NXF_CONFIG_FILE') ?: 'nextflow.config'
+        def local = currentDir.resolve(configFileName)
         if( local.exists() && local != base ) {
             log.debug "Found config local: $local"
             result << local
