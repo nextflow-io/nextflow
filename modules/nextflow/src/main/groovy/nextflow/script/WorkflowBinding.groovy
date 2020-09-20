fc607f118b5162fa9b1f80cfb910b2ff11bf0111
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-09-20 18:52:07 +0200

    Fix invalid error message on missing process #1719

--- modules/nextflow/src/main/groovy/nextflow/script/WorkflowBinding.groovy
+++ modules/nextflow/src/main/groovy/nextflow/script/WorkflowBinding.groovy
@@ -19,6 +19,7 @@ package nextflow.script
 
 import groovy.transform.CompileStatic
 import groovy.transform.PackageScope
+import groovy.util.logging.Slf4j
 import nextflow.exception.IllegalInvocationException
 import nextflow.extension.OpCall
 import nextflow.extension.OperatorEx
@@ -28,6 +29,7 @@ import nextflow.extension.OperatorEx
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@Slf4j
 @CompileStatic
 class WorkflowBinding extends Binding  {
 
@@ -64,6 +66,8 @@ class WorkflowBinding extends Binding  {
         return owner
     }
 
+    ScriptMeta getScriptMeta() { meta }
+
     @Override
     String toString() {
         "${this.getClass().getSimpleName()}[vars=${variables}]"
@@ -84,6 +88,7 @@ class WorkflowBinding extends Binding  {
     @Override
     Object invokeMethod(String name, Object args) {
         if( meta ) {
+            log.trace "Trying to invoke component: $name - args=${args}"
             final component = getComponent0(name)
             if( component ) {
                 checkScope0(component)
