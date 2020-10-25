5083066ee1430399dc466915602f6d202d7239ff
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-10-25 16:38:48 +0100

    Ad support for Bitbucket tags and branches retrivial

--- modules/nextflow/src/main/groovy/nextflow/scm/BitbucketRepositoryProvider.groovy
+++ modules/nextflow/src/main/groovy/nextflow/scm/BitbucketRepositoryProvider.groovy
@@ -17,6 +17,7 @@
 
 package nextflow.scm
 
+import groovy.transform.Memoized
 import groovy.util.logging.Slf4j
 import nextflow.exception.AbortOperationException
 /**
@@ -59,6 +60,52 @@ final class BitbucketRepositoryProvider extends RepositoryProvider {
 
     String getMainBranch() {
         invokeAndParseResponse(getMainBranchUrl()) ?. mainbranch ?. name
+    }
+
+    @Memoized
+    protected <T> List<T> invokeAndResponseWithPaging(String url, Closure<T> parse) {
+        final result = new ArrayList<T>(50)
+        do {
+            final resp = invokeAndParseResponse(url)
+            final tags = resp.values as List<Map>
+            for( Map entry : tags ) {
+                parse.call(entry)
+            }
+            // next iteration
+            // https://developer.atlassian.com/bitbucket/api/2/reference/meta/pagination
+            url = resp.next
+        }
+        while( url != null )
+        return result
+    }
+
+    /**
+     * Fetch the repository tags
+     * https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/refs/tags
+     *
+     * @return A list of {@link TagInfo}
+     */
+    @Override
+    List<TagInfo> getTags() {
+        final result = new ArrayList<TagInfo>()
+        final url = "$config.endpoint/api/2.0/repositories/$project/refs/tags"
+        final mapper = { Map entry -> result.add( new TagInfo(entry.name, entry.target?.hash) ) }
+        invokeAndResponseWithPaging(url, mapper)
+        return result
+    }
+
+    /**
+     * https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/refs/branches
+     *
+     * @return A list of {@link BranchInfo}
+     */
+    @Override
+    List<BranchInfo> getBranches() {
+        final result = new ArrayList<BranchInfo>()
+        final url = "$config.endpoint/api/2.0/repositories/$project/refs/branches"
+        final mapper = { Map entry -> result.add( new BranchInfo(entry.name, entry.target?.hash) ) }
+        invokeAndResponseWithPaging(url, mapper)
+        return result
     }
 
     @Override
