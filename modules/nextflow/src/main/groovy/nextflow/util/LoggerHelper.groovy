c3f73772f4f5b07136510bcfce45b2aec8c960c7
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-11-07 16:10:00 +0100

    Fix println not shown when using ansi logging #1621

--- modules/nextflow/src/main/groovy/nextflow/util/LoggerHelper.groovy
+++ modules/nextflow/src/main/groovy/nextflow/util/LoggerHelper.groovy
@@ -392,7 +392,7 @@ class LoggerHelper {
         @Override
         String doLayout(ILoggingEvent event) {
             final session = (Session)Global.session
-            fmtEvent(event, session)
+            fmtEvent(event, session, true)
         }
     }
 
@@ -403,10 +403,10 @@ class LoggerHelper {
      * @param session Nextflow session object
      * @return the formatted logging message
      */
-    static protected String fmtEvent(ILoggingEvent event, Session session) {
+    static protected String fmtEvent(ILoggingEvent event, Session session, boolean newLine) {
         final buffer = new StringBuilder(512);
         if( event.level == Level.INFO ) {
-            buffer .append(event.getFormattedMessage()) .append(CoreConstants.LINE_SEPARATOR)
+            buffer .append(event.getFormattedMessage())
         }
         else if( event.level == Level.ERROR ) {
             def error = ( event.getThrowableProxy() instanceof ThrowableProxy
@@ -419,8 +419,10 @@ class LoggerHelper {
             buffer
                     .append( event.getLevel().toString() ) .append(": ")
                     .append(event.getFormattedMessage())
-                    .append(CoreConstants.LINE_SEPARATOR)
         }
+
+        if( newLine )
+            buffer.append(CoreConstants.LINE_SEPARATOR)
 
         return buffer.toString()
     }
@@ -469,11 +471,9 @@ class LoggerHelper {
         // extra formatting
         if( error ) {
             buffer.append(" -- Check script '${error[0]}' at line: ${error[1]} or see '${logFileName}' file for more details")
-            buffer.append(CoreConstants.LINE_SEPARATOR)
         }
         else if( logFileName && !quiet ) {
             buffer.append(" -- Check '${logFileName}' file for details")
-            buffer.append(CoreConstants.LINE_SEPARATOR)
         }
 
     }
@@ -606,7 +606,8 @@ class LoggerHelper {
     }
 
     /**
-     * Capture logging events and forward them to
+     * Capture logging events and forward them to. This is only used when
+     * ANSI interactive logging is enabled
      */
     static private class CaptureAppender extends AppenderBase<ILoggingEvent> {
 
@@ -615,7 +616,7 @@ class LoggerHelper {
             final session = (Session)Global.session
 
             try {
-                final message = fmtEvent(event, session).trim()
+                final message = fmtEvent(event, session, false)
                 final renderer = session?.ansiLogObserver
                 if( !renderer || !renderer.started || renderer.stopped )
                     System.out.println(message)
