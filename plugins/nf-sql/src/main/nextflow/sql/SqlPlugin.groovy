fd4dd3d7dd3b97c7a3af95c9f3b5c9d55c99e16c
Abhinav Sharma <abhi18av@gmail.com>
2021-01-05 14:30:05 -0300

    Add stubs for the nf-sql plugin

---
+++ plugins/nf-sql/src/main/nextflow/sql/SqlPlugin.groovy
@@ -0,0 +1,19 @@
+package nextflow.sql
+
+import groovy.transform.CompileStatic
+import nextflow.plugin.BasePlugin
+import org.pf4j.PluginWrapper
+
+/**
+ * Nextflow Tower plugin
+ *
+ * @author Abhinav Sharma <abhi18av@outlook.com>
+ */
+@CompileStatic
+class SqlPlugin extends BasePlugin {
+
+    SqlPlugin(PluginWrapper wrapper) {
+        super(wrapper)
+    }
+
+}
