662f7f2aa32aed546eea8748f327984d8b74b023
Ido Bar <IdoBar@users.noreply.github.com>
2021-01-12 00:52:49 +1000

    Check queue status across all available servers
    Check queue status for user-submitted jobs across all queues in all available servers. 
    This should fix issue #1840 
    Please verify the escaping of the single quotes in line 85.
    
    Signed-off-by: Ido Bar <ido.idobar@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
@@ -82,8 +82,8 @@ class PbsProExecutor extends PbsExecutor {
 
     @Override
     protected List<String> queueStatusCommand(Object queue) {
-        String cmd = 'qstat -f'
-        if( queue ) cmd += ' ' + queue
+        String cmd = 'qstat -u $USER -f $(qstat -B | egrep -v \'(^Server|^---)\' | awk -v ORS=" " \'{print "@"$1}\''
+       // if( queue ) cmd += ' ' + queue
         return ['bash','-c', "set -o pipefail; $cmd | { egrep '(Job Id:|job_state =)' || true; }".toString()]
     }
 
