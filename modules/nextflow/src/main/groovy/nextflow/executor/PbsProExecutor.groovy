08b7d7ffb4f53f0b9e0ce0251217c00921a2b554
Ido Bar <IdoBar@users.noreply.github.com>
2021-01-12 14:36:07 +1000

    Fix missing closing bracket in PbsProExecutor.groovy line 85
    Fix missing closing bracket in `PbsProExecutor.groovy` line 85
    
    Signed-off-by: Ido Bar <ido.idobar@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
@@ -82,7 +82,7 @@ class PbsProExecutor extends PbsExecutor {
 
     @Override
     protected List<String> queueStatusCommand(Object queue) {
-        String cmd = 'qstat -u $USER -f $(qstat -B | egrep -v \'(^Server|^---)\' | awk -v ORS=" " \'{print "@"$1}\''
+        String cmd = 'qstat -u $USER -f $( qstat -B | egrep -v \'(^Server|^---)\' | awk -v ORS=" " \'{print "@"$1}\' )'
        // if( queue ) cmd += ' ' + queue
         return ['bash','-c', "set -o pipefail; $cmd | { egrep '(Job Id:|job_state =)' || true; }".toString()]
     }
