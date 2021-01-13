02d65f37179bcc2132c1f494965c897e1396f2f3
Ido Bar <IdoBar@users.noreply.github.com>
2021-01-13 21:18:26 +1000

    Show queue status of all servers if `queue` is not provided
    Maintain original behaviour if `queue` is provided, otherwise parse all user jobs in all available servers.
    
    Signed-off-by: Ido Bar <ido.idobar@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/PbsProExecutor.groovy
@@ -82,8 +82,12 @@ class PbsProExecutor extends PbsExecutor {
 
     @Override
     protected List<String> queueStatusCommand(Object queue) {
-        String cmd = 'qstat -u $USER -f $( qstat -B | egrep -v \'(^Server|^---)\' | awk -v ORS=" " \'{print "@"$1}\' )'
-       // if( queue ) cmd += ' ' + queue
+        String cmd = 'qstat -u $USER -f ' 
+        if( queue ) {
+            cmd += queue
+        } else {
+            cmd += '$( qstat -B | egrep -v \'(^Server|^---)\' | awk -v ORS=" " \'{print "@"$1}\' )'
+        }
         return ['bash','-c', "set -o pipefail; $cmd | { egrep '(Job Id:|job_state =)' || true; }".toString()]
     }
 
