39b6b6f87046b93ad184beff8ef8f648f8647c66
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-02-28 21:14:01 +0100

    Update copyright notice

--- modules/nf-commons/src/main/nextflow/plugin/DevPluginClasspath.groovy
+++ modules/nf-commons/src/main/nextflow/plugin/DevPluginClasspath.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020, Seqera Labs
+ * Copyright 2020-2021, Seqera Labs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
