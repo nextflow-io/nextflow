d66c4726a7dfda0bad1f98fcb628f00d12f38bda
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-03-10 16:17:37 +0100

    Fix local dependant formatting - Solves #1962

--- modules/nextflow/src/main/groovy/nextflow/container/DockerBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/container/DockerBuilder.groovy
@@ -117,7 +117,7 @@ class DockerBuilder extends ContainerBuilder<DockerBuilder> {
         result << 'run -i '
 
         if( cpus )
-            result << "--cpus ${String.format("%.1f", cpus)} "
+            result << "--cpus ${String.format(Locale.ROOT, "%.1f", cpus)} "
 
         if( cpuset ) {
             if( legacy )
