74c1ea1248009aa1b7707ce07c8f9120f7129e55
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-05-07 22:58:55 +0200

    Fix nested params file parse [ci fast]

--- modules/nextflow/src/test/groovy/nextflow/config/ConfigBuilderTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/config/ConfigBuilderTest.groovy
@@ -1905,6 +1905,56 @@ class ConfigBuilderTest extends Specification {
         then:
         cfg2.params.test.foo == "CLI_FOO"
         cfg2.params.test.bar == "bar_def"
+
+        cleanup:
+        folder?.deleteDir()
+    }
+
+    def 'parse nested json' () {
+        given:
+        def folder = Files.createTempDirectory('test')
+        and:
+        def config = folder.resolve('nf.json')
+        config.text = '''\
+        {
+            "title": "something",
+            "nested": {
+                "name": "Mike"
+            }
+        }
+        '''.stripIndent()
+
+        when:
+        def cfg1 = new ConfigBuilder().setCmdRun(new CmdRun(paramsFile: config.toString())).build()
+
+        then:
+        cfg1.params.title == "something"
+        cfg1.params.nested == [name: 'Mike']
+        
+        cleanup:
+        folder?.deleteDir()
+    }
+
+    def 'parse nested yaml' () {
+        given:
+        def folder = Files.createTempDirectory('test')
+        and:
+        def config = folder.resolve('nf.yaml')
+        config.text = '''\
+            title: "something"
+            nested: 
+                name: "Mike"
+        '''.stripIndent()
+
+        when:
+        def cfg1 = new ConfigBuilder().setCmdRun(new CmdRun(paramsFile: config.toString())).build()
+
+        then:
+        cfg1.params.title == "something"
+        cfg1.params.nested == [name: 'Mike']
+
+        cleanup:
+        folder?.deleteDir()
     }
 
 }
