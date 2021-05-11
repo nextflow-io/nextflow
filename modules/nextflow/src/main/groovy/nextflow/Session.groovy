68e5ca182d0588e0a3d85f7ff2de8f076faca4f9
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-05-11 22:56:39 +0200

    Fix Error reporting for malformed config [ci fast]

--- modules/nextflow/src/main/groovy/nextflow/Session.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Session.groovy
@@ -1121,10 +1121,13 @@ class Session implements ISession {
 
 
     private void getContainerConfig0(String engine, List<Map> drivers) {
-        def config = this.config?.get(engine) as Map
-        if( config ) {
+        def config = this.config?.get(engine)
+        if( config instanceof Map ) {
             config.engine = engine
             drivers << config
+        }
+        else if( config!=null ) {
+            log.warn "Malformed configuration for container engine '$engine' -- One or more attributes should be provided"
         }
     }
 
