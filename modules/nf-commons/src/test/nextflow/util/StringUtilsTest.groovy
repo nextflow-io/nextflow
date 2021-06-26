190aa4eaf1313f693ef73c2ce6eb75ef357ce829
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-06-26 16:20:09 +0200

    Fix PublishDir overwriting task output files
    This commit prevent the publishDir directive from deleting the task
    output files and replacing them with a circular symlink when the
    publish target contains already a symlink directory to the task work dir.
    
    This can happen if the task declares two output paths in which the first
    is a directory and the second one or more files in the same directory, eg.
    
      output:
        path 'some-dir'
        path 'some-dir/file1.txt'
    
    In above situation, the publish dir creates a symlink `some-dir` in the
    publish target directory pointing to the task work dir. The following
    file publishing follows the previously created link messing up the tasks output files.
    
    This commit solves the problem in two ways:
    1. given the set of files to publish compute a prefix tree omitting
       all files that are contained in a parent path, e.g. given the previous
       situation only 'some-dir' is published since the sub-files are expected
       to be contained on that directory. This is also expected to speed-up publish
       throughput omitting the copy for child files.
    2. Proactively checking if the publish operation is trying to write in a task
       work directory resolving the destination file path of published file.
       In this case a warning is reported to the user.

---
+++ modules/nf-commons/src/test/nextflow/util/StringUtilsTest.groovy
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020-2021, Seqera Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package nextflow.util
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class StringUtilsTest extends Specification {
+
+    @Unroll
+    def 'should get url protocol' () {
+        expect:
+        StringUtils.getUrlProtocol(STR)  == EXPECTED
+        where:
+        EXPECTED    | STR
+        'ftp'       | 'ftp://abc.com'
+        's3'        | 's3://bucket/abc'
+        null        | 'gitzabc:xyz'
+        null        | '/a/bc/'
+    }
+
+    @Unroll
+    def 'should find base url #STR' () {
+        expect:
+        StringUtils.baseUrl(STR) == BASE
+
+        where:
+        BASE                | STR
+        null                | null
+        'http://foo.com'    | 'http://foo.com'
+        'http://foo.com'    | 'http://foo.com/abc'
+        'http://foo.com'    | 'http://foo.com/abc/mskd0fs =ds0f'
+        and:
+        'https://foo.com'    | 'https://foo.com'
+        'https://foo.com'    | 'https://foo.com/abc'
+        'https://foo.com'    | 'https://foo.com/abc/mskd0fs =ds0f'
+        and:
+        'https://foo.com'    | 'HTTPS://FOO.COM'
+        'https://foo.com'    | 'HTTPS://FOO.COM/ABC'
+        'https://foo.com'    | 'HTTPS://FOO.COM/ABC/MSKD0FS =DS0F'
+        and:
+        'https://foo.com:80' | 'https://foo.com:80'
+        'https://foo.com:80' | 'https://foo.com:80/abc'
+        'https://foo.com:80' | 'https://foo.com:80/abc/mskd0fs =ds0f'
+        and:
+        'ftp://foo.com:80'   | 'ftp://foo.com:80'
+        'ftp://foo.com:80'   | 'ftp://foo.com:80/abc'
+        's3://foo.com:80'    | 's3://foo.com:80/abc'
+        and:
+        'dx://project-123'   | 'dx://project-123'
+        'dx://project-123:'  | 'dx://project-123:'
+        'dx://project-123:'  | 'dx://project-123:/abc'
+        and:
+        null                 | 'blah'
+        null                 | 'http:/xyz.com'
+        null                 | '1234://xyz'
+        null                 | '1234://xyz.com/abc'
+    }
+}
