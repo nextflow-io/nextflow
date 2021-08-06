eb9361fdc0cefb09a3010742b8f86d6cc250a491
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-08-06 14:13:52 +0200

    [release 21.07.0-edge] Update timestamp and build number

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -53,17 +53,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "21.06.0-edge"
+    static public final String APP_VER = "21.07.0-edge"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1625342773485
+    static public final long APP_TIMESTAMP = 1628247982136
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5578
+    static public final int APP_BUILDNUM = 5583
 
 
     /**
