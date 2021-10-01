040e190bd82f2f2395afa1937920cbba38855aeb
Manuele Simi <mas2182@med.cornell.edu>
2021-10-01 03:26:55 -0400

    Add missing information for pulling images from private registry in Azure Batch (#2355) [ci skip]

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -53,17 +53,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "21.08.0-edge"
+    static public final String APP_VER = "21.09.0-SNAPSHOT"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1630341567917
+    static public final long APP_TIMESTAMP = 1633032422963
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5609
+    static public final int APP_BUILDNUM = 5624
 
 
     /**
