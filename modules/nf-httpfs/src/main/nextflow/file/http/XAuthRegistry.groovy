e4e6c696044a8ed3629b2cf821b7d79fa058495f
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-10-26 18:11:24 +0200

    Add implict authentication for resources served by tower [ci fast]
    This commit automatically propagate the authentication information
    provided by the Tower client to resources served by the Tower server
    e.g. dataset files.

---
+++ modules/nf-httpfs/src/main/nextflow/file/http/XAuthRegistry.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020-2021, Seqera Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package nextflow.file.http
+
+import groovy.transform.CompileStatic
+
+/**
+ * Provides a pluggable authentication registry for {@link XFileSystemProvider}
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+@Singleton(strict = false)
+@CompileStatic
+class XAuthRegistry {
+
+    private List<XAuthProvider> providers = new ArrayList<>()
+
+    protected XAuthRegistry() {}
+
+    void register(XAuthProvider provider) {
+        providers.add(provider)
+    }
+
+    void unregister(XAuthProvider provider) {
+        final p = providers.indexOf(provider)
+        if( p!=-1 )
+            providers.remove(p)
+    }
+
+    boolean authorize(URLConnection connection) {
+        for( XAuthProvider it : providers ) {
+            if( it.authorize(connection) )
+                return true
+        }
+        return false
+    }
+
+}
