74fffa6a04379e3ddaacab58b134480c12d3e92d
Jordi Deu-Pons <jordi@jordeu.net>
2021-12-24 06:02:09 +0100

    Improve TowerReports code

--- plugins/nf-tower/src/main/io/seqera/tower/plugin/TowerReports.groovy
+++ plugins/nf-tower/src/main/io/seqera/tower/plugin/TowerReports.groovy
@@ -20,11 +20,11 @@ import java.util.stream.Collectors
  */
 class TowerReports {
 
-    private Path reportsPath
     private PrintWriter reportsFile
-    private Agent<PrintWriter> reportsWriter
-    private List<String> reportPatterns
-    private List<PathMatcher> reportMatchers
+    private boolean processReports
+    private Agent<PrintWriter> writer
+    private List<String> patterns
+    private List<PathMatcher> matchers
     private YamlSlurper yamlSlurper
 
     TowerReports() {
@@ -40,13 +40,13 @@ class TowerReports {
     void flowCreate(String workflowId) {
         Path launchDir = Paths.get('.').toRealPath()
         loadReportPatterns(launchDir, workflowId)
-        if (reportPatterns) {
-            this.reportsPath = launchDir.resolve(System.getenv().getOrDefault("TOWER_REPORTS_FILE", "nf-${workflowId}-reports.tsv"))
+        if (processReports) {
+            final reportsPath = launchDir.resolve(System.getenv().getOrDefault("TOWER_REPORTS_FILE", "nf-${workflowId}-reports.tsv"))
             this.reportsFile = new PrintWriter(Files.newBufferedWriter(reportsPath, Charset.defaultCharset()))
-            this.reportsWriter = new Agent<PrintWriter>(reportsFile)
+            this.writer = new Agent<PrintWriter>(reportsFile)
 
             // send header
-            this.reportsWriter.send { PrintWriter it -> it.println("key\tpath")}
+            this.writer.send { PrintWriter it -> it.println("key\tpath")}
         }
     }
 
@@ -54,8 +54,8 @@ class TowerReports {
      * On flow complete stop writing the reports file at background.
      */
     void flowComplete() {
-        if (reportsWriter) {
-            reportsWriter.await()
+        if (processReports) {
+            writer.await()
             reportsFile.flush()
             reportsFile.close()
         }
@@ -68,18 +68,20 @@ class TowerReports {
      * @param workflowId Tower workflow ID
      */
     protected void loadReportPatterns(Path launchDir, String workflowId) {
+        processReports = false
         Path towerConfigPath = launchDir.resolve("nf-${workflowId}-tower.yml")
         if (Files.exists(towerConfigPath)) {
             final towerConfig = yamlSlurper.parse(towerConfigPath)
-            this.reportPatterns = new ArrayList<>()
+            this.patterns = new ArrayList<>()
             if (towerConfig instanceof Map && towerConfig.containsKey("reports")) {
                 final reports = towerConfig.get("reports") as Map
                 reports.keySet().forEach(k -> {
                     if (k) {
-                        reportPatterns.add(k.toString())
+                        patterns.add(k.toString())
                     }
                 })
             }
+            processReports = this.patterns.size() > 0
         }
     }
 
@@ -90,21 +92,21 @@ class TowerReports {
      * @param destination Path of the published file at destination filesystem.
      */
     void filePublish(Path destination) {
-        if (reportsWriter && destination) {
+        if (processReports && destination) {
 
-            if (!reportMatchers) {
+            if (!matchers) {
                 // Initialize report matchers on first event to use the
                 // path matcher of the destination filesystem
-                reportMatchers = reportPatterns.stream()
+                matchers = patterns.stream()
                         .map(p -> FileHelper.getPathMatcherFor("glob:**/${p}", destination.fileSystem))
                         .collect(Collectors.toList())
             }
 
-            for (int p=0; p < reportMatchers.size(); p++) {
-                if (reportMatchers.get(p).matches(destination)) {
+            for (int p=0; p < matchers.size(); p++) {
+                if (matchers.get(p).matches(destination)) {
                     final dst = destination.toAbsolutePath().toString()
-                    final pattern = reportPatterns.get(p)
-                    reportsWriter.send { PrintWriter it -> it.println("${pattern}\t${dst}") }
+                    final pattern = patterns.get(p)
+                    writer.send { PrintWriter it -> it.println("${pattern}\t${dst}") }
                     break
                 }
             }
