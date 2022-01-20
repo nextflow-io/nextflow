48b2035120f0f1f30a9c6a64382656d077122f7c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-01-20 21:49:13 +0100

    Update integration minimum JDK 11

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-TEST_JDK=${TEST_JDK:=8}
+TEST_JDK=${TEST_JDK:=11}
 X_BRANCH=${TRAVIS_BRANCH:-${CIRCLE_BRANCH:-'master'}}
 X_PULL_REQUEST=${TRAVIS_PULL_REQUEST:-false}
 ## handle github variables
 [[ $GITHUB_REF ]] && X_BRANCH=$(echo $GITHUB_REF | awk '{n=split($1,A,"/"); print A[n]}')
 [[ $GITHUB_EVENT_NAME == pull_request ]] && X_PULL_REQUEST=true
 
-if [[ $X_PULL_REQUEST != false ]] && [ ${TEST_JDK} -gt 8 ]; then
-  echo "Skipping integration tests on PR and JDK>8"
+if [[ $X_PULL_REQUEST != false ]] && [ ${TEST_JDK} -gt 11 ]; then
+  echo "Skipping integration tests on PR and JDK>11"
   exit 0
 fi
 
-if [[ $X_BRANCH != master && $X_BRANCH != testing ]] && [ ${TEST_JDK:=8} -gt 8 ]; then
-  echo "Integration tests are only executed on branch 'master' or 'testing' for JDK>8"
+if [[ $X_BRANCH != master && $X_BRANCH != testing ]] && [ ${TEST_JDK:=11} -gt 11 ]; then
+  echo "Integration tests are only executed on branch 'master' or 'testing' for JDK>11"
   exit 0
 fi
 
