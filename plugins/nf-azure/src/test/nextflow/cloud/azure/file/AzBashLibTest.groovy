af14703fc37e65626cac1d0aadb3d45dab953567
Abhinav Sharma <abhi18av@gmail.com>
2022-02-08 19:19:13 +0200

    Remove the temp test

--- plugins/nf-azure/src/test/nextflow/cloud/azure/file/AzBashLibTest.groovy
+++ plugins/nf-azure/src/test/nextflow/cloud/azure/file/AzBashLibTest.groovy
@@ -11,18 +11,6 @@ import spock.lang.Specification
  */
 class AzBashLibTest extends Specification {
 
-    def 'TEMP'() {
-
-        when:
-        def script = AzBashLib.script(new AzCopyOpts([blobSize: '100', blobTier: 'Hot']),
-                10,
-                20,
-                Duration.of('30s'))
-
-        then:
-        print(script)
-    }
-
     def 'should return base script'() {
         expect:
         AzBashLib.script() == '''
@@ -63,7 +51,7 @@ class AzBashLibTest extends Specification {
                 '''.stripIndent()
     }
 
-    def 'should return script with config, with no azcopy opts'() {
+    def 'should return script with config, with default azcopy opts'() {
 
         expect:
         AzBashLib.script(new AzCopyOpts(), 10, 20, Duration.of('30s')) == '''\
@@ -159,4 +147,99 @@ class AzBashLibTest extends Specification {
             '''.stripIndent()
     }
 
+    def 'should return script with config, with azcopy opts'() {
+
+        expect:
+        AzBashLib.script(new AzCopyOpts([blobTier: "Hot", blockSize: "10"]), 10, 20, Duration.of('30s')) == '''\
+            # bash helper functions
+            nxf_cp_retry() {
+                local max_attempts=20
+                local timeout=30
+                local attempt=0
+                local exitCode=0
+                while (( $attempt < $max_attempts ))
+                do
+                  if "$@"
+                    then
+                      return 0
+                  else
+                    exitCode=$?
+                  fi
+                  if [[ $exitCode == 0 ]]
+                  then
+                    break
+                  fi
+                  nxf_sleep $timeout
+                  attempt=$(( attempt + 1 ))
+                  timeout=$(( timeout * 2 ))
+                done
+            }
+            
+            nxf_parallel() {
+                IFS=$'\\n'
+                local cmd=("$@")
+                local cpus=$(nproc 2>/dev/null || < /proc/cpuinfo grep '^process' -c)
+                local max=$(if (( cpus>10 )); then echo 10; else echo $cpus; fi)
+                local i=0
+                local pid=()
+                (
+                set +u
+                while ((i<${#cmd[@]})); do
+                    local copy=()
+                    for x in "${pid[@]}"; do
+                      [[ -e /proc/$x ]] && copy+=($x)
+                    done
+                    pid=("${copy[@]}")
+            
+                    if ((${#pid[@]}>=$max)); then
+                      nxf_sleep 0.2
+                    else
+                      eval "${cmd[$i]}" &
+                      pid+=($!)
+                      ((i+=1))
+                    fi
+                done
+                for p in "${pid[@]}"; do
+                    wait $p
+                done
+                )
+                unset IFS
+            }
+            
+            nxf_az_upload() {
+                local name=$1
+                local target=${2%/} ## remove ending slash
+                local base_name="$(basename "$name")"
+                local dir_name="$(dirname "$name")"
+    
+                if [[ -d $name ]]; then
+                  if [[ "$base_name" == "$name" ]]; then
+                    azcopy cp "$name" "$target?$AZ_SAS" --recursive  --block-blob-tier Hot --block-size-mb 10
+                  else
+                    azcopy cp "$name" "$target/$dir_name?$AZ_SAS" --recursive  --block-blob-tier Hot --block-size-mb 10
+                  fi
+                else
+                  azcopy cp "$name" "$target/$name?$AZ_SAS"  --block-blob-tier Hot --block-size-mb 10
+                fi
+            }
+            
+            nxf_az_download() {
+                local source=$1
+                local target=$2
+                local basedir=$(dirname $2)
+                local ret
+                mkdir -p "$basedir"
+            
+                ret=$(azcopy cp "$source?$AZ_SAS" "$target" 2>&1) || {
+                    ## if fails check if it was trying to download a directory
+                    mkdir -p $target
+                    azcopy cp "$source/*?$AZ_SAS" "$target" --recursive >/dev/null || {
+                        rm -rf $target
+                        >&2 echo "Unable to download path: $source"
+                        exit 1
+                    }
+                }
+            }
+            '''.stripIndent()
+    }
 }
