5bc7c3e957f71b15ac1e06e490782f38dd722125
Jorge Aguilera <98739503+jorgeaguileraseqera@users.noreply.github.com>
2022-06-01 19:06:21 +0200

    Fix StackOverflowError when dump all profiles (#2922)
    Solve #2815
    
    Signed-off-by: Jorge Aguilera <jorge.aguilera@seqera.io>

--- modules/nextflow/src/test/groovy/nextflow/config/ConfigParserTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/config/ConfigParserTest.groovy
@@ -17,6 +17,8 @@
 
 package nextflow.config
 
+import spock.lang.Ignore
+
 import java.nio.file.Files
 import java.nio.file.NoSuchFileException
 import java.nio.file.Path
@@ -660,6 +662,66 @@ class ConfigParserTest extends Specification {
         config.params.baz == 'baz1'
         config.params.nested.baz == 'baz2'
         config.params.nested.foo.bar == 'bar2'
+    }
+
+    def 'should load parse profiles into configurations' () {
+
+        given:
+        def folder = File.createTempDir()
+        def snippet1 = new File(folder,'config1.txt').absoluteFile
+        def snippet2 = new File(folder,'config2.txt').absoluteFile
+
+        snippet1.text = '''
+            snippet = 'snippet1'
+            profiles {
+                debug{
+                    cleanup = 1
+                }
+            }
+        '''
+
+        snippet2.text = '''
+            snippet = 'snippet2'
+            profiles {
+                debug{
+                    cleanup = 2
+                }
+            }
+        '''
+
+
+        def configText = """
+        workDir = '/my/scratch'
+
+        profiles {
+
+            standard {}
+
+            slow {
+                includeConfig "$snippet1"
+            }
+
+            fast {
+                workDir = '/fast/scratch'
+                includeConfig "$snippet2"
+            }
+
+        }
+        """
+
+        when:
+        def config1 = new ConfigParser()
+                .parse(configText)
+        then:
+        config1.workDir == '/my/scratch'
+        config1.profiles.standard.size() == 0
+        config1.profiles.slow.snippet == 'snippet1'
+        config1.profiles.fast.snippet == 'snippet2'
+        config1.profiles.debug.cleanup == 2
+
+        cleanup:
+        folder.deleteDir()
+
     }
 
 
