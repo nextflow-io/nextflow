cb4bfdc49ad7e60c67891bfdb25515b161c20dab
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-06-06 13:24:36 +0200

    Bump version 22.05.1-SNAPSHOT [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -53,17 +53,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "22.05.0-edge"
+    static public final String APP_VER = "22.05.1-SNAPSHOT"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1653500378215
+    static public final long APP_TIMESTAMP = 1654514528449
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5704
+    static public final int APP_BUILDNUM = 5706
 
     /**
      * The app build time string relative to UTC timezone
