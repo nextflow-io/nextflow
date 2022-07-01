95f7d8dc0098f6b1723869a5d31bc0e2cf6aa443
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-07-01 10:52:56 +0200

    Revert "Allow to override lsf.conf settings with nextflow config #2862 [ci fast]"
    This reverts commit 1680a556448c1c36912dc50e59fbf2e29047c642.

--- modules/nextflow/src/main/groovy/nextflow/executor/LsfExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/LsfExecutor.groovy
@@ -284,8 +284,9 @@ class LsfExecutor extends AbstractGridExecutor {
             perJobMemLimit = str == 'Y'
             log.debug "[LSF] Detected lsf.conf LSB_JOB_MEMLIMIT=$str ($perJobMemLimit)"
         }
-
-        perJobMemLimit = session.getExecConfigProp(name, 'perJobMemLimit', perJobMemLimit)
+        else {
+            perJobMemLimit = session.getExecConfigProp(name, 'perJobMemLimit', false)
+        }
 
         // per task reserve https://github.com/nextflow-io/nextflow/issues/1071#issuecomment-481412239
         if( conf.get('RESOURCE_RESERVE_PER_TASK') ) {
@@ -293,8 +294,6 @@ class LsfExecutor extends AbstractGridExecutor {
             perTaskReserve = str == 'Y'
             log.debug "[LSF] Detected lsf.conf RESOURCE_RESERVE_PER_TASK=$str ($perTaskReserve)"
         }
-
-        perTaskReserve = session.getExecConfigProp(name, 'perTaskReserve', perTaskReserve)
     }
 
 }
