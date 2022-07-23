9d08e6b0aef42d7b01390732b3c68f7b190e0280
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-07-23 14:10:52 +0200

    Parallelize build integration tests
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- validation/test.sh
+++ validation/test.sh
@@ -7,87 +7,97 @@ get_abs_filename() {
 
 export NXF_IGNORE_WARN_DSL2=true
 export NXF_CMD=${NXF_CMD:-$(get_abs_filename ../launch.sh)}
-
-#
-# Tests
-#
-(
-  cd ../tests/
-  sudo bash cleanup.sh
-  cd checks
-  bash run.sh
-)
-
-if [[ $TEST_SMOKE == true ]]; then
-  echo Skipping tests since TEST_SMOKE flag is true
-  exit 0
-fi
-
 # disable ansi log to make log more readable
 export NXF_ANSI_LOG=false
 
 #
-# Hello 
+# Integration test
 #
-git clone https://github.com/nextflow-io/hello
-( 
-  cd hello; 
-  $NXF_CMD run .
-  $NXF_CMD run . -resume
-)
+if [[ $TEST_MODE == 'test_integration' ]]; then
 
-#
-# AMPA-NF
-#
-git clone https://github.com/cbcrg/ampa-nf
-docker pull cbcrg/ampa-nf
-(
-  cd ampa-nf; 
-  $NXF_CMD run . -with-docker 
-  $NXF_CMD run . -with-docker -resume 
-)
+    (
+      cd ../tests/
+      sudo bash cleanup.sh
+      cd checks
+      bash run.sh
+    )
 
-#
-# RNASEQ-NF
-#
-echo nextflow-io/rnaseq-nf
-[[ $TOWER_ACCESS_TOKEN ]] && OPTS='-with-tower' || OPTS=''
-$NXF_CMD run nextflow-io/rnaseq-nf -with-docker $OPTS
-$NXF_CMD run nextflow-io/rnaseq-nf -with-docker $OPTS -resume
+    if [[ $TEST_SMOKE == true ]]; then
+      echo Skipping tests since TEST_SMOKE flag is true
+      exit 0
+    fi
+
+    #
+    # Hello
+    #
+    git clone https://github.com/nextflow-io/hello
+    (
+      cd hello;
+      $NXF_CMD run .
+      $NXF_CMD run . -resume
+    )
+
+    #
+    # AMPA-NF
+    #
+    git clone https://github.com/cbcrg/ampa-nf
+    docker pull cbcrg/ampa-nf
+    (
+      cd ampa-nf;
+      $NXF_CMD run . -with-docker
+      $NXF_CMD run . -with-docker -resume
+    )
+
+    #
+    # RNASEQ-NF
+    #
+    echo nextflow-io/rnaseq-nf
+    [[ $TOWER_ACCESS_TOKEN ]] && OPTS='-with-tower' || OPTS=''
+    $NXF_CMD run nextflow-io/rnaseq-nf -with-docker $OPTS
+    $NXF_CMD run nextflow-io/rnaseq-nf -with-docker $OPTS -resume
+
+    exit 0
+fi
+
 
 if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
   echo "Skipping cloud integration tests on PR event"
   exit 0
 fi
 
+
 #
 # AWS Batch tests
 #
-if [ "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS batch tests"
-  bash awsbatch.sh
-else
-  echo "Missing AWS_ACCESS_KEY_ID variable -- Skipping AWS Batch tests"
+if [[ $TEST_MODE == 'test_aws' ]]; then
+    if [ "$AWS_ACCESS_KEY_ID" ]; then
+      echo "AWS batch tests"
+      bash awsbatch.sh
+    else
+      echo "Missing AWS_ACCESS_KEY_ID variable -- Skipping AWS Batch tests"
+    fi
 fi
-
 
 #
 # Azure Batch tests
 #
-if [ "$AZURE_BATCH_ACCOUNT_KEY" ]; then
-  echo "Azure batch tests"
-  bash azure.sh
-else
-  echo "Missing AZURE_BATCH_ACCOUNT_KEY variable -- Skipping Azure Batch tests"
+if [[ $TEST_MODE == 'test_azure' ]]; then
+    if [ "$AZURE_BATCH_ACCOUNT_KEY" ]; then
+      echo "Azure batch tests"
+      bash azure.sh
+    else
+      echo "Missing AZURE_BATCH_ACCOUNT_KEY variable -- Skipping Azure Batch tests"
+    fi
 fi
 
 #
 # Google Life Sciences
 #
-if [ "$GOOGLE_SECRET" ]; then
-  echo "Google LS tests"
-  bash google.sh
-else
-  echo "Missing GOOGLE_SECRET variable -- Skipping Google LS tests"
+if [[ $TEST_MODE == 'test_google' ]]; then
+    if [ "$GOOGLE_SECRET" ]; then
+      echo "Google LS tests"
+      bash google.sh
+    else
+      echo "Missing GOOGLE_SECRET variable -- Skipping Google LS tests"
+    fi
 fi
-
