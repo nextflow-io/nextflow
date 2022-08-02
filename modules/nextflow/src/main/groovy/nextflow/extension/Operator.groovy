a27985763e8563d84573569013077e1654145fba
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-08-02 12:34:57 -0400

    feature: import custom functions from plugin extensions
    add Factory and Operator annotations
    
    Signed-off-by: Jorge Aguilera <jorge.aguilera@seqera.io>

--- modules/nextflow/src/main/groovy/nextflow/extension/Operator.groovy
+++ modules/nextflow/src/main/groovy/nextflow/extension/Operator.groovy
@@ -1,8 +1,8 @@
 package nextflow.extension
 
 /**
- * An annotation interface for functions that the plugin want to expose
- * Nextflow will search for all methods annotated with @Functions in the ExtensionPoint and allow to the user imported them
+ * An annotation interface for operators that the plugin want to expose
+ * Nextflow will search for all methods annotated with @Operators in the ExtensionPoint and allow to the user imported them
  *
  * @author : jorge <jorge.aguilera@seqera.io>
  *
