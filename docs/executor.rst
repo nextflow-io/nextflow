683ba39d7e446307fa025a3990963cab0ea24b9e
Eric Bonnet <eric.d.bonnet@gmail.com>
2022-08-17 12:13:32 +0200

    Add support for Bridge batch scheduler (#3106)
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Signed-off-by: Eric Bonnet <eric.d.bonnet@gmail.com>

--- docs/executor.rst
+++ docs/executor.rst
@@ -61,6 +61,30 @@ running workloads. Moreover a Azure Blob storage container must be used as pipel
 See the :ref:`Azure Batch <azure-batch>` page for further configuration details.
 
 
+.. _bridge-executor:
+
+Bridge
+======
+
+`Bridge <https://github.com/cea-hpc/bridge>`_ is an abstraction layer to ease batch system and resource manager usage in 
+heterogeneous HPC environments.
+
+It is open source software and can be installed on top of existing classical job schedulers such as Slurm or LSF, or other 
+schedulers. Bridge allows to submit jobs, get information on running jobs, stop jobs, get information on the cluster system, etc.
+
+For more details on how to install the Bridge system, see the `documentation <https://github.com/cea-hpc/bridge>`_.
+
+To enable the Bridge executor simply set ``process.executor = 'bridge'`` in the ``nextflow.config`` file.
+
+Resource requests and other job characteristics can be controlled via the following process directives:
+
+* :ref:`process-clusterOptions`
+* :ref:`process-cpus`
+* :ref:`process-memory`
+* :ref:`process-queue`
+* :ref:`process-time`
+
+
 .. _ga4ghtes-executor:
 
 GA4GH TES
---
+++ modules/nextflow/src/test/groovy/nextflow/executor/BridgeExecutorTest.groovy
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2020-2022, Seqera Labs
+ * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ * Copyright 2022, CEA-CNRGH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.executor
+import java.nio.file.Paths
+
+import nextflow.processor.TaskConfig
+import nextflow.processor.TaskProcessor
+import nextflow.processor.TaskRun
+import spock.lang.Specification
+/**
+ *
+ * @author Eric Bonnet <eric.d.bonnet@gmail.com>
+ */
+class BridgeExecutorTest extends Specification {
+
+    def testParseJob() {
+
+        given:
+        def exec = [:] as BridgeExecutor 
+
+        expect:
+        //submission pattern example: Submitted Batch Session 1277017
+        exec.parseJobId('Submitted Batch Session 10') == '10'
+        exec.parseJobId('Submitted Batch Session 20') == '20'
+
+        when:
+        exec.parseJobId('Something else 10')
+        then:
+        thrown(IllegalStateException)
+
+    }
+
+    def testKill() {
+
+        given:
+        def exec = [:] as BridgeExecutor 
+        expect:
+        exec.killTaskCommand(123) == ['ccc_mdel','123']
+
+    }
+
+    def testGetCommandLine() {
+
+        when:
+        def exec = [:] as BridgeExecutor 
+        then:
+        exec.getSubmitCommandLine(Mock(TaskRun), Paths.get('/some/path/job.sh')) == ['ccc_msub', 'job.sh']
+    }
+
+    def testGetHeaders() {
+
+        setup:
+        // Bridge executor
+        def executor = [:] as BridgeExecutor
+
+        // mock process
+        def proc = Mock(TaskProcessor)
+
+        // task object
+        def task = new TaskRun()
+        task.processor = proc
+        task.workDir = Paths.get('/work/path')
+        task.name = 'virtual_digest'
+
+        when:
+        task.index = 21
+        task.config = new TaskConfig()
+        task.config.queue = 'normal'
+        task.config.time = '10h'
+        task.config.memory = '8 GB' 
+        task.config.cpus = 2
+        then:
+        executor.getHeaders(task) == '''
+                #MSUB -r nf-virtual_digest
+                #MSUB -o /work/path/.command.log
+                #MSUB -c 2
+                #MSUB -T 36000
+                #MSUB -M 8192
+                #MSUB -q normal
+                '''
+                .stripIndent().leftTrim()
+    }
+
+    def testQstatCommand() {
+
+        setup:
+        def executor = [:] as BridgeExecutor
+        def text =
+                """
+                5 pending
+                6 pending
+                13 running
+                15 failed
+                4 running
+                22 suspended
+                7 unknown
+                """.stripIndent().trim()
+
+
+        when:
+        def result = executor.parseQueueStatus(text)
+        then:
+        result.size() == 7
+        result['4'] == AbstractGridExecutor.QueueStatus.RUNNING
+        result['5'] == AbstractGridExecutor.QueueStatus.PENDING
+        result['6'] == AbstractGridExecutor.QueueStatus.PENDING
+        result['13'] == AbstractGridExecutor.QueueStatus.RUNNING
+        result['15'] == AbstractGridExecutor.QueueStatus.ERROR
+        result['7'] == AbstractGridExecutor.QueueStatus.ERROR
+        result['22'] == AbstractGridExecutor.QueueStatus.HOLD
+
+    }
+
+}
