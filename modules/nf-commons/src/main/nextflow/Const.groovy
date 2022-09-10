3a6f477104c7ebba21dd25c5e30d37d6cc243457
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-09-10 17:01:06 +0200

    [release 22.09.3-edge] Update timestamp and build number
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -53,17 +53,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "22.09.2-edge"
+    static public final String APP_VER = "22.09.3-edge"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1662579369570
+    static public final long APP_TIMESTAMP = 1662821706240
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5764
+    static public final int APP_BUILDNUM = 5767
 
     /**
      * The app build time string relative to UTC timezone
