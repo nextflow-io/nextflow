ce7fa651c5fd2c0d7164ad0ecf0e2ef4e13ba8fd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-09-19 19:05:55 +0200

    Add wave interactive debug session
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/fusion/FusionHelper.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/fusion/FusionHelper.groovy
@@ -17,11 +17,15 @@
 
 package nextflow.executor.fusion
 
+import java.nio.file.Path
+
 import groovy.transform.CompileStatic
 import groovy.transform.Memoized
 import nextflow.Session
 import nextflow.container.ContainerBuilder
 import nextflow.container.ContainerConfig
+import nextflow.extension.FilesEx
+import nextflow.io.BucketParser
 
 /**
  * Helper method to handle fusion common logic
@@ -70,4 +74,23 @@ class FusionHelper {
 
         return ['sh', '-c', containerCmd]
     }
+
+    static Path toContainerMount(Path path, String scheme, Set<String> buckets) {
+        if( path == null )
+            return null
+
+        final p = BucketParser.from( FilesEx.toUriString(path) )
+
+        if( p.scheme != scheme )
+            throw new IllegalArgumentException("Unexpected path for Fusion script launcher: ${path.toUriString()}")
+
+        final result = "/fusion/$p.scheme/${p.bucket}${p.path}"
+        buckets.add(p.bucket)
+        return Path.of(result)
+    }
+
+    static Path toContainerMount(Path path, String scheme) {
+        return toContainerMount(path, scheme, new HashSet<String>(1))
+    }
+
 }
