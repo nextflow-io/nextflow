059183f24e613790378fddee3c0e6c283fbabfed
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-10-16 22:36:45 +0200

    Fix K8s config namespace is not applied
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/k8s/client/ConfigDiscovery.groovy
+++ modules/nextflow/src/main/groovy/nextflow/k8s/client/ConfigDiscovery.groovy
@@ -87,10 +87,13 @@ class ConfigDiscovery {
         final token = path('/var/run/secrets/kubernetes.io/serviceaccount/token').text
         final namespace = path('/var/run/secrets/kubernetes.io/serviceaccount/namespace').text
 
-        if( namespace && namespace != cfgNamespace )
-            log.warn ("K8s namespace provided in the nextflow configuration does not match with pod namespace")
-
-        new ClientConfig( server: server, token: token, namespace: namespace, serviceAccount: serviceAccount, sslCert: cert, isFromCluster: true )
+        return new ClientConfig(
+                server: server,
+                token: token,
+                namespace: cfgNamespace ?: namespace,
+                serviceAccount: serviceAccount,
+                sslCert: cert,
+                isFromCluster: true )
     }
 
     protected Path path(String path) {
