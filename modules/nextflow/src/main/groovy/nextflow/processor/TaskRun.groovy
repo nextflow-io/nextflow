5ddb3d16cecfcd94fc089ad986a1714e589f5cd1
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-11-12 13:27:11 +0100

    Fix unexpected error on task resume [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskRun.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskRun.groovy
@@ -362,6 +362,9 @@ class TaskRun implements Cloneable {
             catch( IllegalStateException e ) {
                 log.debug "Cannot access `tag` property for task: $baseName ($index)"
             }
+            catch( Exception e ) {
+                log.debug "Unable to evaluate `tag` property for task: $baseName ($index)", e
+            }
 
         // fallback on the current task index, however do not set the 'name' attribute
         // so it has a chance to recover the 'sampleId' at next invocation
