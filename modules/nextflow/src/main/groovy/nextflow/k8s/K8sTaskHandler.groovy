4df8c8d296a302e35e4a75cf67964f5959998e69
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-12-05 17:14:27 +0100

    Expose process queue as K8s pod label [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/k8s/K8sTaskHandler.groovy
+++ modules/nextflow/src/main/groovy/nextflow/k8s/K8sTaskHandler.groovy
@@ -280,6 +280,8 @@ class K8sTaskHandler extends TaskHandler implements FusionAwareTask {
         result.'nextflow.io/taskName' = task.getName()
         result.'nextflow.io/processName' = task.getProcessor().getName()
         result.'nextflow.io/sessionId' = "uuid-${executor.getSession().uniqueId}" as String
+        if( task.config.queue )
+            result.'nextflow.io/queue' = task.config.queue
         return result
     }
 
