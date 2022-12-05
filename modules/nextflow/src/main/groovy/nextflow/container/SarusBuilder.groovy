4184539dd45ce3f304fb745cd111366e83aad64c
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-05 22:27:03 +0800

    sarus: edit to author/copyright, 2x files
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/container/SarusBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/container/SarusBuilder.groovy
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-2022, Seqera Labs
- * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ * Copyright 2022, Pawsey Supercomputing Research Centre
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- modules/nextflow/src/test/groovy/nextflow/container/SarusBuilderTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/container/SarusBuilderTest.groovy
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-2022, Seqera Labs
- * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ * Copyright 2022, Pawsey Supercomputing Research Centre
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +19,7 @@ import spock.lang.Specification
 import java.nio.file.Paths
 /**
  *
- * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ * @author Marco De La Pierre <marco.delapierre@gmail.com>
  */
 class SarusrBuilderTest extends Specification {
 
