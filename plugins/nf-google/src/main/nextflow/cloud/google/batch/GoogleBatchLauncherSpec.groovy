23a255310f085206ed52b823b76713c45517dfe0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-01-06 23:04:03 +0700

    Update docs
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-google/src/main/nextflow/cloud/google/batch/GoogleBatchLauncherSpec.groovy
+++ plugins/nf-google/src/main/nextflow/cloud/google/batch/GoogleBatchLauncherSpec.groovy
@@ -10,14 +10,26 @@ package nextflow.cloud.google.batch
 import com.google.cloud.batch.v1.Volume
 
 /**
- *
+ * Defines the operation supported by Google Batch tasks launcher
+ * 
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
 interface GoogleBatchLauncherSpec {
 
+    /**
+     * @return
+     *      A list of string representing the container mounts. Each mount uses the docker
+     *      mount conventional syntax e.g. {@code /mnt/disks/foo/scratch:/mnt/disks/foo/scratch:rw}
+     */
     List<String> getContainerMounts()
 
+    /**
+     * @return A list of Batch {@link Volume} to be made accessible to the container
+     */
     List<Volume> getVolumes()
 
+    /**
+     * @return A string representing the command to be executed by the containerised task
+     */
     String runCommand()
 }
