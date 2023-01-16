a4a263e190f4b7bb73275a180a179dbcaa29c9ef
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-01-16 21:17:37 +0100

    Fix output env var tokenize
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -1497,12 +1497,20 @@ class TaskProcessor {
         final env = workDir.resolve(TaskRun.CMD_ENV).text
         final result = new HashMap(50)
         for(String line : env.readLines() ) {
-            def (k,v) = line.tokenize('=')
+            def (k,v) = tokenize0(line)
+            if (!k) continue
             result.put(k,v)
         }
         return result
     }
 
+    private List<String> tokenize0(String line) {
+        int p=line.indexOf('=')
+        return p==-1
+                ? List.of(line,'')
+                : List.of(line.substring(0,p), line.substring(p+1))
+    }
+    
     /**
      * Collects the process 'std output'
      *
