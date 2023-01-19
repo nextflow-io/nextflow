7907725513f6c26629085a83e62b034cd208eaed
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-01-19 10:33:35 +0100

    Add refresh token to wave request [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
+++ plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
@@ -30,6 +30,7 @@ import groovy.json.JsonOutput
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import nextflow.Session
+import nextflow.SysEnv
 import nextflow.conda.CondaConfig
 import nextflow.extension.FilesEx
 import nextflow.file.FileHelper
@@ -663,6 +664,31 @@ class WaveClientTest extends Specification {
             assert (it[0] as SubmitContainerTokenRequest).towerEndpoint == 'http://foo.com'
         }
 
+    }
+
+    def 'should send request with tower access token and refresh token' () {
+        given:
+        SysEnv.push([TOWER_WORKFLOW_ID:'1234', TOWER_REFRESH_TOKEN: 'xyz', TOWER_ACCESS_TOKEN: 'foo'])
+        and:
+        def config = [wave:[:], tower:[endpoint: 'http://foo.com']]
+        def sess = Mock(Session) {getConfig() >> config }
+        and:
+        def wave = Spy(new WaveClient(sess))
+        def assets = Mock(WaveAssets)
+        def request = new SubmitContainerTokenRequest()
+        when:
+        wave.sendRequest(assets)
+        then:
+        1 * wave.makeRequest(assets) >> request
+        1 * wave.sendRequest(request) >> { List it ->
+            assert (it[0] == request)
+            assert (it[0] as SubmitContainerTokenRequest).towerAccessToken == 'foo'
+            assert (it[0] as SubmitContainerTokenRequest).towerRefreshToken == 'xyz'
+            assert (it[0] as SubmitContainerTokenRequest).towerEndpoint == 'http://foo.com'
+        }
+
+        cleanup:
+        SysEnv.pop()
     }
 
     // --== launch web server ==--
