5580b844ddf8a28cf256702d257827f36bb59f1b
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-25 16:18:45 +0800

    fix for CondaCache
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/conda/CondaCache.groovy
+++ modules/nextflow/src/main/groovy/nextflow/conda/CondaCache.groovy
@@ -116,8 +116,8 @@ class CondaCache {
         if( config.useMicromamba )
             useMicromamba = config.useMicromamba as boolean
 
-        if( config.getChannels() )
-            channels = config.getChannels()
+        if( config.channels )
+            channels = config.channels
     }
 
     /**
