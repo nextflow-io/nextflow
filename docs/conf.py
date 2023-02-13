84761f286ab739d4aaa0177b3e19f3e49ee25ae6
Marcel Ribeiro Dantas <mribeirodantas@seqera.io>
2023-02-13 12:59:35 -0300

    Automatically update year in the copyright notice at the bottom [ci skip] (#3641)
    Signed-off-by: Marcel Ribeiro-Dantas <mribeirodantas@seqera.io>

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 
 import sys, os
 import sphinx_rtd_theme
+import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -45,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Nextflow'
-copyright = u'2022, Seqera Labs, S.L'
+copyright = str(datetime.date.today().year) + u', Seqera Labs, S.L'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
