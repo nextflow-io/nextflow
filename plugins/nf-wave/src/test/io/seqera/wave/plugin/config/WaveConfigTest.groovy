474a867b686022eeeea74cffc2a57c073300ce92
Marco De La Pierre <marco.delapierre@gmail.com>
2023-02-15 01:43:59 +0000

    WaveSpack: updated Spack version in builderimage
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- plugins/nf-wave/src/test/io/seqera/wave/plugin/config/WaveConfigTest.groovy
+++ plugins/nf-wave/src/test/io/seqera/wave/plugin/config/WaveConfigTest.groovy
@@ -110,7 +110,7 @@ class WaveConfigTest extends Specification {
         when:
         def opts = new WaveConfig([:])
         then:
-        opts.spackOpts().builderImage == 'spack/ubuntu-jammy:v0.19.0'
+        opts.spackOpts().builderImage == 'spack/ubuntu-jammy:v0.19.1'
         opts.spackOpts().runnerImage == 'ubuntu:22.04'
         opts.spackOpts().osPackages == ''
         opts.spackOpts().cFlags == '-O3'
