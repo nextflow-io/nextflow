49fe25783df458b91b3e6991ce1dbd90bee21f3d
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-02-17 17:34:10 +0100

    Bump fusion 2.1
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
+++ plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
@@ -756,9 +756,9 @@ class WaveClientTest extends Specification {
         
         where:
         CONFIG                                      | EXPECTED
-        [:]                                         | 'https://fusionfs.seqera.io/releases/v2.0-amd64.json'
-        [wave:[containerPlatform: 'arm64']]         | 'https://fusionfs.seqera.io/releases/v2.0-arm64.json'
-        [wave:[containerPlatform: 'linux/arm64']]   | 'https://fusionfs.seqera.io/releases/v2.0-arm64.json'
-        [wave:[containerPlatform: 'linux/arm64/v8']]    | 'https://fusionfs.seqera.io/releases/v2.0-arm64.json'
+        [:]                                         | 'https://fusionfs.seqera.io/releases/v2.1-amd64.json'
+        [wave:[containerPlatform: 'arm64']]         | 'https://fusionfs.seqera.io/releases/v2.1-arm64.json'
+        [wave:[containerPlatform: 'linux/arm64']]   | 'https://fusionfs.seqera.io/releases/v2.1-arm64.json'
+        [wave:[containerPlatform: 'linux/arm64/v8']]    | 'https://fusionfs.seqera.io/releases/v2.1-arm64.json'
     }
 }
