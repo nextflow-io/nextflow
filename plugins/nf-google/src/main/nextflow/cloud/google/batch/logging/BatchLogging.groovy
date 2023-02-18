fe9fc6287b8f1eee398a0f3c0d8d69e15ffb05f2
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-02-18 11:26:17 +0100

    Update copyright info
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-google/src/main/nextflow/cloud/google/batch/logging/BatchLogging.groovy
+++ plugins/nf-google/src/main/nextflow/cloud/google/batch/logging/BatchLogging.groovy
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-2022, Seqera Labs
- * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ * Copyright 2023, Seqera Labs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
