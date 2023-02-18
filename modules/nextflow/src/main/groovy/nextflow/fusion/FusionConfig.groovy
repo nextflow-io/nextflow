cf3fd747a992897e0edccd8a7583299ede749071
Jordi Deu-Pons <jordi@jordeu.net>
2023-02-18 16:26:54 +0100

    Improve wave debug-task plugin command
    This commit adds the ability to specify a custom container 
    when debugging a wave-based container task. 
    
    When providing the container name, the nextflow cache is not 
    needed to launch the task execution  
    
    Signed-off-by: Jordi Deu-Pons <jordi@jordeu.net>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/fusion/FusionConfig.groovy
+++ modules/nextflow/src/main/groovy/nextflow/fusion/FusionConfig.groovy
@@ -30,6 +30,8 @@ class FusionConfig {
     final static public String DEFAULT_FUSION_AMD64_URL = 'https://fusionfs.seqera.io/releases/v2.1-amd64.json'
     final static public String DEFAULT_FUSION_ARM64_URL = 'https://fusionfs.seqera.io/releases/v2.1-arm64.json'
 
+    final static public String FUSION_PATH = '/usr/bin/fusion'
+
     final private Boolean enabled
     final private String containerConfigUrl
     final private Boolean exportAwsAccessKeys
