752e3df3e91aace856774aa4abd437d06b67a0d3
lindenb <plindenbaum@yahoo.fr>
2023-04-04 18:02:27 +0200

    added jsonPath parameter, tests, update doc

--- modules/nextflow/src/main/groovy/nextflow/splitter/JsonSplitter.groovy
+++ modules/nextflow/src/main/groovy/nextflow/splitter/JsonSplitter.groovy
@@ -24,21 +24,27 @@ import com.google.gson.stream.JsonToken;
 /**
  * Split a Json file in records
  *
- * @author Pierre Lindenbaum PhD
+ * @author Pierre Lindenbaum PhD Institut-du-Thorax Nantes France.
  */
 @Slf4j
 @CompileStatic
 @InheritConstructors
 class JsonSplitter extends AbstractTextSplitter {
+    public static final String OBJECT_KEY = "key";
+    public static final String OBJECT_VALUE = "value";
+
     /**
-     * use lenient json parsing for JsonReader
+     * json path to find a specific region of the json
      */
-    private boolean lenient  = false;
+    private String jsonPath;
+
+    /** number of items processed so far */
+    private long itemsCount = 0L;
 
     /**
      * Set the splitter options by specifying a map of named parameters.
      * Valid parameters are:
-     * <li>{@code lenient}
+     * <li>{@code jsonPath}
      *
      * @param options
      * @return The splitter instance itself
@@ -46,7 +52,9 @@ class JsonSplitter extends AbstractTextSplitter {
     @Override
     JsonSplitter options(Map opts) {
         super.options(opts)
-        this.lenient = opts.lenient == true		
+        if (opts.jsonPath) {
+            this.jsonPath = (opts.jsonPath as String)
+        }
         return this
     }
 
@@ -57,9 +65,11 @@ class JsonSplitter extends AbstractTextSplitter {
     @Override
     protected Map<String,?> validOptions() {
         def result = super.validOptions()
-	result.remove('file')       // <-- `file` mode not support by JsonSplitter (not sure about this)
-        result.remove('compress')   // <-- `compress` mode not supported
-        result.put("lenient",false);
+        result.remove('compress')
+        result.remove('keepHeader')
+        result.remove('by')
+        result.remove('elem')
+        result.jsonPath= [String]
         return result
     }
 
@@ -67,177 +77,234 @@ class JsonSplitter extends AbstractTextSplitter {
     protected process( Reader  reader) {
         def result = null
         counter.reset() // <-- make sure to start
-		itemsCount = 0
+        itemsCount = 0L;
         try {
             final JsonReader jsonreader = new JsonReader(reader);
-            jsonreader.setLenient(this.lenient);
-        
-            final JsonToken token = jsonreader.peek();
-            if (token.equals(JsonToken.BEGIN_ARRAY)) {
-            	processArray(jsonreader);
-            	}
-            else if(token.equals(JsonToken.BEGIN_OBJECT)) {
-            	processObject(jsonreader);
-            	}
-            else {
-            	throw new IOException("Expected JSON to be an object or an array but got " + token);
-            	}
-			/* extra content at the end ? */
-			if(jsonreader.hasNext()) {
-				final JsonToken last =  jsonreader.peek();
-				if(!token.equals(JsonToken.END_DOCUMENT)) {
-						log.warn("Extra content at the end of the json stream");
-						}
-				}
+            result = scanJsonForPath(jsonreader,
+                    "",
+                    (this.jsonPath==null?"":this.jsonPath)
+                    );
             jsonreader.close();
+            if (result!=null) {
+                // make sure to process collected entries
+                if ( collector && collector.hasChunk() ) {
+                    result = invokeEachClosure(closure, collector.nextChunk())
+                }
             }
+        }
+        catch (Exception ex) {
+            log.warn "Something was wrong when parsing JSON. reason: ${ex.message ?: ex}", ex
+            throw ex;
+        }
         finally {
             reader.closeQuietly()
             if( collector instanceof Closeable )
                 collector.closeQuietly()
         }
-
         return result
     }
+
+    /** recursive loop over the json tree until  this.jsonPath was found
+     * @param reader the JsonReader
+     * @param currentPath json path. Root is an empty string
+     * @param expectedPath expected Path
+     * @returns the output of processArray/processObject or null
+     */
+    private scanJsonForPath(
+            JsonReader reader,
+            String currentPath,
+            String expectedPath
+    ) {
+        // did we reach the expected path ?
+        boolean got_path =  currentPath.equals(expectedPath)
+
+        JsonToken token = reader.peek();
+        if( token.equals(JsonToken.END_DOCUMENT)) {
+            return null;
+        }
+        else if (token.equals(JsonToken.BEGIN_ARRAY)) {
+            if(got_path) {
+                return processArray(reader);
+            }
+            else /* loop over the current array */
+            {
+                int item_index = 0;
+                reader.beginArray();
+                while(reader.hasNext()) {
+                    token = reader.peek();
+                    /* end of array */
+                    if ( token.equals(JsonToken.END_ARRAY) ) {
+                        break;
+                    }
+                    def ret = scanJsonForPath(reader, currentPath + "[" + item_index + "]", expectedPath );
+                    if (ret!=null) return ret;
+                    item_index++;
+                }
+                reader.endArray();
+            }
+        }
+        else if(token.equals(JsonToken.BEGIN_OBJECT)) {
+            if(got_path) {
+                return processObject(reader);
+            }
+            else /* loop over the current object */
+            {
+                reader.beginObject();
+                while(reader.hasNext()) {
+                    token = reader.peek();
+                    /* end of array */
+                    if ( token.equals(JsonToken.END_OBJECT) ) {
+                        break;
+                    }
+                    final String key = reader.nextName();
+                    def ret = scanJsonForPath(reader, currentPath + (currentPath.endsWith(".") || currentPath.isEmpty()? "": ".") + key, expectedPath);
+                    if (ret!=null) return ret;
+                }
+                reader.endObject();
+            }
+        }
+        else {
+            if(got_path)  throw new IOException(
+                "Expected JSON to be an object or an array but got " +
+                token.name() + " for json-path \"" + currentPath +"\"."
+                );
+            reader.skipValue();
+        }
+        return null;
+    }
+
 
     /** process and emit each record of a JSON array */
     private processArray( JsonReader reader ) {
         def result = null
-    	reader.beginArray();
-    	while(reader.hasNext()) {
-	    	final JsonToken token = reader.peek();
-	    	/* end of array */
-	    	if ( token.equals(JsonToken.END_ARRAY) ) {
-	    		reader.endArray();
-	    		break;
-	    		}
-		 	final Object value = fromJson( reader );
-		 	result = processChunk( value )
-			 
-			 // -- check the limit of allowed records has been reached
-			 if( limit>0 && ++itemsCount == limit )
-				 break
-			}
-	
-	 // make sure to process collected entries
-	    if ( collector && collector.hasChunk() ) {
-		result = invokeEachClosure(closure, collector.nextChunk())
-	    }
-	
-	return result;
+        reader.beginArray();
+        while(reader.hasNext()) {
+            final JsonToken token = reader.peek();
+            /* end of array */
+            if ( token.equals(JsonToken.END_ARRAY) ) {
+                reader.endArray();
+                break;
+            }
+            final Object value = fromJson( reader );
+            result = processChunk( value )
+
+            // -- check the limit of allowed records has been reached
+            if( limit>0 && ++itemsCount == limit ) {
+                break
+            }
+        }
+
+        return result;
     }
-    
+
     /** process and emit each record of a JSON object */
     private processObject( JsonReader reader ) {
         def result = null
-    	reader.beginObject();
-    	while(reader.hasNext()) {
-    	final JsonToken token = reader.peek();
-    	/* end of array */
-    	if ( token.equals(JsonToken.END_OBJECT) ) {
-    		reader.endObject();
-    		break;
-    		}
-    	 final Map<String,Object> map = new HashMap<>();
-    	 final String key = reader.nextName();
-    	 final Object value = fromJson(reader);
-    	 map.put(key,value);
-    	  // -- apply the splitting logic for the fetched record
-         result = processChunk( map )
-		 
-		 // -- check the limit of allowed records has been reached
-		 if( limit>0 && ++itemsCount == limit )
-			 break
-    	 }
+        reader.beginObject();
+        while(reader.hasNext()) {
+            final JsonToken token = reader.peek();
+            /* end of array */
+            if ( token.equals(JsonToken.END_OBJECT) ) {
+                reader.endObject();
+                break;
+            }
+            final Map<String,Object> map = new HashMap<>(2);
+            final String key = reader.nextName();
+            final Object value = fromJson(reader);
+            map.put(OBJECT_KEY, key);
+            map.put(OBJECT_VALUE, value);
+            // -- apply the splitting logic for the fetched record
+            result = processChunk( map )
 
-	 // make sure to process collected entries
-	    if ( collector && collector.hasChunk() ) {
-		result = invokeEachClosure(closure, collector.nextChunk())
-	    }
-
-    	 return result;
+            // -- check the limit of allowed records has been reached
+            if( limit>0 && ++itemsCount == limit ) {
+                break
+            }
+        }
+        return result;
     }
-    
-    
+
+
     @Override
     protected fetchRecord(BufferedReader reader) {
-    	throw new IllegalStateException("should never be called");
+        throw new IllegalStateException("should never be called");
     }
-    
+
     /** convert a json stream to an Object */
     static Object fromJson(final JsonReader reader ) {
-    	final JsonToken token = reader.peek();
-    	switch(token) {
-    		case JsonToken.NULL : {
-    			reader.nextNull();
-    			return null;
-    			}
-    		case JsonToken.STRING : {
-    			return reader.nextString();
-    			}
-    		case JsonToken.BOOLEAN : {
-    			return reader.nextBoolean();
-    			}
-    		case JsonToken.NUMBER : {
-    			final String s = reader.nextString();
-    			// look like an integer ?
-    			if(!s.contains(".")) {
-    				// first try as int
-	    			try {
-	    				int v = Integer.parseInt(s);
-	    				return v;
-	    				}
-	    			catch (NumberFormatException ex) {
-						//otherwise try as long
-		    			try {
-		    				long v = Long.parseLong(s);
-		    				return v;
-		    				}
-		    			catch (NumberFormatException ex2) {
-		    				//ignore
-		    				}
-						}
-	    			}
-	    		// try as double
-	    		try {
-    				double v = Double.parseDouble(s);
-    				return v;
-    				}
-    			catch (NumberFormatException ex) {
-    				//ignore
-    				}
-    			//everything failed, return as string
-    			return s;
-    			}
-    		case JsonToken.BEGIN_ARRAY : {
-    			reader.beginArray();
-    			final List<Object> array = new ArrayList<>();
-    			for(;;) {
-    				final JsonToken token2 = reader.peek();
-    				if(token2.equals(JsonToken.END_ARRAY)) {
-    					reader.endArray();
-    					break;
-    					}
-    				array.add(fromJson(reader));
-    				}
-    			return array;
-    			}
-    		case JsonToken.BEGIN_OBJECT : {
-    			reader.beginObject();
-    			final Map<String,Object> object = new LinkedHashMap<>();
-    			for(;;) {
-    				final JsonToken token2 = reader.peek();
-    				if(token2.equals(JsonToken.END_OBJECT)) {
-    					reader.endObject();
-    					break;
-    					}
-    				final String key = reader.nextName();
-    				final Object value = fromJson(reader);
-    				object.put(key,value);
-    				}
-    			return object;
-    			}
-    		default: throw new IllegalStateException("Got json event: " + token);
-	    	}
-	    }
-	}
+        final JsonToken token = reader.peek();
+        switch(token) {
+            case JsonToken.NULL : {
+                reader.nextNull();
+                return null;
+            }
+            case JsonToken.STRING : {
+                return reader.nextString();
+            }
+            case JsonToken.BOOLEAN : {
+                return reader.nextBoolean();
+            }
+            case JsonToken.NUMBER : {
+                final String s = reader.nextString();
+                // look like an integer ?
+                if(!s.contains(".")) {
+                    // first try as int
+                    try {
+                        int v = Integer.parseInt(s);
+                        return v;
+                    }
+                    catch (NumberFormatException ex) {
+                        //otherwise try as long
+                        try {
+                            long v = Long.parseLong(s);
+                            return v;
+                        }
+                        catch (NumberFormatException ex2) {
+                            //ignore
+                        }
+                    }
+                }
+                // try as double
+                try {
+                    double v = Double.parseDouble(s);
+                    if(Double.isInfinite(v) || Double.isNaN(v)) return s;
+                    return v;
+                }
+                catch (NumberFormatException ex) {
+                    //ignore
+                }
+                //everything failed, return as string
+                return s;
+            }
+            case JsonToken.BEGIN_ARRAY : {
+                reader.beginArray();
+                final List<Object> array = new ArrayList<>();
+                for(;;) {
+                    final JsonToken token2 = reader.peek();
+                    if(token2.equals(JsonToken.END_ARRAY)) {
+                        reader.endArray();
+                        break;
+                    }
+                    array.add(fromJson(reader));
+                }
+                return array;
+            }
+            case JsonToken.BEGIN_OBJECT : {
+                reader.beginObject();
+                final Map<String,Object> object = new LinkedHashMap<>();
+                for(;;) {
+                    final JsonToken token2 = reader.peek();
+                    if(token2.equals(JsonToken.END_OBJECT)) {
+                        reader.endObject();
+                        break;
+                    }
+                    final String key = reader.nextName();
+                    final Object value = fromJson(reader);
+                    object.put(key,value);
+                }
+                return object;
+            }
+            default: throw new IllegalStateException("Got json event: " + token);
+        }
+    }
+}
--- modules/nextflow/src/test/groovy/nextflow/splitter/JsonSplitterTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/splitter/JsonSplitterTest.groovy
@@ -21,108 +21,158 @@ import java.nio.charset.Charset
 import com.google.gson.stream.JsonReader;
 
 /**
- *
- * @author Pierre Lindenbaum PhD
+ * @author Pierre Lindenbaum PhD Institut-du-Thorax Nantes France.
  */
 class JsonSplitterTest extends Specification {
 
     def json_array = '''
-        	[
-		    {
-			"beaconId": "se.nbis.swefreq",
-			"apiVersion": 1.0,
-			"exists": false,
-			"alleleRequest": {
-			    "referenceName": "MT",
-			    "referenceBases": "T",
-			    "assemblyId": "GRCh38",
-			    "includeDatasetResponses": "NONE",
-			    "alternateBases": "C",
-			    "start": 9
-			},
-			"datasetAlleleResponses": [],
-			"beaconHandover": []
-		    },
-		    {
-			"beaconId": "fi.rahtiapp.beaconpy-elixirbeacon",
-			"apiVersion": 1,
-			"exists": true,
-			"alleleRequest": {
-			    "referenceName": "MT",
-			    "referenceBases": "T",
-			    "assemblyId": "GRCh38",
-			    "includeDatasetResponses": "NONE",
-			    "alternateBases": "C",
-			    "start": 9
-			},
-			"datasetAlleleResponses": []
-		    },
-		    {
-			"beaconId": "fi.rahtiapp.staging-elixirbeacon",
-			"apiVersion": "1.1.0",
-			"exists": true,
-			"alleleRequest": {
-			    "referenceName": "MT",
-			    "referenceBases": "T",
-			    "assemblyId": "GRCh38",
-			    "includeDatasetResponses": "NONE",
-			    "alternateBases": "C",
-			    "start": 9
-			},
-			"datasetAlleleResponses": []
-		    }
-		]
+            [
+            {
+            "beaconId": "se.nbis.swefreq",
+            "apiVersion": 1.0,
+            "exists": false,
+            "alleleRequest": {
+                "referenceName": "MT",
+                "referenceBases": "T",
+                "assemblyId": "GRCh38",
+                "includeDatasetResponses": "NONE",
+                "alternateBases": "C",
+                "start": 9
+            },
+            "datasetAlleleResponses": [],
+            "beaconHandover": []
+            },
+            {
+            "beaconId": "fi.rahtiapp.beaconpy-elixirbeacon",
+            "apiVersion": 1,
+            "exists": true,
+            "alleleRequest": {
+                "referenceName": "MT",
+                "referenceBases": "T",
+                "assemblyId": "GRCh38",
+                "includeDatasetResponses": "NONE",
+                "alternateBases": "C",
+                "start": 9
+            },
+            "datasetAlleleResponses": []
+            },
+            {
+            "beaconId": "fi.rahtiapp.staging-elixirbeacon",
+            "apiVersion": "1.1.0",
+            "exists": true,
+            "alleleRequest": {
+                "referenceName": "MT",
+                "referenceBases": "T",
+                "assemblyId": "GRCh38",
+                "includeDatasetResponses": "NONE",
+                "alternateBases": "C",
+                "start": 9
+            },
+            "datasetAlleleResponses": []
+            }
+        ]
         '''
 
     def json_object = '''
-    	    {
+            {
                 "seq_id": "NC_000008.9",
                 "position": 19864003,
                 "deleted_sequence": "C",
-                "inserted_sequence": "G"
+                "inserted_sequence": ["G",["C","A"]]
             }
-    	'''
-
+        '''
 
     def testSplitArray() {
-
         when:
-		def items = new JsonSplitter().target(json_array).list()
+        def items = new JsonSplitter().target(json_array).list()
         then:
-		items.size() == 3
-		items[0] instanceof Map
-		items[0].get("beaconId").equals("se.nbis.swefreq");
-		items[0].get("exists") instanceof Boolean
-		items[0].get("apiVersion") instanceof Double
-		items[1].get("apiVersion") instanceof Integer
-
+        
+        items.size() == 3
+        items[0] instanceof Map
+        items[0].get("beaconId").equals("se.nbis.swefreq");
+        items[0].get("exists") instanceof Boolean
+        items[0].get("apiVersion") instanceof Double
+        items[1].get("apiVersion") instanceof Integer
     }
-    
+
+    def testSplitArrayLimit() {
+        when:
+        def items = new JsonSplitter(limit:2).target(json_array).list()
+        
+        then:
+        items.size() == 2
+        items[0].get("beaconId").equals("se.nbis.swefreq");
+    }
+
+
     def testSplitObject() {
-
         when:
-        	def items = new JsonSplitter().target(json_object).list()
+        def items = new JsonSplitter().target(json_object).list()
 
         then:
-		items.size() == 4
-		items[0] instanceof Map
-		items[0].get("seq_id").equals("NC_000008.9");
-		items[1].get("position") == 19864003
+        items.size() == 4
+        items[0] instanceof Map
+        items[0].get(JsonSplitter.OBJECT_KEY).equals("seq_id");
+        items[0].get(JsonSplitter.OBJECT_VALUE).equals("NC_000008.9");
+        items[1].get(JsonSplitter.OBJECT_KEY).equals("position");
+        items[1].get(JsonSplitter.OBJECT_VALUE) == 19864003
     }
-    
-    def testNotAJsonFile() {
 
+    def testSplitObjectLimit() {
         when:
-        	def items = new JsonSplitter().target("[not a json string").list()
+        def items = new JsonSplitter(limit:1).target(json_object).list()
+
         then:
-      		 thrown(com.google.gson.stream.MalformedJsonException)
+        items.size() == 1
+        items[0].get(JsonSplitter.OBJECT_KEY).equals("seq_id");
+        items[0].get(JsonSplitter.OBJECT_VALUE).equals("NC_000008.9");
     }
-    
-    private static Object parseJsonString(String str) {
-        StringReader r = new StringReader(str);
+
+
+    def testJsonPathArray1() {
+        when:
+        def items = new JsonSplitter("jsonPath":"[0].alleleRequest").
+        target(json_array).list()
+
+        then:
+        items.size() == 6
+        items[0].get(JsonSplitter.OBJECT_KEY).equals("referenceName");
+        items[0].get(JsonSplitter.OBJECT_VALUE).equals("MT");
+    }
+
+
+    def testJsonPathObject1() {
+        when:
+        def items = new JsonSplitter("jsonPath":"inserted_sequence[1]").
+        target(json_object).list()
+
+        then:
+        items.size() == 2
+        items[0].equals("C")
+        items[1].equals("A")
+    }
+
+
+    def testFromJson() {
+        when:
+        StringReader r = new StringReader(
+                "[true,false,null,1,10000000000,1E1000,1.0,[],{}]"
+                );
         JsonReader jr = new JsonReader(r);
-       
+        def items = JsonSplitter.fromJson(jr);
+        jr.close();
         r.close();
+        
+        then:
+        items instanceof List
+        items[0] == true
+        items[1] == false
+        items[2] == null
+        items[3] instanceof Integer
+        items[4] instanceof Long
+        items[5] instanceof String
+        items[6] instanceof Double
+        items[7] instanceof List
+        items[8] instanceof Map
     }
-    
 }
