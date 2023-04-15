2769e60cebe53af4eb9e5bd61653d146761c0d59
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-04-15 09:20:58 +0200

    [release 23.04.1] Update timestamp and build number [ci skip]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -52,17 +52,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "23.04.0"
+    static public final String APP_VER = "23.04.1"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1680383394638
+    static public final long APP_TIMESTAMP = 1681541460707
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5857
+    static public final int APP_BUILDNUM = 5866
 
     /**
      * The app build time string relative to UTC timezone
