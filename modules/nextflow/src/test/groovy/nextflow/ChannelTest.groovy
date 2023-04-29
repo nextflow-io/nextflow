80b414f33eabcdf5affc363a43331ad718173b0c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-04-29 18:26:52 +0200

    Rollback warnings [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/test/groovy/nextflow/ChannelTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/ChannelTest.groovy
@@ -167,6 +167,36 @@ class ChannelTest extends Specification {
         result.val == Channel.STOP
     }
 
+    def testFrom() {
+        given:
+        DataflowQueue result
+
+        when:
+        result = Channel.from('hola')
+        then:
+        result.val == 'hola'
+        result.val == Channel.STOP
+
+        when:
+        result = Channel.from('alpha','delta')
+        then:
+        result.val == 'alpha'
+        result.val == 'delta'
+        result.val == Channel.STOP
+
+        when:
+        result = Channel.from(['alpha','delta'])
+        then:
+        result.val == 'alpha'
+        result.val == 'delta'
+        result.val == Channel.STOP
+
+        when:
+        result = Channel.from([])
+        then:
+        result.val == Channel.STOP
+    }
+
     def testSingleFile() {
 
         when:
