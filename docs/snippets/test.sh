7cdb425f5309151526b8b0fb436754c2547d32de
Ben Sherman <bentshermann@gmail.com>
2023-05-25 16:05:53 -0500

    Fix sonatype warnings
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- docs/snippets/test.sh
+++ docs/snippets/test.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 NXF_CMD=${NXF_CMD:-nextflow}
-NXF_FILES=${1:-'*.nf'}
+NXF_FILES=${@:-'*.nf'}
 
 for pipeline in $NXF_FILES ; do
 
     echo "> Running test: $pipeline"
 
     # run the pipeline
-    $NXF_CMD -q run $pipeline > .out
+    $NXF_CMD -q run "$pipeline" > .out
 
     # verify output (if ground truth exists)
-    outfile="`basename $pipeline .nf`.out"
+    outfile="$(basename "$pipeline" .nf).out"
 
-    if [[ -f $outfile ]] ; then
-        sort $outfile > a.out
+    if [[ -f "$outfile" ]] ; then
+        sort "$outfile" > a.out
         sort .out > b.out
         diff a.out b.out
     else
