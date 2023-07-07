9a52f8480342f2ad5682f9036d965f8e604abbcd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-07-07 07:55:05 +0200

    Fix failing test
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-azure/src/test/nextflow/executor/BashWrapperBuilderWithAzTest.groovy
+++ plugins/nf-azure/src/test/nextflow/executor/BashWrapperBuilderWithAzTest.groovy
@@ -171,7 +171,9 @@ class BashWrapperBuilderWithAzTest extends Specification {
                 while ((i<${#cmd[@]})); do
                     local copy=()
                     for x in "${pid[@]}"; do
-                      [[ -e /proc/$x ]] && copy+=($x)
+                      # if the process exist, keep in the 'copy' array, otherwise wait on it to capture the exit code
+                      # see https://github.com/nextflow-io/nextflow/pull/4050
+                      [[ -e /proc/$x ]] && copy+=($x) || wait $x
                     done
                     pid=("${copy[@]}")
             
