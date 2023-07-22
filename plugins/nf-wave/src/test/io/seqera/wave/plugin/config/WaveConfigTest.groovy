a39055f77565a689124ee94f08b280061db52d06
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-07-22 16:43:13 +0200

    Revert "Increase wave timeout to 60 mins"
    This reverts commit 903e4d906b9e7d3c51797e6db4a777e1913b7424.

--- plugins/nf-wave/src/test/io/seqera/wave/plugin/config/WaveConfigTest.groovy
+++ plugins/nf-wave/src/test/io/seqera/wave/plugin/config/WaveConfigTest.groovy
@@ -186,7 +186,7 @@ class WaveConfigTest extends Specification {
         when:
         def opts = new WaveConfig([:])
         then:
-        opts.httpOpts().connectTimeout() == java.time.Duration.ofSeconds(60)
+        opts.httpOpts().connectTimeout() == java.time.Duration.ofSeconds(30)
 
         when:
         opts = new WaveConfig([httpClient: [connectTimeout: '90s']])
