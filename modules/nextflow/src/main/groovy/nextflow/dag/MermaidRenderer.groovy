5eb9de1cfed6e3b17cbd7938c1504e95e2607105
Ben Sherman <bentshermann@gmail.com>
2023-07-25 06:40:37 -0500

    Improve DAG rendering
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/dag/MermaidRenderer.groovy
+++ modules/nextflow/src/main/groovy/nextflow/dag/MermaidRenderer.groovy
@@ -19,6 +19,9 @@ package nextflow.dag
 import java.nio.file.Path
 
 import groovy.transform.CompileStatic
+import nextflow.Global
+import nextflow.Session
+import nextflow.script.WorkflowMetadata
 
 /**
  * Render the DAG using the Mermaid format to the specified file.
@@ -28,6 +31,8 @@ import groovy.transform.CompileStatic
  */
 @CompileStatic
 class MermaidRenderer implements DagRenderer {
+
+    private WorkflowMetadata metadata = ((Session)Global.session).workflowMetadata
 
     @Override
     void renderWorkflowGraph(DAG dag, Path file) {
@@ -78,46 +83,78 @@ class MermaidRenderer implements DagRenderer {
 
     @Override
     void renderTaskGraph(TaskDAG dag, Path file) {
-        def renderedOutputs = [] as Set<Path>
-        def numInputs = 0
-        def numOutputs = 0
-
         def lines = []
         lines << "flowchart TD"
 
-        // render tasks and task inputs
+        // render workflow inputs
+        final inputs = [:] as Map<Path,String>
+
+        lines << "    subgraph \" \""
+
         dag.vertices.each { task, vertex ->
-            // render task vertex
-            lines << "    ${vertex.getSlug()}[\"${vertex.label}\"]"
+            vertex.inputs.each { name, path ->
+                if( dag.getProducerVertex(path) || path in inputs )
+                    return
+
+                inputs[path] = "in${inputs.size()}".toString()
+                lines << "    ${inputs[path]}[\"${normalizePath(path)}\"]"
+            }
+        }
+
+        lines << "    end"
+
+        // render tasks
+        final taskOutputs = [] as Set<Path>
+
+        dag.vertices.each { task, vertex ->
+            lines << "    ${vertex.getSlug()}([\"${vertex.label}\"])"
 
             vertex.inputs.each { name, path ->
                 // render task input from predecessor task
                 final pred = dag.getProducerVertex(path)
                 if( pred != null ) {
                     lines << "    ${pred.getSlug()} -->|${path.name}| ${vertex.getSlug()}"
-                    renderedOutputs << path
+                    taskOutputs << path
                 }
 
                 // render task input from source node
                 else {
-                    numInputs += 1
-                    lines << "    i${numInputs}(( )) -->|${path}| ${vertex.getSlug()}"
+                    lines << "    ${inputs[path]} --> ${vertex.getSlug()}"
                 }
             }
         }
 
-        // render task outputs with sink nodes
+        // render task outputs
+        final outputs = [:] as Map<Path,String>
+
         dag.vertices.each { task, vertex ->
             vertex.outputs.each { path ->
-                if( path !in renderedOutputs ) {
-                    numOutputs += 1
-                    lines << "    ${vertex.getSlug()} -->|${path.name}| o${numOutputs}(( ))"
-                }
+                if( path in taskOutputs || path in outputs )
+                    return
+
+                outputs[path] = "out${outputs.size()}".toString()
+                lines << "    ${vertex.getSlug()} --> ${outputs[path]}"
             }
         }
+
+        // render workflow outputs
+        lines << "    subgraph \" \""
+
+        outputs.each { path, label ->
+            lines << "    ${label}[${path.name}]"
+        }
+
+        lines << "    end"
 
         lines << ""
 
         file.text = lines.join('\n')
+    }
+
+    String normalizePath(Path path) {
+        path.toUriString()
+            .replace(metadata.workDir.toUriString(), '/work')
+            .replace(metadata.projectDir.toUriString(), '')
+            .replace(metadata.launchDir.toUriString(), '')
     }
 }
