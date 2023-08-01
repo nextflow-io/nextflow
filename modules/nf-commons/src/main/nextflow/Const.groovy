eb357e868a7c028bab75454e9de7efee16f5f23a
Aaron Fishman <uk.afishman@gmail.com>
2023-08-01 15:29:08 +0000

    reverting Const.groovy
    Signed-off-by: Aaron Fishman <uk.afishman@gmail.com>

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -57,12 +57,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1690879865426
+    static public final long APP_TIMESTAMP = 1690040653541
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5872
+    static public final int APP_BUILDNUM = 5870
 
     /**
      * The app build time string relative to UTC timezone
