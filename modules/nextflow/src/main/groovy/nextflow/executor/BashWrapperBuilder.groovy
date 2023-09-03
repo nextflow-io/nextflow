9a30c3d02341eb3cfda6eee0f970f9c76e2e122e
Robert Syme <rob.syme@gmail.com>
2023-09-03 13:42:31 -0400

    Allow setting shell directive when using the trace file. (#4210)
    Signed-off-by: Rob Syme <rob.syme@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/executor/BashWrapperBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/BashWrapperBuilder.groovy
@@ -468,7 +468,7 @@ class BashWrapperBuilder {
         final traceWrapper = isTraceRequired()
         if( traceWrapper ) {
             // executes the stub which in turn executes the target command
-            launcher = "/bin/bash ${fileStr(wrapperFile)} nxf_trace"
+            launcher = "${interpreter} ${fileStr(wrapperFile)} nxf_trace"
         }
         else {
             launcher = "${interpreter} ${fileStr(scriptFile)}"
