ab1877a30f4dd0e6bc2ddc572f696561e8d74005
Ben Sherman <bentshermann@gmail.com>
2023-09-07 10:27:30 -0500

    Always emit publish event for cached task outputs (#4227)
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/PublishDir.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/PublishDir.groovy
@@ -378,11 +378,10 @@ class PublishDir {
             if( checkSourcePathConflicts(destination))
                 return
             
-            if( !overwrite )
-                return
-
-            FileHelper.deletePath(destination)
-            processFileImpl(source, destination)
+            if( overwrite ) {
+                FileHelper.deletePath(destination)
+                processFileImpl(source, destination)
+            }
         }
 
         notifyFilePublish(destination, source)
