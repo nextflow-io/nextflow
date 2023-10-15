7814822c98f86690e8d239280bbb8457fa7053b7
Ben Sherman <bentshermann@gmail.com>
2023-10-15 04:47:47 -0500

    Update logos, fonts, and colors (#4407)
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -136,12 +136,12 @@ html_css_files = ['theme.css']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/nextflow-logo.png'
+html_logo = '_static/nextflow-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/favicon.ico'
+html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -217,7 +217,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'images/nextflow-logo.png'
+latex_logo = '_static/nextflow-logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
