a2c69f387ae7fac3933c40889dbca9cc9818dedc
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-11-21 13:58:58 +0100

    Add support for low-priority in scaling formula
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-azure/src/test/nextflow/cloud/azure/batch/AzBatchServiceTest.groovy
+++ plugins/nf-azure/src/test/nextflow/cloud/azure/batch/AzBatchServiceTest.groovy
@@ -237,6 +237,18 @@ class AzBatchServiceTest extends Specification {
         formula.contains '$TargetDedicatedNodes = lifespan < interval ? 3 : targetPoolSize;'
     }
 
+    def 'should check scaling formula for low-priority' () {
+        given:
+        def exec = Mock(AzBatchExecutor) { getConfig() >> new AzConfig([:]) }
+        def svc = new AzBatchService(exec)
+
+        when:
+        def formula = svc.scaleFormula( new AzPoolOpts(lowPriority: true, vmCount: 3, maxVmCount: 10, scaleInterval: Duration.of('5 min')) )
+        then:
+        formula.contains 'interval = TimeInterval_Minute * 5;'
+        formula.contains '$TargetLowPriorityNodes = lifespan < interval ? 3 : targetPoolSize;'
+    }
+
     def 'should  check formula vars' () {
         given:
         def exec = Mock(AzBatchExecutor) { getConfig() >> new AzConfig([:]) }
