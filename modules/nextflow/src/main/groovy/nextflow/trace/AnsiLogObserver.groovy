7cd9c1481991c3dc3de46d21726849ba5236195d
Ben Sherman <bentshermann@gmail.com>
2024-01-11 04:53:08 -0600

    Fix Ignore stored process message when ansi log is enabled (#4645)
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/trace/AnsiLogObserver.groovy
+++ modules/nextflow/src/main/groovy/nextflow/trace/AnsiLogObserver.groovy
@@ -121,7 +121,9 @@ class AnsiLogObserver implements TraceObserver {
         boolean warn
         if( isHashLogPrefix(message) && !(warn=message.indexOf('NOTE:')>0) )
             return
-        
+        if( message.startsWith('[skipping]') )
+            return
+
         if( !started || !statsObserver.hasProgressRecords() ) {
             println message
         }
