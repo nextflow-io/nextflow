98d29cee086bc7c163d73f6c3a6617260009f2da
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2024-05-02 22:23:47 +0200

    Fix script error text alignment (#4681)
    Signed-off-by: Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>
    Co-authored-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -1328,9 +1328,10 @@ class TaskProcessor {
         else
             message = err0(error.cause)
 
+        message.eachLine { line ->
+            result << '  ' << line << '\n'
+        }
         result
-            .append('  ')
-            .append(message)
             .append('\n')
             .toString()
     }
