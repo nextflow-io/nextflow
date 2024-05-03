73676870fee7e81efa64aff1c5af440b52096d82
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-05-03 10:15:02 +0200

    Use for instead eachLine in error formatting [ci fast]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -1335,9 +1335,10 @@ class TaskProcessor {
         else
             message = err0(error.cause)
 
-        message.eachLine { line ->
+        for( String line : message.readLines() ) {
             result << '  ' << line << '\n'
         }
+
         result
             .append('\n')
             .toString()
