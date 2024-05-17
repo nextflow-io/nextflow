f7b4cc0ae08a4493e56a0efdbe440887b9fc0232
Ben Sherman <bentshermann@gmail.com>
2024-05-17 05:12:30 -0500

    Workflow output definition (#4784)
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Signed-off-by: Dr Marco Claudio De La Pierre <marco.delapierre@gmail.com>

--- modules/nextflow/src/test/groovy/nextflow/script/WorkflowDefTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/script/WorkflowDefTest.groovy
@@ -161,54 +161,6 @@ class WorkflowDefTest extends Dsl2Spec {
 
     }
 
-
-    def 'should capture publish defs' () {
-
-        given:
-        def config = new CompilerConfiguration()
-        config.setScriptBaseClass(TestScript.class.name)
-        config.addCompilationCustomizers( new ASTTransformationCustomizer(NextflowDSL))
-
-        def SCRIPT = '''
-                    
-            workflow {
-              publish: 
-                foo
-                bar to: 'some/path'
-                baz.out to: 'other/path'
-              main:   
-                x = 1 
-            }
-        '''
-
-        when:
-        def script = (TestScript)new GroovyShell(new ScriptBinding(), config).parse(SCRIPT).run()
-        then:
-        thrown(MultipleCompilationErrorsException)
-    }
-
-    def 'should not allow publish is sub-workflow' () {
-
-        given:
-        def config = new CompilerConfiguration()
-        config.setScriptBaseClass(TestScript.class.name)
-        config.addCompilationCustomizers( new ASTTransformationCustomizer(NextflowDSL))
-
-        def SCRIPT = '''
-                    
-            workflow alpha {
-              publish: foo
-              main:   
-                x = 1 
-            }
-        '''
-
-        when:
-        new GroovyShell(config).parse(SCRIPT)
-        then:
-        thrown(MultipleCompilationErrorsException)
-    }
-
     def 'should report malformed workflow block' () {
 
         given:
