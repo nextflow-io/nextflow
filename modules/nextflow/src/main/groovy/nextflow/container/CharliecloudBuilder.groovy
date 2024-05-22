ceffd7edf3c6bb279793153d0db06444956a9548
Niklas Schandry <schandry@gmail.com>
2024-05-22 13:52:09 +0200

    swap builder back

--- modules/nextflow/src/main/groovy/nextflow/container/CharliecloudBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/container/CharliecloudBuilder.groovy
@@ -16,287 +16,301 @@
 
 package nextflow.container
 
-import java.nio.file.FileSystems
 import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.concurrent.ConcurrentHashMap
 
-import groovy.transform.CompileStatic
-import groovy.transform.PackageScope
-import groovy.util.logging.Slf4j
-import groovyx.gpars.dataflow.DataflowVariable
-import groovyx.gpars.dataflow.LazyDataflowVariable
-import nextflow.Global
-import nextflow.file.FileMutex
-import nextflow.util.Duration
+import nextflow.executor.BashWrapperBuilder
+import nextflow.util.Escape
+import nextflow.util.MemoryUnit
+import nextflow.util.PathTrie
 /**
- * Handle caching of remote Charliecloud images
+ * Base class for container wrapper builders
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
- * @author Patrick Hüther <patrick.huether@gmail.com>
  */
-@Slf4j
-@CompileStatic
-class CharliecloudCache {
-
-    static final private Map<String,DataflowVariable<Path>> localImageNames = new ConcurrentHashMap<>()
-
-    private ContainerConfig config
-
-    private Map<String,String> env
-
-    private boolean missingCacheDir
-
-    private Duration pullTimeout = Duration.of('20min')
-
-    private String registry
-
-    /** Only for debugging purpose - do not use */
-    @PackageScope
-    CharliecloudCache() {}
+abstract class ContainerBuilder<V extends ContainerBuilder> {
 
     /**
-     * Create a Charliecloud cache object
-     *
-     * @param config A {@link ContainerConfig} object
-     * @param env The environment configuration object. Specifying {@code null} the current system environment is used
+     * Create a builder instance given the container engine
      */
-    CharliecloudCache(ContainerConfig config, Map<String,String> env=null) {
-        this.config = config
-        this.env = env ?: System.getenv()
+    static ContainerBuilder create(String engine, String containerImage) {
+        if( engine == 'docker' )
+            return new DockerBuilder(containerImage)
+        if( engine == 'podman' )
+            return new PodmanBuilder(containerImage)
+        if( engine == 'singularity' )
+            return new SingularityBuilder(containerImage)
+        if( engine == 'apptainer' )
+            return new ApptainerBuilder(containerImage)
+        if( engine == 'udocker' )
+            return new UdockerBuilder(containerImage)
+        if( engine == 'sarus' )
+            return new SarusBuilder(containerImage)
+        if( engine == 'shifter' )
+            return new ShifterBuilder(containerImage)
+        if( engine == 'charliecloud' )
+            return new CharliecloudBuilder(containerImage)
+        //
+        throw new IllegalArgumentException("Unknown container engine: $engine")
     }
 
-    /**
-     * Given a remote image URL string returns a normalised name used to store
-     * the image in the local file system
-     *
-     * @param imageUrl
-     *          A Docker remote image url. {@code pditommaso/foo:latest}
-     * @return
-     *          A file name corresponding to the image URL eg. {@code pditommaso-foo.img}
-     */
-    @PackageScope
-    String simpleName(String imageUrl) {
-        def p = imageUrl.indexOf('://')
-        def name = p != -1 ? imageUrl.substring(p+3) : imageUrl
-        
-        // add registry
-        if( registry ) {        
-            if( !registry.endsWith('/') ) {
-                registry += '/'
-            }
-            name = registry + name
-        }
-        name = name.replace(':','+').replace('/','%')
-        return name 
-    }
+    final protected List env = []
 
-    /**
-     * Check if the specified directory exists and if it is valid
-     * Charliecloud should take care of creating it (and expects to have done so: https://hpc.github.io/charliecloud/faq.html#storage-directory-seems-invalid)
-     *
-     * @param
-     *      str A path string representing a folder where to store the charliecloud containers once downloaded
-     * @return
-     *      the directory as a {@link Path} object
-     */
-    @PackageScope
-    Path checkDir(String str) {
-        def result = Paths.get(str)
-        if( !result.exists() ) {
-            log.info "Charliecloud cache directory: $str does not exist -- Charliecloud will attempt to initialize it at the specified location"
-        } 
-        else if( !result.resolve('img').exists() || !result.resolve('dlcache').exists() ) {
-            throw new IOException("Charliecloud cache directory exists but seems invalid: $str -- See https://hpc.github.io/charliecloud/faq.html#storage-directory-seems-invalid")
-        }
-        return result.toAbsolutePath()
-    }
+    final protected List<Path> mounts = []
 
-    /**
-     * Retrieve the directory where to store the charliecloud images once downloaded.
-     * It tries these settings in the following order:
-     * 1) {@code charliecloud.cacheDir} setting in the nextflow config file
-     * 2) the {@code NXF_CHARLIECLOUD_CACHEDIR} environment variable
-     * 3) the {@code $workDir/charliecloud} path
-     *
-     * @return
-     *      the {@code Path} where store the charliecloud images as flattened directories
-     */
-    @PackageScope
-    Path getCacheDir() {
+    protected List<String> runOptions = []
 
-        if( config.pullTimeout )
-            pullTimeout = config.pullTimeout as Duration
+    protected List<String> engineOptions = []
 
-        def str = config.cacheDir as String
+    protected Integer cpus
+
+    protected String cpuset
+
+    protected String memory
+
+    protected String temp
+
+    protected String image
+
+    protected Path workDir
+
+    protected boolean readOnlyInputs
+
+    @Deprecated
+    protected String entryPoint
+
+    protected String runCommand
+
+    protected boolean mountWorkDir = true
+
+    protected boolean privileged
+
+    String getImage() { image }
+
+    V addRunOptions(String str) {
         if( str )
-            return checkDir(str)
+            runOptions.add(str)
+        return (V)this
+    }
 
-        str = env.get('NXF_CHARLIECLOUD_CACHEDIR')
+    V addEngineOptions(String str) {
         if( str )
-            return checkDir(str)
-
-        str = env.get('CH_IMAGE_STORAGE')
-        if( str )
-            return checkDir(str)
-
-        def workDir = Global.session.workDir
-        if( workDir.fileSystem != FileSystems.default ) {
-            throw new IOException("Charliecloud cannot store image in a remote work directory -- Use a POSIX compatible work directory or specify an alternative path with the `NXF_CHARLIECLOUD_CACHEDIR` env variable")
-        }
-
-        missingCacheDir = true
-        def result = workDir.resolve('charliecloud')
-
-        return result
+            engineOptions.add(str)
+        return (V)this
     }
+
+    V setCpus(Integer value) {
+        this.cpus = value
+        return (V)this
+    }
+
+    V setCpuset(String value) {
+        this.cpuset = value
+        return (V)this
+    }
+
+    V setMemory( value ) {
+        if( value instanceof MemoryUnit )
+            this.memory = "${value.toMega()}m"
+
+        else if( value instanceof String )
+            this.memory = value
+
+        else
+            throw new IllegalArgumentException("Not a supported memory value")
+
+        return (V)this
+    }
+
+    V setWorkDir( Path path ) {
+        this.workDir = path
+        return (V)this
+    }
+
+    V setName(String name) {
+        return (V)this
+    }
+
+    V setTemp( String value ) {
+        this.temp = value
+        return (V)this
+    }
+
+    abstract V params( Map config )
+
+    abstract V build(StringBuilder result)
 
     /**
-     * Get the path on the file system where a remote charliecloud image is stored at
-     *
-     * @param imageUrl The charliecloud remote URL
-     * @return the container image local {@link Path}
+     * @return The command string to run a container from Docker image
      */
-    @PackageScope
-    Path localImagePath(String imageUrl) {
-        getCacheDir().resolve('img').resolve(simpleName(imageUrl))
+    String getRunCommand() {
+        if( !runCommand ) throw new IllegalStateException("Missing `runCommand` -- make sure `build` method has been invoked")
+        runCommand
     }
 
-    /**
-     * Run ch-image to pull a remote image and store it in the file system.
-     * Requires charliecloud 0.28 or later.
-     *
-     * @param imageUrl The docker image remote URL
-     * @return  the container image local {@link Path}
-     */
-    @PackageScope
-    Path downloadCharliecloudImage(String imageUrl) {
-        final localPath = localImagePath(imageUrl)
+    String getRunCommand(String launcher) {
+        def run = getRunCommand()
+        if( !run )
+            throw new IllegalStateException("Missing `runCommand` -- make sure `build` method has been invoked")
 
-        if( localPath.exists() ) {
-            log.debug "Charliecloud found local store for image=$imageUrl; path=$localPath"
-            return localPath
-        }
-
-        int count = 0;
-        int maxTries = 5;
-        boolean imagePulled = false
-        while(!imagePulled) {
-            try {
-                downloadCharliecloudImage0(imageUrl, localPath)
-                imagePulled = true
-            } catch (e) {
-                if (++count == maxTries) throw e
-                log.info "Another image is currently pulled. Attempting again in 30 seconds [$count/$maxTries]"
-                Thread.sleep(30000)
-            }
-        }   
-        return localPath
-
-    }
-
-
-    @PackageScope
-    Path downloadCharliecloudImage0(String imageUrl, Path targetPath) {
-
-        if( targetPath.exists() ) {
-            // If we're here we're an additional process that has waited for the pulling
-            // before we got the mutex to advance here.
-            log.debug "Charliecloud found local store for image=$imageUrl; path=$targetPath"
-            return targetPath
-        }
-        log.trace "Charliecloud pulling remote image `$imageUrl`"
-
-        if( missingCacheDir )
-            log.warn1 "Charliecloud cache directory has not been defined -- Remote image will be stored in the path: $targetPath.parent.parent -- Use the charliecloud.cacheDir config option or set the NXF_CHARLIECLOUD_CACHEDIR variable to specify a different location"
-
-        log.info "Charliecloud pulling image $imageUrl [cache $targetPath]"
-            
-        String cmd = "ch-image pull -s $targetPath.parent.parent $imageUrl > /dev/null"
-        try {
-            runCommand( cmd, targetPath )
-            log.debug "Charliecloud pull complete image=$imageUrl path=$targetPath"
-        }
-        catch( Exception e ){
-            // clean-up to avoid to keep eventually corrupted container
-            targetPath.deleteDir()
-            throw e
-        }
-        return targetPath
-    }
-
-    @PackageScope
-    int runCommand( String cmd, Path storePath ) {
-        log.trace """Charliecloud pull
-                     command: $cmd
-                     timeout: $pullTimeout
-                     folder : $storePath""".stripIndent(true)
-
-        final max = pullTimeout.toMillis()
-        final builder = new ProcessBuilder(['bash','-c',cmd])
-        builder.environment().remove('CH_IMAGE_STORAGE')
-        final proc = builder.start()
-        final err = new StringBuilder()
-        final consumer = proc.consumeProcessErrorStream(err)
-        proc.waitForOrKill(max)
-        def status = proc.exitValue()
-        if( status != 0 ) {
-            consumer.join()
-            def msg = "Charliecloud failed to pull image\n  command: $cmd\n  status : $status\n  hint   : Try and increase charliecloud.pullTimeout in the config (current is \"${pullTimeout}\")\n  message:\n"
-            msg += err.toString().trim().indent('    ')
-            throw new IllegalStateException(msg)
-        }
-        return status
-    }
-
-    /**
-     * Given a remote image URL returns a {@link DataflowVariable} which holds
-     * the local image path.
-     *
-     * This method synchronise multiple concurrent requests so that only one
-     * image download is actually executed.
-     *
-     * @param imageUrl The Docker image remote URL
-     * @return The {@link DataflowVariable} which holds (and pulls) the local image directory
-     */
-    @PackageScope
-    DataflowVariable<Path> getLazyImagePath(String imageUrl) {
-        if( imageUrl in localImageNames ) {
-            log.trace "Charliecloud found local cache for image `$imageUrl`"
-            return localImageNames[imageUrl]
-        }
-
-        synchronized (localImageNames) {
-            def result = localImageNames[imageUrl]
-            if( result == null ) {
-                result = new LazyDataflowVariable<Path>({ downloadCharliecloudImage(imageUrl) })
-                localImageNames[imageUrl] = result
-            }
-            else {
-                log.trace "Charliecloud found local cache for image `$imageUrl` (2)"
-            }
+        if( launcher ) {
+            def result = run
+            result += entryPoint ? " $entryPoint -c \"$launcher\"" : " $launcher"
             return result
         }
+        return run + ' ' + launcher
     }
 
-    /**
-     * Pull remote image, caching the resulting flattened directory in the file system.
-     *
-     * This method synchronise multiple concurrent requests so that only one
-     * image download is actually executed.
-     *
-     * @param url The docker image remote URL
-     * @return the container image local {@link Path}
-     */
-    Path getCachePathFor(String url) {
-        def promise = getLazyImagePath(url)
-        def result = promise.getVal()
-        if( promise.isError() )
-            throw new IllegalStateException(promise.getError())
-        if( !result )
-            throw new IllegalStateException("Charliecloud cannot pull image `$url`")
-        log.trace "Charliecloud cache for `$url` path=$result"
+    String getKillCommand() { BashWrapperBuilder.KILL_CMD }
+
+    String getRemoveCommand() { return null }
+
+    @Deprecated
+    final StringBuilder appendHelpers( StringBuilder wrapper ) {
+        final result = getScriptHelpers()
+        if( result ) wrapper.append(result)
+        return wrapper
+    }
+
+    String getScriptHelpers() {
+        return null
+    }
+
+    V build() {
+        build(new StringBuilder())
+    }
+
+    V addEnv( entry ) {
+        env.add(entry instanceof GString ? entry.toString() : entry)
+        return (V)this
+    }
+
+    V addMount( Path path ) {
+        if( path )
+            mounts.add(path)
+        return (V)this
+    }
+
+    V addMounts( List<Path> paths ) {
+        if( paths ) for( Path it : paths )
+            mounts.add(it)
+        return (V)this
+    }
+
+    V addMountForInputs( Map<String,Path> inputFiles ) {
+        mounts.addAll( inputFilesToPaths(inputFiles) )
+        return (V)this
+    }
+
+    V addMountWorkDir(boolean flag) {
+        this.mountWorkDir = flag
+        return (V)this
+    }
+
+    static List<Path> inputFilesToPaths( Map<String,Path> inputFiles ) {
+
+        List<Path> files = []
+        inputFiles.each { name, storePath ->
+
+            def path = storePath.getParent()
+            if( path ) files << path
+
+        }
+        return files
+    }
+
+
+    protected CharSequence appendEnv( StringBuilder result ) {
+        for( Object e : env ) {
+            makeEnv(e, result) << ' '
+        }
         return result
     }
 
+    /**
+     * Get the env command line option for the give environment definition
+     *
+     * @param env
+     * @param result
+     * @return
+     */
+    protected StringBuilder makeEnv( env, StringBuilder result = new StringBuilder() ) {
+
+        if( env instanceof Map ) {
+            short index = 0
+            for( Map.Entry entry : env.entrySet() ) {
+                if( index++ ) result << ' '
+                result << ("-e \"${entry.key}=${entry.value}\"")
+            }
+        }
+        else if( env instanceof String && env.contains('=') ) {
+            result << '-e "' << env << '"'
+        }
+        else if( env instanceof String ) {
+            result << "-e \"$env\""
+        }
+        else if( env ) {
+            throw new IllegalArgumentException("Not a valid environment value: $env [${env.class.name}]")
+        }
+
+        return result
+    }
+
+
+    /**
+     * Get the volumes command line options for the given list of input files
+     *
+     * @param mountPaths
+     * @param binDir
+     * @param result
+     * @return
+     */
+    protected CharSequence makeVolumes(List<Path> mountPaths, StringBuilder result = new StringBuilder() ) {
+
+        // add the work-dir to the list of container mounts
+        final workDirStr = workDir?.toString()
+        final allMounts = new ArrayList<Path>(mountPaths)
+        if( workDir )
+            allMounts << workDir
+
+        // find the longest commons paths and mount only them
+        final trie = new PathTrie()
+        for( String it : allMounts ) { trie.add(it) }
+
+        // when mounts are read-only make sure to remove the work-dir path
+        final paths = trie.longest()
+        if( readOnlyInputs && workDirStr && paths.contains(workDirStr) )
+            paths.remove(workDirStr)
+
+        for( String it : paths ) {
+            if(!it) continue
+            result << composeVolumePath(it,readOnlyInputs)
+            result << ' '
+        }
+
+        // when mounts are read-only, make sure to include the work-dir as writable
+        if( readOnlyInputs && workDir ) {
+            result << composeVolumePath(workDirStr)
+            result << ' '
+        }
+
+        // -- append by default the current path -- this is needed when `scratch` is set to true
+        if( mountWorkDir ) {
+            result << composeVolumePath('$NXF_TASK_WORKDIR')
+            result << ' '
+        }
+
+        return result
+    }
+
+    protected String composeVolumePath( String path, boolean readOnly = false ) {
+        return "-v ${escape(path)}:${escape(path)}${mountFlags(readOnly)}"
+    }
+
+    protected String escape(String path) {
+        path.startsWith('$') ? "\"$path\"" : Escape.path(path)
+    }
+
+    protected String mountFlags(boolean readOnly) {
+        readOnly ? ":ro" : ''
+    }
 }
