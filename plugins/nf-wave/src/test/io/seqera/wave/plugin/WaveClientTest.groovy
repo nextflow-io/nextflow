e410be47c775c28f604107ebd0a0aac64bc7167b
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-06-15 15:55:14 +0200

    Fix support for s5cmd 2.2.2
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
+++ plugins/nf-wave/src/test/io/seqera/wave/plugin/WaveClientTest.groovy
@@ -1092,11 +1092,11 @@ class WaveClientTest extends Specification {
 
         where:
         ARCH                | EXPECTED
-        'linux/amd64'       | 'https://nf-xpack.seqera.io/s5cmd/linux_amd64_2.0.0.json'
-        'linux/x86_64'      | 'https://nf-xpack.seqera.io/s5cmd/linux_amd64_2.0.0.json'
-        'arm64'             | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.0.0.json'
-        'linux/arm64'       | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.0.0.json'
-        'linux/arm64/v8'    | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.0.0.json'
+        'linux/amd64'       | 'https://nf-xpack.seqera.io/s5cmd/linux_amd64_2.2.2.json'
+        'linux/x86_64'      | 'https://nf-xpack.seqera.io/s5cmd/linux_amd64_2.2.2.json'
+        'arm64'             | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.2.2.json'
+        'linux/arm64'       | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.2.2.json'
+        'linux/arm64/v8'    | 'https://nf-xpack.seqera.io/s5cmd/linux_arm64_2.2.2.json'
     }
 
     def 'should configure custom s5cmd' () {
