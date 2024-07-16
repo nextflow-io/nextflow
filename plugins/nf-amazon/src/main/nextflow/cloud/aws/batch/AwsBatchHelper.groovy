ddb15dab726ed1e5cc9714b9911f0834cffe2e1f
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-07-16 11:33:38 +0200

    Fix AWS Cloudwatch access when using custom log group name
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-amazon/src/main/nextflow/cloud/aws/batch/AwsBatchHelper.groovy
+++ plugins/nf-amazon/src/main/nextflow/cloud/aws/batch/AwsBatchHelper.groovy
@@ -198,7 +198,7 @@ class AwsBatchHelper {
      *      The Batch jobs as a string value or {@code null} if no logs is available. Note, if the log
      *      is made of multiple *page* this method returns only the first one
      */
-    String getTaskLogStream(String jobId) {
+    String getTaskLogStream(String jobId, String groupName) {
         final streamId = getLogStreamId(jobId)
         if( !streamId ) {
             log.debug "Unable to find CloudWatch log stream for batch job id=$jobId"
@@ -206,7 +206,7 @@ class AwsBatchHelper {
         }
 
         final logRequest = new GetLogEventsRequest()
-                .withLogGroupName("/aws/batch/job")
+                .withLogGroupName(groupName ?: "/aws/batch/job")
                 .withLogStreamName(streamId)
 
         final result = new StringBuilder()
