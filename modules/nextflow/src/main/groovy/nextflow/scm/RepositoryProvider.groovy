e792dfbe1e1d1a8249e71cc7b5ed0d4e9ee6fe96
Nathan Johnson <nath.p.johnson@gmail.com>
2024-11-11 16:54:02 +0000

    feat: nextflow.scm.RepositoryProvider.revision now public Refs: PLAT-252

--- modules/nextflow/src/main/groovy/nextflow/scm/RepositoryProvider.groovy
+++ modules/nextflow/src/main/groovy/nextflow/scm/RepositoryProvider.groovy
@@ -65,7 +65,7 @@ abstract class RepositoryProvider {
     /**
      * The name of the commit/branch/tag
      */
-    protected String revision
+    public String revision
 
     RepositoryProvider setCredentials(String userName, String password) {
         config.user = userName
