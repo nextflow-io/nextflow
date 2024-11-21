3ea05c51237b2383829875279931e53c666d2029
Jorge Ejarque <jorgee@users.noreply.github.com>
2024-11-21 08:12:45 +0100

    Fix possible deadlock in dynamic maxRetry resolution [ci fast] (#5474)
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/test/groovy/nextflow/processor/TaskConfigTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/processor/TaskConfigTest.groovy
@@ -157,8 +157,8 @@ class TaskConfigTest extends Specification {
 
         where:
         value   | expected
-        null    | 0
-        0       | 0
+        null    | 1
+        0       | 1
         1       | 1
         '3'     | 3
         10      | 10
@@ -171,8 +171,8 @@ class TaskConfigTest extends Specification {
         when:
         config = new TaskConfig()
         then:
-        config.maxRetries == 0
-        config.getMaxRetries() == 0
+        config.maxRetries == 1
+        config.getMaxRetries() == 1
         config.getErrorStrategy() == ErrorStrategy.TERMINATE
 
         when:
