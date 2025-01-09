938608284c8e592cd22fde9b67dcca6dcf6ee2b6
Robert Syme <rob.syme@gmail.com>
2025-01-09 11:11:07 -0500

    Fix Only close the cache db if it is not null (#5631)
    Signed-off-by: Rob Syme <rob.syme@gmail.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/Session.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Session.groovy
@@ -1209,10 +1209,8 @@ class Session implements ISession {
         if( aborted || cancelled || error )
             return
 
-        CacheDB db = null
-        try {
-            log.trace "Cleaning-up workdir"
-            db = CacheFactory.create(uniqueId, runName).openForRead()
+        log.trace "Cleaning-up workdir"
+        try (CacheDB db = CacheFactory.create(uniqueId, runName).openForRead()) {
             db.eachRecord { HashCode hash, TraceRecord record ->
                 def deleted = db.removeTaskEntry(hash)
                 if( deleted ) {
@@ -1223,10 +1221,7 @@ class Session implements ISession {
             log.trace "Clean workdir complete"
         }
         catch( Exception e ) {
-            log.warn("Failed to cleanup work dir: ${workDir.toUriString()}")
-        }
-        finally {
-            db.close()
+            log.warn("Failed to cleanup work dir: ${workDir.toUriString()}", e)
         }
     }
 
