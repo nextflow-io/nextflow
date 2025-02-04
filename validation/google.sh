0d98442d61cc6a305c3a34d4275d7ed9752e7b10
Jorge Ejarque <jorgee@users.noreply.github.com>
2025-02-04 21:32:36 +0100

    Fix Mount input file buckets in task arrays for Google Batch (#5739)
    Signed-off-by: Christian Romberg <christian.romberg@mbiomics.com>
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Christian Romberg <christian.romberg@mbiomics.com>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- validation/google.sh
+++ validation/google.sh
@@ -12,6 +12,10 @@ export GOOGLE_APPLICATION_CREDENTIALS=$PWD/google_credentials.json
 [[ $TOWER_ACCESS_TOKEN ]] && OPTS='-with-tower' || OPTS=''
 set -x
 
+$NXF_CMD -C ./google.config -q run ./test-arrays.nf > array_output
+[[ `grep 'Hi from the nf-test-array bucket!' -c array_output` == 3 ]] && echo OK || { echo 'Failed array tasks' && false; }
+
+
 $NXF_CMD -C ./google.config \
     run ./test-readspair.nf \
     -with-report \
@@ -47,7 +51,7 @@ $NXF_CMD -C ./google.config run ./test-complexpaths.nf -resume
 [[ -e 'foo/sample_(1 2).vcf' ]] || false
 
 ## run test-subdirs inputs/outputs
-$NXF_CMD -C ./gls.config run ./test-subdirs.nf
+$NXF_CMD -C ./gls.config -q run ./test-subdirs.nf
 
 ## run publishDir overwrite
 $NXF_CMD -C ./gls.config run ./test-overwrite.nf
