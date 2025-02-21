8cd7f3fd820caccf031b327fe2439f6114e0b725
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2025-02-21 12:00:21 +0100

    Update Azure batch defaults
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- plugins/nf-azure/src/test/nextflow/cloud/azure/batch/AzBatchServiceTest.groovy
+++ plugins/nf-azure/src/test/nextflow/cloud/azure/batch/AzBatchServiceTest.groovy
@@ -452,7 +452,7 @@ class AzBatchServiceTest extends Specification {
         then:
         1 * svc.guessBestVm(LOC, CPUS, MEM, null, TYPE) >> VM
         and:
-        spec.poolId == 'nf-pool-ea3f8ad6099d4e16f02692625f137486-Standard_X1'
+        spec.poolId == 'nf-pool-e3331cce25aa1563d6046b3de9ec2d93-Standard_X1'
         spec.metadata == [foo: 'bar']
 
     }
