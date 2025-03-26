4e2e3a00cbf2a202c4f0ec280775673f9573011f
Florian Wuennemann <flowuenne@gmail.com>
2025-03-26 03:44:14 +0000

    Added stable GPU metrics to TraceRecord.

--- modules/nextflow/src/main/groovy/nextflow/trace/TraceRecord.groovy
+++ modules/nextflow/src/main/groovy/nextflow/trace/TraceRecord.groovy
@@ -103,7 +103,10 @@ class TraceRecord implements Serializable {
             vol_ctxt: 'num',        // -- /proc/$pid/status field 'voluntary_ctxt_switches'
             inv_ctxt: 'num',        // -- /proc/$pid/status field 'nonvoluntary_ctxt_switches'
             hostname: 'str',
-            cpu_model:  'str'
+            cpu_model:  'str',
+            gpu_model:  'str',
+            gpu_mem_total: 'str',
+            gpu_driver: 'str'
     ]
 
     static public Map<String,Closure<String>> FORMATTER = [
@@ -451,6 +454,9 @@ class TraceRecord implements Serializable {
                         this.put(name, val)
                         break
 
+                    case 'gpu_model':
+                    case 'gpu_mem_total':
+                    case 'gpu_driver':
                     case 'cpu_model':
                         this.put(name, value)
                         break
