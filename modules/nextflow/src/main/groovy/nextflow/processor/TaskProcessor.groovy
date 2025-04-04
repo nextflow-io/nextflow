fa54d6a9131d768a379e484f2cd724b28b7ae30e
kdesnos <kdesnos@insa-rennes.fr>
2025-04-04 15:29:40 +1100

    Log: print info on processes inputs and outputs.
    Signed-off-by: kdesnos <kdesnos@insa-rennes.fr>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -599,7 +599,14 @@ class TaskProcessor {
             return
         }
         session.addIgniter {
-            log.debug "Starting process > $name"
+            // Closure to get the (nested) list of param_type:param_name
+            def getParamNamesAndTypes 
+            getParamNamesAndTypes = { paramList ->
+                paramList.collect{param ->
+                    "${(param instanceof InParam)? param.getTypeName() : param.class.simpleName}:${(param !instanceof TupleInParam && param !instanceof TupleOutParam)? param.name : "[${getParamNamesAndTypes(param.getInner())}]"}"}.join(', ')
+            }
+            // Print process name with associated input and outputs
+            log.debug "Starting process > $name (${getParamNamesAndTypes(getConfig().getInputs())}) -> (${getParamNamesAndTypes(getConfig().getOutputs())})"
             op.start()
         }
     }
