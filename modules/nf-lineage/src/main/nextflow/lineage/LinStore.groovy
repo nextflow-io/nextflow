c853bcfe526fbd75cac05517dde76fd1618abbfc
Jorge Ejarque <jorgee@users.noreply.github.com>
2025-05-09 18:43:16 +0200

    Support fromPath with pattern in Lineage Pseudo-FS (#6054) [ci fast]
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>

--- modules/nf-lineage/src/main/nextflow/lineage/LinStore.groovy
+++ modules/nf-lineage/src/main/nextflow/lineage/LinStore.groovy
@@ -20,6 +20,9 @@ import com.google.common.annotations.Beta
 import groovy.transform.CompileStatic
 import nextflow.lineage.serde.LinSerializable
 import nextflow.lineage.config.LineageConfig
+
+import java.nio.file.Path
+
 /**
  * Interface for the lineage store
  *
@@ -62,4 +65,14 @@ interface LinStore extends Closeable {
      */
     Map<String,LinSerializable> search(Map<String, List<String>> params)
 
+    /**
+     * Search for keys starting with a parent key.
+     * For example, if a LinStore contains the following keys: '123abc', '123abc/samples/file1.txt' and '123abc/summary',
+     * The execution of the function with parentKey='123abc' will return a list with '123abc/samples/file1.txt' and '123abc/summary'.
+     * Similarly, the execution of the function with parentKey='123abc/samples' will just return '123abc/samples/file1.txt"
+     *
+     * @param parentKey
+     * @return list of keys
+     */
+    List<String> getSubKeys(String parentKey)
 }
