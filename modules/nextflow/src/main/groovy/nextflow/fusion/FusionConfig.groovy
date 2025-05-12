464a64af1caf33a7c5e8bdfc9ab62583492d2b24
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2025-05-12 09:07:29 +0200

    Add support for ARM and Snapshots [ci fast] (#6057)
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/fusion/FusionConfig.groovy
+++ modules/nextflow/src/main/groovy/nextflow/fusion/FusionConfig.groovy
@@ -37,6 +37,7 @@ class FusionConfig {
     final static public String DEFAULT_FUSION_AMD64_URL = 'https://fusionfs.seqera.io/releases/v2.4-amd64.json'
     final static public String DEFAULT_FUSION_ARM64_URL = 'https://fusionfs.seqera.io/releases/v2.4-arm64.json'
     final static public String DEFAULT_SNAPSHOT_AMD64_URL = 'https://fusionfs.seqera.io/releases/v2.4-snap_amd64.json'
+    final static public String DEFAULT_SNAPSHOT_ARM64_URL = 'https://fusionfs.seqera.io/releases/v2.4-snap_arm64.json'
 
     final static public String DEFAULT_TAGS = "[.command.*|.exitcode|.fusion.*](nextflow.io/metadata=true),[*](nextflow.io/temporary=true)"
 
