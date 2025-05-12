bd2270135088847d19c3f9e05d05ed23ab47278b
Phil Ewels <phil.ewels@seqera.io>
2025-05-12 14:31:57 +0200

    Lint: Make error / warning state more obvious and easier to scan quickly
    Signed-off-by: Phil Ewels <phil.ewels@seqera.io>

--- modules/nextflow/src/main/groovy/nextflow/script/parser/v2/ErrorListener.groovy
+++ modules/nextflow/src/main/groovy/nextflow/script/parser/v2/ErrorListener.groovy
@@ -90,9 +90,10 @@ class StandardErrorListener implements ErrorListener {
 
     @Override
     void onError(SyntaxException error, String filename, SourceUnit source) {
-        term.bold().a(filename).reset()
+        term.reset().fg(Ansi.Color.RED).a(Ansi.Attribute.NEGATIVE_ON).a(" Error ").reset()
+        term.bold().a(" ${filename}").reset()
         term.a(":${error.getStartLine()}:${error.getStartColumn()}: ")
-        term = highlightString("error: ${error.getOriginalMessage()}", term)
+        term = highlightString(error.getOriginalMessage(), term)
         if( mode != 'concise' ) {
             term.newline()
             term = printCodeBlock(source, Range.of(error), term, Ansi.Color.RED)
@@ -103,9 +104,10 @@ class StandardErrorListener implements ErrorListener {
     @Override
     void onWarning(WarningMessage warning, String filename, SourceUnit source) {
         final token = warning.getContext().getRoot()
-        term.bold().a(filename).reset()
+        term.reset().fg(Ansi.Color.YELLOW).a(Ansi.Attribute.NEGATIVE_ON).a(" Warning ").reset()
+        term.bold().a(" ${filename}").reset()
         term.a(":${token.getStartLine()}:${token.getStartColumn()}: ")
-        term.fg(Ansi.Color.YELLOW).a("warning: ${warning.getMessage()}").fg(Ansi.Color.DEFAULT)
+        term = highlightString(warning.getMessage(), term)
         if( mode != 'concise' ) {
             term.newline()
             term = printCodeBlock(source, Range.of(warning), term, Ansi.Color.YELLOW)
