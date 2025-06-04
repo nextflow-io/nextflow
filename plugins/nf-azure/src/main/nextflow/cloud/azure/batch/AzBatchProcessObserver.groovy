08918479dd7674e4568d90612353f9fc7eb8e2a6
adamrtalbot <12817534+adamrtalbot@users.noreply.github.com>
2025-06-04 11:25:59 +0100

    feat: Azure Batch eagerly terminates jobs after all tasks have been submitted
    Azure Batch "job leak" is still an issue. This commit fixes #5839 which allows Nextflow to set jobs to auto terminate when all tasks have been submitted. This means that eventually jobs will move into terminated state even if something prevents nextflow reaching a graceful shutdown. Very early implementation and needs some refinement.
    
    Signed-off-by: adamrtalbot <12817534+adamrtalbot@users.noreply.github.com>

---
+++ plugins/nf-azure/src/main/nextflow/cloud/azure/batch/AzBatchProcessObserver.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021, Microsoft Corp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.cloud.azure.batch
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import nextflow.Session
+import nextflow.processor.TaskProcessor
+import nextflow.trace.TraceObserver
+
+/**
+ * Observer that handles process termination events for Azure Batch executor.
+ * When a process terminates (all tasks have been submitted), this observer 
+ * will eagerly set the corresponding Azure Batch job to auto-terminate when
+ * all tasks complete.
+ *
+ * @author Adam Talbot <adam.talbot@seqera.io>
+ */
+@Slf4j
+@CompileStatic
+class AzBatchProcessObserver implements TraceObserver {
+
+    private Session session
+
+    AzBatchProcessObserver(Session session) {
+        this.session = session
+    }
+
+    /**
+     * Called when a process terminates (all tasks have been submitted).
+     * This method will find the Azure Batch job ID associated with the process
+     * and set it to auto-terminate when all tasks complete.
+     */
+    @Override
+    void onProcessTerminate(TaskProcessor processor) {
+        // Check if this process uses the Azure Batch executor
+        if( !(processor.executor instanceof AzBatchExecutor) ) {
+            return
+        }
+        
+        final executor = processor.executor as AzBatchExecutor
+        final batchService = executor.getBatchService()
+        if( !batchService?.config?.batch()?.terminateJobsOnCompletion ) {
+            log.trace "Azure Batch job auto-termination is disabled, skipping eager termination for process: ${processor.name}"
+            return
+        }
+
+        try {
+            // Find all job IDs associated with this processor
+            final jobIds = batchService.allJobIds.findAll { key, jobId ->
+                key.processor == processor
+            }.values()
+
+            for( String jobId : jobIds ) {
+                log.debug "Setting Azure Batch job ${jobId} to auto-terminate for completed process: ${processor.name}"
+                batchService.setJobAutoTermination(jobId)
+            }
+        }
+        catch( Exception e ) {
+            log.warn "Failed to set auto-termination for Azure Batch jobs associated with process '${processor.name}' - Reason: ${e.message ?: e}"
+        }
+    }
+} 
---
+++ plugins/nf-azure/src/test/nextflow/cloud/azure/batch/AzBatchProcessObserverTest.groovy
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2021, Microsoft Corp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.cloud.azure.batch
+
+import nextflow.Session
+import nextflow.cloud.azure.config.AzBatchOpts
+import nextflow.cloud.azure.config.AzConfig
+import nextflow.executor.Executor
+import nextflow.processor.TaskProcessor
+import spock.lang.Specification
+
+/**
+ * Test for AzBatchProcessObserver
+ *
+ * @author Adam Talbot <adam.talbot@seqera.io>
+ */
+class AzBatchProcessObserverTest extends Specification {
+
+    def 'should skip non-azure batch executors'() {
+        given:
+        def session = Mock(Session)
+        def observer = new AzBatchProcessObserver(session)
+        def executor = Mock(Executor)  // Not an AzBatchExecutor
+        def processor = Mock(TaskProcessor) {
+            getExecutor() >> executor
+        }
+
+        when:
+        observer.onProcessTerminate(processor)
+
+        then:
+        // No exception should be thrown and method should return early
+        noExceptionThrown()
+    }
+
+    def 'should skip when termination is disabled'() {
+        given:
+        def session = Mock(Session)
+        def observer = new AzBatchProcessObserver(session)
+        def config = Mock(AzConfig)
+        def batchOpts = Mock(AzBatchOpts) {
+            terminateJobsOnCompletion >> false
+        }
+        def batchService = Mock(AzBatchService) {
+            getConfig() >> config
+        }
+        def executor = Mock(AzBatchExecutor) {
+            getBatchService() >> batchService
+        }
+        def processor = Mock(TaskProcessor) {
+            getExecutor() >> executor
+            getName() >> 'test-process'
+        }
+        config.batch() >> batchOpts
+
+        when:
+        observer.onProcessTerminate(processor)
+
+        then:
+        0 * batchService.setJobAutoTermination(_)
+    }
+
+    def 'should set job auto-termination when enabled'() {
+        given:
+        def session = Mock(Session)
+        def observer = new AzBatchProcessObserver(session)
+        def config = Mock(AzConfig)
+        def batchOpts = Mock(AzBatchOpts) {
+            terminateJobsOnCompletion >> true
+        }
+        def processor = Mock(TaskProcessor) {
+            getName() >> 'test-process'
+        }
+        def jobKey1 = new AzJobKey(Mock(TaskProcessor), 'pool1')
+        def jobKey2 = new AzJobKey(processor, 'pool1')  // This one should match
+        def jobKey3 = new AzJobKey(Mock(TaskProcessor), 'pool2')
+        def allJobIds = [
+            (jobKey1): 'job1',
+            (jobKey2): 'job2',  // This should be processed
+            (jobKey3): 'job3'
+        ]
+        def batchService = Mock(AzBatchService) {
+            getConfig() >> config
+            getAllJobIds() >> allJobIds
+        }
+        def executor = Mock(AzBatchExecutor) {
+            getBatchService() >> batchService
+        }
+        processor.getExecutor() >> executor
+        config.batch() >> batchOpts
+
+        when:
+        observer.onProcessTerminate(processor)
+
+        then:
+        1 * batchService.setJobAutoTermination('job2')
+    }
+
+    def 'should handle exceptions gracefully'() {
+        given:
+        def session = Mock(Session)
+        def observer = new AzBatchProcessObserver(session)
+        def config = Mock(AzConfig)
+        def batchOpts = Mock(AzBatchOpts) {
+            terminateJobsOnCompletion >> true
+        }
+        def batchService = Mock(AzBatchService) {
+            getConfig() >> config
+            getAllJobIds() >> { throw new RuntimeException('Test error') }
+        }
+        def executor = Mock(AzBatchExecutor) {
+            getBatchService() >> batchService
+        }
+        def processor = Mock(TaskProcessor) {
+            getExecutor() >> executor
+            getName() >> 'test-process'
+        }
+        config.batch() >> batchOpts
+
+        when:
+        observer.onProcessTerminate(processor)
+
+        then:
+        // Should not throw exception
+        noExceptionThrown()
+    }
+} 
