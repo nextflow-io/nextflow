11c2905085ec7e878977664fa1244feb5ee00549
Nathan Johnson <swingingsimian@users.noreply.github.com>
2025-07-25 08:34:59 +0100

    Update Gitea provider to gitea.com as default server (#6285) [ci fast]
    Signed-off-by: Nathan Johnson <nathan.johnson@seqera.io>
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Co-authored-by: Nathan Johnson <nathanjohnson@Nathans-MBP.fritz.box>
    Co-authored-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- modules/nextflow/src/test/groovy/nextflow/scm/GiteaRepositoryProviderTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/scm/GiteaRepositoryProviderTest.groovy
@@ -27,20 +27,9 @@ import spock.lang.Unroll
  */
 class GiteaRepositoryProviderTest extends Specification {
 
-    static final String CONFIG = '''
-        providers {
-            mygitea {
-                server = 'https://gitea.com'
-                endpoint = 'https://gitea.com/api/v1'
-                platform = 'gitea'
-            }
-        }
-        '''
-
     def 'should return repo url' () {
         given:
-        def config = new ConfigSlurper().parse(CONFIG)
-        def obj = new ProviderConfig('gitea', config.providers.mygitea as ConfigObject)
+        def obj = new ProviderConfig('gitea')
 
         expect:
         new GiteaRepositoryProvider('pditommaso/hello', obj).getEndpointUrl() == 'https://gitea.com/api/v1/repos/pditommaso/hello'
@@ -48,8 +37,7 @@ class GiteaRepositoryProviderTest extends Specification {
 
     def 'should return project URL' () {
         given:
-        def config = new ConfigSlurper().parse(CONFIG)
-        def obj = new ProviderConfig('gitea', config.providers.mygitea as ConfigObject)
+        def obj = new ProviderConfig('gitea')
 
         expect:
         new GiteaRepositoryProvider('pditommaso/hello', obj).getRepositoryUrl() == 'https://gitea.com/pditommaso/hello'
@@ -57,8 +45,7 @@ class GiteaRepositoryProviderTest extends Specification {
 
     def 'should return content URL' () {
         given:
-        def config = new ConfigSlurper().parse(CONFIG)
-        def obj = new ProviderConfig('gitea', config.providers.mygitea as ConfigObject)
+        def obj = new ProviderConfig('gitea')
 
         expect:
         new GiteaRepositoryProvider('pditommaso/hello', obj)
@@ -91,7 +78,7 @@ class GiteaRepositoryProviderTest extends Specification {
     def 'should read file content'() {
         given:
         def token =  System.getenv('NXF_GITEA_ACCESS_TOKEN')
-        def config = new ProviderConfig('gitea', new ConfigSlurper().parse(CONFIG).providers.mygitea as ConfigObject) .setAuth(token)
+        def config = new ProviderConfig('gitea') .setAuth(token)
 
         when:
         def repo = new GiteaRepositoryProvider('pditommaso/test-hello', config)
@@ -112,7 +99,7 @@ class GiteaRepositoryProviderTest extends Specification {
     def 'should read bytes gitea content'() {
         given:
         def token =  System.getenv('NXF_GITEA_ACCESS_TOKEN')
-        def config = new ProviderConfig('gitea', new ConfigSlurper().parse(CONFIG).providers.mygitea as ConfigObject) .setAuth(token)
+        def config = new ProviderConfig('gitea') .setAuth(token)
         def repo = new GiteaRepositoryProvider('pditommaso/test-hello', config)
         and:
         def DATA = this.class.getResourceAsStream('/test-asset.bin').bytes
@@ -129,7 +116,7 @@ class GiteaRepositoryProviderTest extends Specification {
     def 'should read bytes file content'() {
         given:
         def token =  System.getenv('NXF_GITEA_ACCESS_TOKEN')
-        def config = new ProviderConfig('gitea', new ConfigSlurper().parse(CONFIG).providers.mygitea as ConfigObject).setAuth(token)
+        def config = new ProviderConfig('gitea').setAuth(token)
 
         when:
         def repo = new GiteaRepositoryProvider('pditommaso/test-hello', config)
