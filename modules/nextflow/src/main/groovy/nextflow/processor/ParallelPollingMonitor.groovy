fcf5b8791e73e96c0500c3c6f0d729de20906ba7
Jorge Ejarque <jorgee@users.noreply.github.com>
2025-07-28 16:38:07 +0200

    Fix Job arrays exceeding queueSize (#6314)
    ---------
    
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/ParallelPollingMonitor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/ParallelPollingMonitor.groovy
@@ -55,7 +55,7 @@ class ParallelPollingMonitor extends TaskPollingMonitor {
 
     @Override
     protected boolean canSubmit(TaskHandler handler) {
-        return super.canSubmit(handler) && (semaphore == null || semaphore.tryAcquire())
+        return super.canSubmit(handler) && (semaphore == null || semaphore.tryAcquire(handler.getForksCount()))
     }
 
     protected RateLimiter createSubmitRateLimit() {
@@ -95,7 +95,7 @@ class ParallelPollingMonitor extends TaskPollingMonitor {
 
     @Override
     boolean evict(TaskHandler handler) {
-        semaphore?.release()
+        semaphore?.release(handler.getForksCount())
         return super.evict(handler)
     }
 }
