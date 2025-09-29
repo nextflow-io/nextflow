74744824f0f5ef9d0da8e9b6103af6cd4a66e80d
Ben Sherman <bentshermann@gmail.com>
2025-09-29 11:48:13 -0500

    Apply type detection fix to FunctionSpec
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/plugin/spec/FunctionSpec.groovy
+++ modules/nextflow/src/main/groovy/nextflow/plugin/spec/FunctionSpec.groovy
@@ -58,7 +58,7 @@ class FunctionSpec {
 
     private static Object fromType(ClassNode cn) {
         final name = Types.getName(cn.getTypeClass())
-        if( cn.getGenericsTypes() != null ) {
+        if( !cn.isGenericsPlaceHolder() && cn.getGenericsTypes() != null ) {
             final typeArguments = cn.getGenericsTypes().collect { gt -> fromType(gt.getType()) }
             return [ name: name, typeArguments: typeArguments ]
         }
