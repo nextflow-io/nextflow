9bcf801b95675b790ac1128995e15aaf9735ea8a
Ben Sherman <bentshermann@gmail.com>
2025-10-08 13:09:39 -0500

    Track remote input path in FileHolder
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskInputResolver.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskInputResolver.groovy
@@ -112,7 +112,7 @@ class TaskInputResolver {
             if( item instanceof Path || coerceToPath ) {
                 final path = resolvePath(item)
                 final target = executor.isForeignFile(path) ? foreignFiles.addToForeign(path) : path
-                final holder = new FileHolder(target)
+                final holder = new FileHolder(path, target)
                 files << holder
             }
             else {
