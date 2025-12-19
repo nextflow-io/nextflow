306b2fb22ff0995b5ef070da1365213467e883d2
jorgee <jorge.ejarque@seqera.io>
2025-12-19 17:46:00 +0100

    add accelerator to trace record
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>

--- plugins/nf-tower/src/test/io/seqera/tower/plugin/TowerClientTest.groovy
+++ plugins/nf-tower/src/test/io/seqera/tower/plugin/TowerClientTest.groovy
@@ -560,4 +560,29 @@ class TowerClientTest extends Specification {
         req.tasks[0].numSpotInterruptions == 3
     }
 
+    def 'should include accelerator in task map'() {
+        given:
+        def client = Spy(new TowerClient())
+        client.getWorkflowProgress(true) >> new WorkflowProgress()
+
+        def now = System.currentTimeMillis()
+        def trace = new TraceRecord([
+            taskId: 42,
+            process: 'foo',
+            workdir: "/work/dir",
+            cpus: 1,
+            submit: now-2000,
+            start: now-1000,
+            complete: now
+        ])
+        trace.setAccelerator(true)
+
+        when:
+        def req = client.makeTasksReq([trace])
+
+        then:
+        req.tasks.size() == 1
+        req.tasks[0].accelerator == true
+    }
+
 }
