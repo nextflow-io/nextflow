5762d6dc0ac6e31a9c9ad1b11814158a213379c5
jorgee <jorge.ejarque@seqera.io>
2026-01-07 13:22:10 +0100

    clean unnecessary change
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>

--- plugins/nf-tower/src/main/io/seqera/tower/plugin/TowerClient.groovy
+++ plugins/nf-tower/src/main/io/seqera/tower/plugin/TowerClient.groovy
@@ -661,6 +661,7 @@ class TowerClient implements TraceObserverV2 {
         record.machineType = trace.getMachineInfo()?.type
         record.priceModel = trace.getMachineInfo()?.priceModel?.toString()
         record.numSpotInterruptions = trace.getNumSpotInterruptions()
+
         return record
     }
 
