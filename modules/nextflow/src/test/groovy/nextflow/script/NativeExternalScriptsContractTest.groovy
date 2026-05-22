187026c9fd477b0d337d11945f33d311d7c60d6d
Edmund Miller <edmund.miller@seqera.io>
2026-05-22 11:58:38 -0500

    Add native external scripts contract spec
    Signed-off-by: Edmund Miller <edmund.miller@seqera.io>

---
+++ modules/nextflow/src/test/groovy/nextflow/script/NativeExternalScriptsContractTest.groovy
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2013-2026, Seqera Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.script
+
+import spock.lang.Narrative
+import spock.lang.PendingFeature
+import spock.lang.See
+import spock.lang.Specification
+import spock.lang.Title
+import spock.lang.Unroll
+
+/**
+ * Executable contract for the native external scripts ADR.
+ *
+ * @author Edmund Miller <edmund.miller@seqera.io>
+ */
+@Title('Native external scripts run unchanged and receive typed Nextflow task context')
+@Narrative('''
+The native external scripts ADR proposes a replacement for template-time source
+rewriting: external Bash, Python, R, Julia, and related scripts should be staged
+as real native-language files, while Nextflow injects task context through
+sidecars and language-specific runtime namespaces.
+
+This contract is intentionally pending. Each feature method names one design
+promise from the ADR so implementation work can replace the stub with a real
+assertion and remove @PendingFeature when the behavior lands.
+''')
+@See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+class NativeExternalScriptsContractTest extends Specification {
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'external script source is staged into the task work directory unchanged'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'native script execution does not perform Groovy or GString template expansion on the source file'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'task context is serialized to a canonical sidecar before the native script runs'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'Bash scripts receive process context through associative arrays instead of rewritten variables'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'Python scripts can import a nextflow namespace object backed by the task sidecar'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'generated Python type stubs specialize process inputs outputs params and resources'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'native external scripts are staged through module and workflow resource bundles'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'script-relative assets remain resolvable from the external script body'() {
+        expect:
+        false
+    }
+
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def 'legacy template expansion remains available for existing template users'() {
+        expect:
+        false
+    }
+
+    @Unroll
+    @PendingFeature
+    @See('https://github.com/nextflow-io/nextflow/blob/master/adr/20260522-native-external-scripts.md')
+    def '#language native scripts expose the common process context fields'() {
+        expect:
+        false
+
+        where:
+        language | fields
+        'Bash'   | ['input', 'output', 'params', 'resources', 'cpus', 'attempt']
+        'Python' | ['input', 'output', 'params', 'resources', 'cpus', 'attempt']
+        'R'      | ['input', 'output', 'params', 'resources', 'cpus', 'attempt']
+        'Julia'  | ['input', 'output', 'params', 'resources', 'cpus', 'attempt']
+    }
+}
