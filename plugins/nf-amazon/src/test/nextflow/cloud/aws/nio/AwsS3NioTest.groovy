1a5f18cd67f8b78424939ddd0d93914f1e62f0b5
jorgee <jorge.ejarque@seqera.io>
2026-05-22 16:53:10 +0200

    fix error in test
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>

--- plugins/nf-amazon/src/test/nextflow/cloud/aws/nio/AwsS3NioTest.groovy
+++ plugins/nf-amazon/src/test/nextflow/cloud/aws/nio/AwsS3NioTest.groovy
@@ -441,7 +441,7 @@ class AwsS3NioTest extends Specification implements AwsS3BaseSpec {
 
     def 'should throw unsupported when trying delete a bucket' () {
         given:
-        final bucketName = createBucket()
+        def bucketName = createBucket()
 
         when:
         Files.delete(s3path("s3://$bucketName"))
