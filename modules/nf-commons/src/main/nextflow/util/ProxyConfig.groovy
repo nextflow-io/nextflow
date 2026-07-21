cac2e0db6ec7aef941fb2e450ecac0cabf5867ea
Ben Sherman <bentshermann@gmail.com>
2026-07-21 19:35:00 +0200

    Accept http requesting-protocol for https proxy CONNECT tunnel auth (#7369)
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- modules/nf-commons/src/main/nextflow/util/ProxyConfig.groovy
+++ modules/nf-commons/src/main/nextflow/util/ProxyConfig.groovy
@@ -70,8 +70,15 @@ class ProxyConfig {
                     return null
                 if( !getRequestingHost().equalsIgnoreCase(host) )
                     return null
-                if( protocol && !protocol.equalsIgnoreCase(getRequestingProtocol()) )
-                    return null
+                if( protocol && !protocol.equalsIgnoreCase(getRequestingProtocol()) ) {
+                    // For Basic proxy authentication the JDK hard-codes "http" as the requesting
+                    // protocol regardless of the destination scheme, so an https proxy config
+                    // must also accept an http requesting protocol, otherwise credentials for an
+                    // HTTPS CONNECT tunnel are discarded.
+                    final isHttpsTunnel = protocol.equalsIgnoreCase('https') && 'http'.equalsIgnoreCase(getRequestingProtocol())
+                    if( !isHttpsTunnel )
+                        return null
+                }
                 if( port && getRequestingPort() != Integer.parseInt(port) ) {
                     return null
                 }
