45577083fae7afb36331031ec15db090c8fd353a
Ben Sherman <bentshermann@gmail.com>
2026-07-30 14:35:01 -0500

    Move typed operators to Channel type reference
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- .docusaurus_site/sidebars.js
+++ .docusaurus_site/sidebars.js
@@ -192,7 +192,6 @@ module.exports = {
                     ]
                 },
                 "reference/process",
-                "reference/operator-typed",
                 "reference/operator"
             ]
         },
